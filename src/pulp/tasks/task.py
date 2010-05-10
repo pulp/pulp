919a80bc778608b613d80e777ed0b4f8bed980aa
Jason L Connor <jconnor@redhat.com>
2010-05-10 16:08:54 -0600

    Removed pool (not needed) Implemented task queue w/ dispatcher that runs in a separate thread Converted Task class to use a wrapper method that executes in the child thread

--- src/pulp/tasks/task.py
+++ src/pulp/tasks/task.py
@@ -16,47 +16,79 @@
 
 __author__ = 'Jason L Connor <jconnor@redhat.com>'
 
+import sys
 import threading
+import traceback
+from datetime import datetime
 
 
 CREATED = 'created'
-PROCESSING = 'processing'
+RUNNING = 'running'
 FINISHED = 'finished'
 ERROR = 'error'
 
 
 class Task(object):
     """
+    Task class
+    Used to run long-running tasks in their own thread.
     """
-    def __init__(self, callable, args, kwargs, callback_name=None):
-        self.status = CREATED
+    def __init__(self, callable, args=[], kwargs={}):
+        """
+        Create a Task for the passed in callable and arguments.
+        @param callable: function, method, lambda, or object implementing __call__
+        @param args: list of positional arguments to be passed into the callable
+        @param kwargs: dictionary of keyword arguments to be passed into the callable
+        """
         self.callable = callable
         self.args = args
-        self.args = kwargs
-        self.progress = None
+        self.kwargs = kwargs
         
-        if callback_name is not None:
-            self.kwargs[callback_name] = self.callback
-            
-        self.__thread = threading.Thread(target=self.callable,
-                                         args=self.args,
-                                         kwargs=self.kwargs)
+        self.status = CREATED
+        self.start_time = None
+        self.finish_time = None
+        self.exception = None
+        self.traceback = None
+        
+        self.__queue = None
+        
+        self.__thread = threading.Thread(target=self.wrapper)
         self.__thread.start()
         
     @property
-    def ident(self):
+    def id(self):
         return self.__thread.ident
+    
+    def _set_queue(self, task_queue):
+        """
+        Semi-private method when used in conjunction with a
+        pulp.tasks.queue.TaskQueue instance for setting a back reference to the
+        queue itself.
+        """
+        self.__queue = task_queue
         
-    def callback(self, current, total):
-        if current == total:
+    def __wrapper(self):
+        """
+        Private wrapper that executes the callable and captures and records any
+        exceptions in a separate thread.
+        """
+        self.status = RUNNING
+        self.start_time = datetime.now()
+        try:
+            self.callable(*self.args, **self.kwargs)
+        except Exception, e:
+            self.exception = e
+            exec_info = sys.exc_info()
+            self.traceback = traceback.format_exception(*exec_info)
+            self.status = ERROR
+        else:
             self.status = FINISHED
-        self.progress = current / total
+        self.finish_time = datetime.now()
+        if self.__queue is not None:
+            self.__queue._finished(self)        
         
     def run(self):
+        """
+        Run this task's callable in a separate thread.
+        """
         self.__thread.run()
-        self.status = PROCESSING
-        
-    def check(self):
-        if not self.__thread.is_alive():
-            self.status = FINISHED
-        return self.status
