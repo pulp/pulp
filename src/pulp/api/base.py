5741a920c1089d3ff62c0f0d084a90fe3ee0cd16
John Matthews <jmatthew@redhat.com>
2010-05-28 15:17:53 -0400

    modelchanges branch will use a different, db _database_model_changes

--- src/pulp/api/base.py
+++ src/pulp/api/base.py
@@ -24,7 +24,8 @@ class BaseApi(object):
     def __init__(self):
         # Mongo DB
         self.connection = pymongo.Connection()
-        self.db = self.connection._database
+        #self.db = self.connection._database
+        self.db = self.connection._database_model_changes
         # Inject the collection's namespace into each object
         self.db.add_son_manipulator(NamespaceInjector())
         # Provides auto-referencing/auto-dereferencing ability
