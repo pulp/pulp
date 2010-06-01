dfce3a3790c659f0df046c60b5d67556b6443ddb
Jason L Connor <jconnor@redhat.com>
2010-06-01 16:00:10 -0600

    removed my own thread class and converted tasks to use "fire and forget" threads i.e. spawn a new thread each time it runs altered Task.wait to account for new semantics removed Task.exit (no longer necessary) modified test_tasks.py to take into account new semantics

--- test/unit/test_tasks.py
+++ test/unit/test_tasks.py
@@ -7,8 +7,10 @@ from pulp.tasks.task import Task, FINISHED
 from pulp.tasks.queue.fifo import FIFOTaskQueue
 
 
-def thread_id(id):
-    assert thread.get_ident() == id
+THREAD_ID = None
+def thread_id():
+    global THREAD_ID
+    THREAD_ID = thread.get_ident()
 
 def print_thread_id():
     print 'thread id: %s' % str(thread.get_ident())
@@ -22,28 +24,21 @@ class TaskTester(unittest.TestCase):
     def tearDown(self):
         pass
 
-#    def test_t_allocation(self):
-#        t = Task(print_thread_id)
-
     def test_task(self):
         t = Task(thread_id)
         print 'task id: %s' % str(t.id)
-        t.args=[t.id]
         t.run()
-        time.sleep(0.0005)
-        t.exit()
-        self.assertTrue(t.status == FINISHED)
+        t.wait()
+        self.assertTrue(t.thread_id == THREAD_ID)
 
     def test_multi_runs(self):
         t = Task(thread_id)
         print 'task id: %s' % str(t.id)
-        t.args=[t.id]
         t.run()
-        time.sleep(0.0005)
+        t.wait()
         self.assertTrue(t.status == FINISHED)
         t.run()
-        time.sleep(0.0005)
-        t.exit()
+        t.wait()
         self.assertTrue(t.status == FINISHED)
 
 
@@ -61,16 +56,10 @@ class FIFOQueueTester(unittest.TestCase):
     def test_task_dispatch(self):
         q = FIFOTaskQueue()
         t = Task(print_thread_id)
-        print 'task id: %s' % str(t.id)
         q.enqueue(t)
-        time.sleep(0.005)
+        t.wait()
         self.assertTrue(t.status == FINISHED)
-        q.clear()
 
 
 if __name__ == '__main__':
-    #unittest.main()
-    cases = [TaskTester, FIFOQueueTester]
-    for c in cases:
-        suite = unittest.TestLoader().loadTestsFromTestCase(c)
-        unittest.TextTestRunner(verbosity=2).run(suite)
+    unittest.main()
