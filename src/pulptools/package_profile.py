d44bcaa6a2177ab69dc02258e15c0009cb5f4bee
Pradeep Kilambi <pkilambi@redhat.com>
2010-06-14 12:47:58 -0400

    package profile module to get installed rpms info. To be invoked by pulp daemon

---
+++ src/pulptools/package_profile.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# Authors: Pradeep Kilambi <pkilambi@redhat.com>
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+from pulptools.logutil import getLogger
+log = getLogger(__name__)
+
+"""
+Module for Package profile accumulation
+"""
+
+class PackageProfile(object):
+    """
+    Class for probing package profile info
+    @ivar type: type of package content to run lookups on eg: 'rpm','jar','zip' etc.
+    @type TYPE: str
+    """
+    def __init__(self, type='rpm'):
+        self.pkgtype = type
+        self.pkglist = []
+        
+    def getPackageList(self):
+        """
+        Get I{ordered} pkg hash objects.
+        @return: A list of ordered pkg hash objects.
+        @rtype: list
+        """
+        if self.pkgtype == 'rpm':
+            return self.__getInstalledRpms()
+
+    def __getInstalledRpms(self):
+        """ Accumulates list of installed rpm info """
+        
+        import rpm
+        ts = rpm.TransactionSet()
+        ts.setVSFlags(-1)
+        installed = ts.dbMatch()
+        for h in installed:
+            pkg = {
+                'name'          : h['name'],
+                'version'       : h['version'],
+                'release'       : h['release'],
+                'epoch'         : h['epoch'] or "",
+                'arch'          : h['arch'],
+                'installtime'   : h['installtime'],
+                'group'         : h['Group'] or "",
+                'summary'       : h['Summary'],
+                'description'   : h['description'],
+                'OS'            : h['OS'],
+                'Platform'      : h['Platform'],
+                'URL'           : h['URL'],
+                'Size'          : h['Size'],
+                'Vendor'        : h['Vendor'],             
+            }
+            self.pkglist.append(pkg)
+        self.pkglist.sort(key=lambda pkg:(pkg['name'], pkg['epoch'], \
+                                          pkg['version'], pkg['release']))
+        return self.pkglist
+    
+    def _getInstalledJars(self):
+        pass
+    
+    def _getInstalledZips(self):
+        pass
+
+
+if __name__ == '__main__':
+    pp = PackageProfile()
+    import pprint
+    pprint.pprint(pp.getPackageList())
