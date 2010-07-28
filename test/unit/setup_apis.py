5c8619349fc81d2c02a422bfc9cfc49d7d858491
Mike McCune <mmccune@redhat.com>
2010-07-28 11:04:37 -0700

    simple util

---
+++ test/unit/setup_apis.py
@@ -0,0 +1,15 @@
+## Simple file you can run with ipython if you want to poke around the API ##
+import sys
+sys.path.append("../../src")
+from pulp.api.consumer import ConsumerApi
+from pulp.api.package import PackageApi
+from pulp.api.repo import RepoApi
+
+from pulp.model import Package
+from pulp.model import Consumer
+from pulp.model import Repo
+
+capi = ConsumerApi(dict())
+papi = PackageApi(dict())
+rapi = RepoApi(dict())
+
