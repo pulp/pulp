a98016eb9beb70e7babe4dbb793252392a203b16
Jason L Connor <jconnor@redhat.com>
2010-08-09 09:23:40 -0600

    some style changes from ported code

--- src/pulp/tasking/queue/thread.py
+++ src/pulp/tasking/queue/thread.py
@@ -29,15 +29,15 @@ def _raise_exception_in_thread(tid, exctype):
     Raises an exception in the threads with id tid.
     """
     if not inspect.isclass(exctype):
-        raise TypeError("Only types can be raised (not instances)")
+        raise TypeError('Only types can be raised (not instances)')
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
     if res == 0:
-        raise ValueError("invalid thread id")
+        raise ValueError('Invalid thread id')
     elif res != 1:
         # NOTE if it returns a number greater than one, you're in trouble, 
         # and you should call it again with exc=NULL to revert the effect
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
+        raise SystemError('PyThreadState_SetAsyncExc failed')
     
     
 class InterruptableThread(threading.Thread):
