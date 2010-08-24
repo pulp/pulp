de44ee96ba9b939c5cb96fcb542e24476c6c5796
Jay Dobies <jason.dobies@redhat.com>
2010-08-24 13:53:05 -0400

    Cleanup in preparation for adding admin cert checking: removed extra logging configuration, extra log messages, added documentation

--- src/pulp/server/webservices/role_check.py
+++ src/pulp/server/webservices/role_check.py
@@ -51,62 +51,58 @@ class RoleCheck(object):
     def __call__(self, f):
         def check_roles(*fargs, **kw):
             '''
-              Strip off the decorator arguments so we can use those to check the
-              Roles of the current caller.
+            Strip off the decorator arguments so we can use those to check the
+            roles of the current caller.
 
-              Note: the first argument cannot be "self" because we get a parse error
-              "takes at least 1 argument" unless the instance is actually included in
-              the argument list, which is redundant.  If this wraps a class instance,
-              the "self" will be the first argument.
+            Note: the first argument cannot be "self" because we get a parse error
+            "takes at least 1 argument" unless the instance is actually included in
+            the argument list, which is redundant.  If this wraps a class instance,
+            the "self" will be the first argument.
             '''
             
-            # Check the roles
-            log.debug("\n\nRole checking start, function: %s" % str(f))
-            roles = {'consumer':None, 'admin': None, 'consumer_id': None}
+            # Determine which roles will be checked by this instance of the decorator
+            roles = {'consumer': None, 'admin': None, 'consumer_id': None}
             for key in self.dec_kw.keys():
-                log.debug("Role Name [%s], check? [%s]" % (key, self.dec_kw[key]))
                 roles[key] = self.dec_kw[key]
             
             admin_access_granted = False
             consumer_access_granted = False
             
-            # Admin role trumps any other checking.  do it first
-            if (roles['admin']):
-                ## If not using cert check uname and password
-                # TODO: Implement uname/pass checking
-                log.debug("Checking username/pass")
+            # Admin role trumps any other checking
+            if roles['admin']:
+                # If not using cert check uname and password
                 try:
                     admin_access_granted = self.check_username_pass(*fargs)
-                    log.debug("admin access granted: %s" % admin_access_granted)
                 except PulpException, pe:
                     # TODO: Figure out how to re-use the same return function in base.py
                     http.status_unauthorized()
                     http.header('Content-Type', 'application/json')
                     return json.dumps(pe.value, default=pymongo.json_util.default)
             
-            ## Check cert
-            if (roles['consumer'] or roles['consumer_id']):
+            # Consumer role checking
+            if not admin_access_granted and (roles['consumer'] or roles['consumer_id']):
                 consumer_access_granted = self.check_consumer(roles['consumer_id'], *fargs)
                 log.debug("consumer_access_granted? %s " % consumer_access_granted)
-            
-            log.debug("AAG: %s, CAG: %s" % (admin_access_granted, consumer_access_granted))
-            if (not admin_access_granted and not consumer_access_granted):
-                log.debug("Access denied.")
+
+            # Process the results of the auth checks
+            if not admin_access_granted and not consumer_access_granted:
                 http.status_unauthorized()
                 http.header('Content-Type', 'application/json')
                 return json.dumps("Authorization Failure.  Check your username and password or your certificate", 
                                   default=pymongo.json_util.default)
-            # Access granted, proceed
-            log.debug("check_roles : Access granted")
-            # Does this wrap a class instance?
+
+            # If we get this far, access has been granted (in access failed the method
+            # would have returned by now)
+
+            # If it wraps a class instance, call the function on the instance;
+            # otherwise just call it directly
             if fargs and getattr(fargs[0], '__class__', None):
                 instance, fargs = fargs[0], fargs[1:]
-                # call the method with just the fargs and kw for the original method
-                ret=f(instance, *fargs, **kw)
+                result = f(instance, *fargs, **kw)
             else:
-                # just send in the give args and kw
-                ret=f(*(fargs), **kw)
-            return ret
+                result = f(*(fargs), **kw)
+
+            return result
 
         # Save wrapped function reference
         self.f = f
@@ -116,72 +112,89 @@ class RoleCheck(object):
         return check_roles
 
     def check_username_pass(self, *fargs):
+        '''
+        If the request uses HTTP authorization, verify the credentials identify a
+        valid user in the system.
+
+        @return: True if the user credentials are in the requests and are valid;
+                 False otherwise
+        @rtype:  boolean
+        '''
+
+        # Get the credentials from the request
         environment = web.ctx.environ
         auth_string = environment.get('HTTP_AUTHORIZATION', None)
-        if (auth_string is not None and auth_string.startswith("Basic")):
-            logging.root.setLevel(logging.DEBUG)
-            logging.root.addHandler(logging.StreamHandler())
-            log.debug("auth_string string: %s" % auth_string)
+
+        # If credentials were passed in, verify them
+        if auth_string is not None and auth_string.startswith("Basic"):
+
+            # Parse out the credentials
             encoded_auth = auth_string.split(" ")[1]
             auth_string = base64.decodestring(encoded_auth)
             uname_pass = auth_string.split(":")
             username = uname_pass[0]
             password = uname_pass[1]
+
+            # Verify a user exists with the given name
             user = userApi.user(username)
             if (user is None):
-                raise PulpException("User with login [%s] does not exist" 
-                                    % username)
-            log.debug("Username: %s hashed password: %s" % (username, password))
-            log.debug("Stored user password: %s" % user['password'])
-            goodPassword = password_util.check_password(user['password'], password) 
-            log.debug("Good Password? [%s]" % goodPassword)
-            return goodPassword
+                return False
+
+            # Verify the correct password was specified
+            good_password = password_util.check_password(user['password'], password)
+            return good_password
+
         return False
-    
-    def check_consumer(self, check_id=False, *fargs): 
-        ## This is where we will extract CERT fields
+            
+    def check_consumer(self, check_id=False, *fargs):
+        '''
+        Determines if the certificate in the request represents a valid consumer certificate.
+
+        @param check_id: if True, the consumer UID will be checked to make sure it
+                         is present in the fargs argument; if False the only validation
+                         will be that the UID exists in the DB (default = False)
+        @type  check_id: boolean
+
+        @return: True if the request contains a valid certificate; False otherwise
+        @rtype:  boolean
+        '''
+
+        # Extract the certificate from the request
         environment = web.ctx.environ
-        # print("Env: %s" % environment)
-        for key in environment.keys():
-            if (key.startswith('SSL_')):
-                value = str(environment.get(key, None))
-                log.debug("SSL k: " + key + ", v: " + value)
-        cs = environment.get('SSL_CLIENT_CERT', None)
+        cert_pem = environment.get('SSL_CLIENT_CERT', None)
+
+        # If there's no certificate, punch out early
+        if cert_pem is None:
+            return False
+
+        # Parse the certificate and extract the consumer UUID
+        idcert = Certificate(content=cert_pem)
+        subject = idcert.subject()
+        consumer_cert_uid = subject.get('CN', None)
+
+        # If there is no consumer ID, this is not a valid consumer certificate
+        if consumer_cert_uid is None:
+            log.error("Consumer UID not found in certificate")
+            return False
+
+        # Check that it is a valid consumer in our DB
+        consumer = consumerApi.consumer(consumer_cert_uid)
+        if not consumer:
+            log.error("Consumer with id [%s] does not exist" % consumer_cert_uid)
+            return False
+
+        # If the ID should be explicitly verified against a provided list, the ID in the
+        # certificate will be verified against the ID found in the certificate
         good_certificate = False
-        if cs != None:
-            idcert = Certificate(content=cs)
-            log.debug("parsed ID CERT: %s" % idcert)
-            subject = idcert.subject()
-            consumer_cert_uid = subject.get('CN', None)
-            if (consumer_cert_uid is None):
-                log.error("Consumer UID not found in certificate.  " + \
-                          "Not a valid Consumer certificate")
-                good_certificate = False
-                return good_certificate 
-            log.error("Consumer UID: %s " % consumer_cert_uid)
-            # Check that it is a valid consumer in our db
-            consumer = consumerApi.consumer(consumer_cert_uid)
-            if not consumer:
-                log.error("Consumer with id: %s does not exist" % 
-                          consumer_cert_uid)
-                good_certificate = False
-                return good_certificate 
-                
-            # Check the consumer_id matches
-            log.debug("check_id: %s" % check_id) 
-            if check_id:
-                log.debug("Checking ID in cert vs param passed in")            
-                for arg in fargs:
-                    log.debug("Arg [%s]" % arg)
-                    if (arg == consumer_cert_uid):
-                        good_certificate = True
-                        break
-            else:
-                good_certificate = True
-            if (not good_certificate):
-                log.error("Certificate UID doesnt match the consumer UID you passed in") 
-            else:
-                log.error("Certificate UID matched.  continue")
+        if check_id:
+            for arg in fargs:
+                log.debug("Checking ID in cert [%s] against expected ID [%s]" %
+                          (consumer_cert_uid, arg))
+                if arg == consumer_cert_uid:
+                    good_certificate = True
+                    break
+        else:
+            good_certificate = True
 
         return good_certificate
             
