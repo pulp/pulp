1eebd9e771778a887c4a2c651c07a0d6485840eb
Jason L Connor <jconnor@redhat.com>
2010-09-10 18:06:18 -0600

    moved canonical server-side queue async module and implemented simple api for it

--- src/pulp/server/async.py
+++ src/pulp/server/async.py
@@ -12,3 +12,24 @@
 # Red Hat trademarks are not licensed under GPLv2. No permission is
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
+
+from pulp.server.tasking.queue.fifo import FIFOTaskQueue
+from pulp.server.tasking.task import Task
+
+# async execution queue -------------------------------------------------------
+
+queue = FIFOTaskQueue()
+
+# async api -------------------------------------------------------------------
+
+def run_async(method, args, kwargs, timeout=None, unique=True):
+    """
+    """
+    task = Task(method, args, kwargs, timeout)
+    if queue.enqueue(task, unique):
+        return task
+    return None
+
+cancel_async = queue.cancel
+
+find_async = queue.find
