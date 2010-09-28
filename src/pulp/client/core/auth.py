7986bf246f18377f3cf2a4f3d4f1817c356ccf05
Jason L Connor <jconnor@redhat.com>
2010-09-28 10:25:54 -0600

    changed auth core module to use new command/action separation

--- src/pulp/client/core/auth.py
+++ src/pulp/client/core/auth.py
@@ -15,91 +15,82 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 
-import gettext
 import os
 import sys
+from gettext import gettext as _
 
-import pulp.client.auth_utils as auth_utils
+from pulp.client import auth_utils
 from pulp.client.config import Config
 from pulp.client.connection import UserConnection
-from pulp.client.core.base import BaseCore
+from pulp.client.core.base import BaseCore, Action
 from pulp.client.logutil import getLogger
 
 
-CFG = Config()
-log = getLogger(__name__)
-
-_ = gettext.gettext
+_cfg = Config()
+_log = getLogger(__name__)
 
 
-class auth(BaseCore):
+class AuthAction(Action):
 
-    def __init__(self):
-        usage = 'usage: %prog auth [OPTIONS]'
-        shortdesc = 'stores authentication credentials for the user on the machine.'
-        desc = ''
-
-        self.name = 'auth'
-        self.actions = {'login' : 'Stores user credentials on this machine',
-                        'logout': 'Removes stored user credentials on this machine', }
-        self.is_admin = True
-
-        BaseCore.__init__(self, 'auth', usage, shortdesc, desc)
-
-    def load_server(self):
-        self.authconn = UserConnection(host=CFG.server.host or "localhost",
-                                       port=CFG.server.port or 443,
+    def setup_server(self):
+        self.authconn = UserConnection(host=_cfg.server.host or "localhost",
+                                       port=_cfg.server.port or 443,
                                        username=self.username,
                                        password=self.password,
-                                       cert_file=self.cert_filename,
-                                       key_file=self.key_filename)
+                                       cert_file=self.cert_file,
+                                       key_file=self.key_file)
 
-    def generate_options(self):
-        usage = 'auth'
-        self.setup_option_parser(usage, '', True)
 
-    def _do_core(self):
-        self.action = self._get_action()
-        if self.action == 'login':
-            self._login()
-        if self.action == 'logout':
-            self._logout()
+class Login(AuthAction):
 
-    def _login(self):
-        if not self.options.username and not self.options.password:
-            print _("username and password are required. Try --help")
+    def run(self):
+        if not self.command_opts.username and not self.command_opts.password:
+            print _("username and password are required; try --help")
             sys.exit(1)
-
         # Retrieve the certificate information from the server
         cert_dict = self.authconn.admin_certificate()
-
         # Determine the destination and store the cert information there
         if not os.path.exists(auth_utils.admin_cert_dir()):
             os.makedirs(auth_utils.admin_cert_dir())
-
         # Write the certificate data
         cert_filename, key_filename = auth_utils.admin_cert_paths()
-
         f = open(cert_filename, 'w')
         f.write(cert_dict['certificate'])
         f.close()
-
         f = open(key_filename, 'w')
         f.write(cert_dict['private_key'])
         f.close()
-
-        print _('User credentials successfully stored at [%s]') % \
+        print _('user credentials successfully stored at [%s]') % \
             auth_utils.admin_cert_dir()
 
-    def _logout(self):
+
+class Logout(AuthAction):
+
+    def run(self):
         # Determine the destination and store the cert information there
         cert_filename, key_filename = auth_utils.admin_cert_paths()
-
         # Remove the certificate and private key files
         if os.path.exists(cert_filename):
             os.remove(cert_filename)
-
         if os.path.exists(key_filename):
             os.remove(key_filename)
+        print _('user credentials removed from [%s]') % auth_utils.admin_cert_dir()
 
-        print _('User credentials removed from [%s]') % auth_utils.admin_cert_dir()
+
+class Auth(BaseCore):
+
+    _default_actions = {
+        'login': 'stores user credentials on this machine',
+        'logout': 'removes stored user credentials on this machine',
+    }
+
+    def __init__(self, name='auth', actions=_default_actions):
+        super(Auth, self).__init__(name, actions)
+        self.login = Login()
+        self.logout = Logout()
+
+    def short_description(self):
+        return 'stores authentication credentials for the user on the machine'
+
+
+command_class = auth = Auth
