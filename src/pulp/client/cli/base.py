daa23d0ca8ee0eb8a708d1495d6bf46eff8af43f
Jason L Connor <jconnor@redhat.com>
2010-09-28 10:17:04 -0600

    separated parser setup from parsing arguments

--- src/pulp/client/cli/base.py
+++ src/pulp/client/cli/base.py
@@ -55,25 +55,35 @@ class PulpBase(object):
         lines = ['Usage: %s <options> <command>' % os.path.basename(sys.argv[0]),
                  'Supported Commands:']
         for name, command in self.commands.items():
-            lines.append('\t%-14s %-25s' % (name, command.shortdesc))
+            lines.append('\t%-14s %-25s' % (name, command.short_description()))
         return '\n'.join(lines)
 
-    def parse_args(self, args):
+    def setup_parser(self):
         """
-        Parse the command line arguments.
-        @note: this method also defines the command line options and may be
-               overridden to define new options or to validate option values
-        @type args: list of str's
-        @param args: command line arguments
-        @rtype: optparse options object and list of str's
-        @return: options and list of any remaining (unparsed) arguments
+        Add options to the command line parser.
+        @note: this method may be overridden to define new options
         """
         credentials = OptionGroup(self.parser, _('pulp user account credentials'))
         credentials.add_option('-u', '--username', dest='username',
                                default=None, help=_('account username'))
         credentials.add_option('-p', '--password', dest='password',
                                default=None, help=_('account password'))
+        credentials.add_option('--cert-file', dest='cert_file',
+                               default=None, help=SUPPRESS_HELP)
+        credentials.add_option('--key-file', dest='key_file',
+                               default=None, help=SUPPRESS_HELP)
         self.parser.add_option_group(credentials)
+
+
+    def parse_args(self, args):
+        """
+        Parse the command line arguments.
+        @note: this method may be overridden to validate option values
+        @type args: list of str's
+        @param args: command line arguments
+        @rtype: optparse options object and list of str's
+        @return: options and list of any remaining (unparsed) arguments
+        """
         return self.parser.parse_args(args)
 
     def find_command(self, command):
@@ -93,11 +103,14 @@ class PulpBase(object):
         @type args: list of str's
         @param args: command line arguments
         """
+        self.setup_parser()
         opts, args = self.parse_args(args)
         if not args:
             self.parser.error(_('no command given: please see --help'))
-        command_module = self.find_command(args[0])
-        if command_module is None:
+        command = self.find_command(args[0])
+        if command is None:
             self.parser.error(_('invalid command: please see --help'))
-        command_module.main(opts, args[1:])
+        command.setup_credentials(opts.username, opts.password,
+                                  opts.cert_file, opts.key_file)
+        command.main(args[1:])
 
