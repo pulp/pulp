0b1219b0df54e584241e83c42b43d6342239db9c
Jason L Connor <jconnor@redhat.com>
2010-10-20 09:44:03 -0600

    added version module

---
+++ src/pulp/server/db/version.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2010 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+
+from pulp.server.db.connection import get_object_db
+from pulp.server.db.model import Base
+
+
+current_data_model_version = 1
+
+_version_db = None
+
+
+class DataModelVersion(Base):
+
+    def __init__(self, version):
+        _id = version
+        version = version
