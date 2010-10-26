a676583fdffc3fe28057450169fbd18d07319c1c
Jason L Connor <jconnor@redhat.com>
2010-10-26 11:28:41 -0600

    added comments and documentation to the version module

--- src/pulp/server/db/version.py
+++ src/pulp/server/db/version.py
@@ -13,26 +13,48 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 
+import logging
+import os
+import sys
+
 import pymongo
 
 from pulp.server.db.connection import get_object_db
 from pulp.server.db.model import Base
 
 
+# current data model version of the code base
+# increment this if you change the data model
 VERSION = 1
 
+_log = logging.getLogger('pulp')
 _version_db = None
 
+# data model version model ----------------------------------------------------
 
 class DataModelVersion(Base):
-
+    """
+    Simple data model to store data model versions and migrations in the
+    database.
+    @ivar version: version of the data model
+    @ivar validated: boolean flag set to True if the data model has been
+                     validated, False initially
+    """
     def __init__(self, version):
+        """
+        @type version: int
+        @param version: data model version this model represents
+        """
         self._id = version
         self.version = version
         self.validated = False
 
+# data model version api ------------------------------------------------------
 
 def _init_db():
+    """
+    Initialize the database connection.
+    """
     global _version_db
     if _version_db is not None:
         return
@@ -40,6 +62,11 @@ def _init_db():
 
 
 def _get_latest_version():
+    """
+    Utility function to fetch the latest DataModelVersion model from the db.
+    @rtype: L{DataModelVersion} instance
+    @return: the data model intance with the most recent version
+    """
     assert _version_db is not None
     versions = _version_db.find()
     if not versions:
@@ -49,38 +76,64 @@ def _get_latest_version():
 
 
 def get_version_in_use():
+    """
+    Fetch the data model version currently in use in the db.
+    @rtype: int
+    @return: integer data model version
+    """
     assert _version_db is not None
     v = _get_latest_version()
     return v.version
 
 
 def check_version():
+    """
+    Check for a version mismatch between the current data model version and the
+    data model version in use in the db. If a mismatch is detected, the
+    mismatch is logged and the application exits.
+    """
     assert _version_db is not None
     v = _get_latest_version()
-    # XXX this should log and exit if there is a version mismatch,
-    # not return a boolean
-    return v.version == VERSION
+    if v.version == VERSION:
+        return
+    _log.critical('data model version mismatch: %d in use, but needs to be %d' %
+                  v.version, VERSION)
+    _log.critical('pulp exiting: please migrate your database to the latest data model')
+    sys.exit(os.EX_DATAERR)
 
 
 def set_version(version):
+    """
+    Set the data model version in the database.
+    @type version: int
+    @param version: data model version
+    """
     assert _version_db is not None
     v = DataModelVersion(version)
     _version_db.save(v, safe=True)
 
 
 def is_validated():
+    """
+    Check to see if the latest data model version has been validated.
+    @rtype: bool
+    @return: True if the data model has been validated, False otherwise
+    """
     assert _version_db is not None
     v = _get_latest_version()
     return v.validated
 
 
 def set_validated():
+    """
+    Flag the latest data model version as validated.
+    """
     assert _version_db is not None
     v = _get_latest_version()
     v.validated = True
     _version_db.save(v, safe=True)
 
-# validate on import ----------------------------------------------------------
+# check for version mismatch on import ----------------------------------------
 
 _init_db()
 check_version()
