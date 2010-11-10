df371482962b21e81869130bc319c05b27ceb7c6
Pradeep Kilambi <pkilambi@redhat.com>
2010-11-10 12:01:24 -0500

    continue checking rest of packages in the batch before exiting

--- src/pulp/client/core/repo.py
+++ src/pulp/client/core/repo.py
@@ -530,8 +530,8 @@ class Upload(RepoAction):
             name, version, release, epoch, arch = pkginfo['nvrea']
 
             if self.pconn.find_package_by_nvrea(id, name, version, release, epoch, arch):
-                system_exit(os.EX_OK, \
-                            _("Package [%s] already exists on the server in repo %s") % (pkginfo['pkgname'], id))
+                print _("Package [%s] already exists on the server in repo %s") % (pkginfo['pkgname'], id)
+                continue
 
             pkgstream = base64.b64encode(open(frpm).read())
             status = self.pconn.upload(id, pkginfo, pkgstream)
