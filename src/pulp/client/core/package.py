fc969a7563e607045eefb0e930303871dc36b899
John Matthews <jmatthew@redhat.com>
2010-11-17 08:38:17 -0500

    change cli "search packages" to "package search"

--- src/pulp/client/core/package.py
+++ src/pulp/client/core/package.py
@@ -16,11 +16,13 @@
 #
 
 import os
+import string
 import sys
 import time
 from gettext import gettext as _
 from optparse import OptionGroup
-from pulp.client.connection import RepoConnection, ConsumerConnection, ConsumerGroupConnection
+
+from pulp.client.connection import RepoConnection, ConsumerConnection, ConsumerGroupConnection, SearchConnection
 from pulp.client.core.base import Action, Command
 from pulp.client.core.utils import print_header, system_exit
 
@@ -29,7 +31,9 @@ from pulp.client.core.utils import print_header, system_exit
 class PackageAction(Action):
 
     def setup_connections(self):
-        self.pconn = RepoConnection()
+
+        self.sconn = SearchConnection()
+        self.rconn = RepoConnection()
         self.cconn = ConsumerConnection()
         self.cgconn = ConsumerGroupConnection()
 
@@ -48,7 +52,7 @@ class Info(PackageAction):
     def run(self):
         name = self.get_required_option('name')
         repoid = self.get_required_option('repoid')
-        pkg = self.pconn.get_package(repoid, name)
+        pkg = self.rconn.get_package(repoid, name)
         if not pkg:
             system_exit(os.EX_DATAERR,
                         _("Package [%s] not found in repo [%s]") %
@@ -100,6 +104,69 @@ class Install(PackageAction):
                   (status['result'], (consumerid or consumergroupid))
         else:
             print _("\nPackage install failed")
+
+class Search(PackageAction):
+
+    description = _('search for packages')
+
+    def setup_parser(self):
+        self.parser.add_option("-a", "--arch", dest="arch", default=None,
+                               help=_("package arch regex to search for"))
+        self.parser.add_option("-e", "--epoch", dest="epoch", default=None,
+                               help=_("package epoch regex to search for"))
+        self.parser.add_option("-f", "--filename", dest="filename", default=None,
+                               help=_("package filename regex to search for"))
+        self.parser.add_option("-n", "--name", dest="name", default=None,
+                               help=_("package name regex to search for"))
+        self.parser.add_option("-r", "--release", dest="release", default=None,
+                               help=_("package release regex to search for"))
+        self.parser.add_option("-v", "--version", dest="version", default=None,
+                               help=_("package version regex to search for"))
+
+    def run(self):
+        arch = self.opts.arch
+        epoch = self.opts.epoch
+        filename = self.opts.filename
+        name = self.opts.name
+        release = self.opts.release
+        version = self.opts.version
+        pkgs = self.sconn.packages(name=name, epoch=epoch, version=version,
+                release=release, arch=arch, filename=filename)
+        if not pkgs:
+           system_exit(os.EX_DATAERR, _("No packages found."))
+
+        name_field_size = self.get_field_size(pkgs, field_name="name")
+        evra_field_size = self.get_field_size(pkgs, msg="%s:%s-%s.%s", 
+                field_names=("epoch", "version", "release", "arch"))
+        filename_field_size = self.get_field_size(pkgs, field_name="filename")
+        repos_field_size = self.get_field_size(pkgs, field_name="repos")
+        print_header(_("Package Information"))
+        print _("%s\t%s\t%s\t%s" % (self.form_item_string("Name", name_field_size),
+                self.form_item_string("EVRA", evra_field_size), 
+                self.form_item_string("Filename", filename_field_size),
+                self.form_item_string("Repositories", repos_field_size)))
+        for pkg in pkgs:
+            repos = ", ".join(pkg["repos"])
+            print "%s\t%s\t%s\t%s" % \
+                    (self.form_item_string(pkg["name"], name_field_size), 
+                    self.form_item_string("%s:%s-%s.%s" % (pkg["epoch"], pkg["version"],
+                        pkg["release"], pkg["arch"]), evra_field_size),
+                    self.form_item_string(pkg["filename"], filename_field_size),
+                    repos)
+
+    def form_item_string(self, msg, field_size):
+        return string.ljust(msg, field_size)
+
+    def get_field_size(self, items, msg=None, field_name="", field_names=()):
+        largest_item_length = 0
+        for item in items:
+            if msg:
+                test_string = msg % (field_names)
+            else:
+                test_string = "%s" % (item[field_name])
+            if len(test_string) > largest_item_length:
+                largest_item_length = len(test_string)
+        return largest_item_length
 
 # package command -------------------------------------------------------------
 
