411603ffbe9f6affee2d2a0ed22f2213a7929832
Jason L Connor <jconnor@redhat.com>
2010-12-14 19:16:23 -0700

    unittests to excesize permission and role code paths

--- test/unit/test_authorization.py
+++ test/unit/test_authorization.py
@@ -14,147 +14,295 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 
-# Python
-import base64
 import logging
 import os
+import random
+import string
 import sys
 import unittest
-import web
 
-# Pulp
 srcdir = os.path.abspath(os.path.dirname(__file__)) + "/../../src/"
 sys.path.insert(0, srcdir)
-
 commondir = os.path.abspath(os.path.dirname(__file__)) + '/../common/'
 sys.path.insert(0, commondir)
 
-
-from pulp.server.api.user import UserApi
 from pulp.server.api.consumer import ConsumerApi
-from pulp.server.api.repo import RepoApi
-from pulp.server.api.role import PermissionApi
-from pulp.server.api.role import RoleApi
-from pulp.server.db.model import Permission
-from pulp.server.db.model import RoleActionType
-from pulp.server.db.model import RoleResourceType
+from pulp.server.api.permission import PermissionAPI
+from pulp.server.api.role import RoleAPI
+from pulp.server.api.user import UserApi
+from pulp.server.auth import authentication, authorization, principal
 
-import pulp.server.auth.auth as auth
 import testutil
+
 
 class TestAuthorization(unittest.TestCase):
 
     def setUp(self):
+        prinicpal.set_principal(auth.SystemPrincipal())
+        authorization.check_builtin_roles()
         self.config = testutil.load_test_config()
-        self.uapi = UserApi()
-        self.capi = ConsumerApi()
-        self.roleapi = RoleApi()
-        self.repoapi = RepoApi()
-        self.userapi = UserApi()
-        self.permapi = PermissionApi()
-        # Make sure to reset the principal between tests
-        auth.set_principal(auth.SystemPrincipal())
+        self.perm_api = PermissionAPI()
+        self.role_api = RoleAPI()
+        self.user_api = UserApi()
+        self.alhpa_num = string.letters + string.digits
 
     def tearDown(self):
-        self.uapi.clean()
-        self.capi.clean()
-        self.roleapi.clean()
-        self.repoapi.clean()
-        self.permapi.clean()
+        self.perm_api.clean()
+        self.role_api.clean()
+        self.user_api.clean()
         testutil.common_cleanup()
 
-    def test_create_role(self):
-        # Create 2 roles so you check for proper unique ids
-        role = self.create_role('test-role')
-        role2 = self.create_role('test-role2')
-        self.assertTrue(role['name'])
-        self.assertTrue(role['description'])
-        self.assertTrue(role['action_types'])
-        self.assertTrue(role['resource_type'])
+    # test data generation
 
-    def test_edit_role(self):
-        role = self.create_role('edit-role')
-        role['description'] = 'updated desc'
-        self.roleapi.update(role)
-        updated = self.roleapi.role('edit-role')
-        self.assertEquals(updated['description'], 'updated desc')
-    
+    def _create_user(self):
+        username = random.sample(self.alhpa_num, random.randint(6, 10))
+        password = random.sample(self.alhpa_num, random.randint(6, 10))
+        return self.user_api.create(username, password, username, username)
+
+    def _create_role(self):
+        name = random.sample(self.alhpa_num, random.randint(6, 10))
+        return self.role_api.create(name)
+
+    def _create_resource(self):
+        return '/%s/' % '/'.join(random.sample(self.alhpa_num,
+                                               random.randint(6, 10))
+                                 for i in random.randint(1, 4))
+
+    # test individual user permissions
+
+    def test_user_create_failure(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.CREATE
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_user_create_success(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.CREATE
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_user_read_failure(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.READ
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_user_read_success(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_user_update_failure(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.UPDATE
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_user_update_success(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.UPDATE
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_user_delete_failure(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.DELETE
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_user_delete_success(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.DELETE
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_user_execute_failure(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.EXECUTE
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_user_execute_success(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.EXECUTE
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_user_permission_revoke(self):
+        u = self._create_user()
+        r = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(r, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+        authorization.revoke_permission_from_user(r, u['login'], [n])
+        self.assertFalse(authorization.is_authorized(r, u, o))
+
+    def test_parent_permissions(self):
+        u = self._create_user()
+        r = self._create_resource()
+        p = r.rsplit('/', 1) + '/'
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user(p, u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    def test_root_permissions(self):
+        u = self._create_user()
+        r = self._create_resource()
+        n = authorization.operation_to_name(o)
+        authorization.grant_permission_to_user('/', u['login'], [n])
+        self.assertTrue(authorization.is_authorized(r, u, o))
+
+    # test role management
+
+    def test_create_role(self):
+        n = 'create_role'
+        r1 = authorization.create_role(n)
+        r2 = self.role_api.role(n)
+        self.assertEquals(r1['_id'], r2['_id'])
+
     def test_delete_role(self):
-        role = self.create_role('delete-role')
-        self.assertTrue(self.roleapi.role('delete-role'))
-        self.roleapi.delete(role['name'])
-        r = self.roleapi.role('delete-role')
-        self.assertFalse(self.roleapi.role('delete-role'))
-        
-    def test_role_within_role(self):
-        role1 = self.create_role('root-role')
-        role2 = self.create_role('child-role')
-        role2['parent'] = role1
-        self.roleapi.update(role2)
-        
-        updated_root = self.roleapi.role('root-role')
-        
-        updated_child = self.roleapi.role('child-role')
-        self.assertEquals(updated_child['parent'], updated_root)
-        
-        
-    def test_create_permission(self):
-        role = self.create_role('perm-test-role')
-        repo = self.repoapi.create('perm-test-repo', 'perm-test', 'i386')
-        user = self.userapi.create('perm-test-user')
-        role = self.roleapi.add_instance(repo['id'], role['name'])
-        self.assertTrue(len(role['permissions']) > 0)
-        perm = role['permissions'][0]
-        self.assertTrue(perm)
-        self.assertTrue(perm['role_id'] == role['id'])
-        self.assertTrue(perm['instance'] == repo)
-        
-        perm = self.permapi.create_with_user(repo, user)
-        self.assertTrue(perm['user_login'] == user['login'])
-        
-    
-    def test_permission_constructor(self):
-        role = self.create_role('const-test')
-        repo = self.repoapi.create('const-test-repo', 'const-test', 'i386')
-        failed = False
-        try:
-            permission = Permission(repo)
-        except:
-            failed = True
-        self.assertTrue(failed)
-        
-    def test_add_user_role(self):
-        (user, repo, role) = self.create_bundle('read-test-role', 'read-test-user')
-        
-        self.roleapi.add_user(role, user)
-        self.assertTrue(len(user['roles'].keys()) > 0 )
-        self.assertTrue(len(role['users']) == 1)
-        
-    
-    # Check if a user has a role
-    def test_read_repo(self):
-        (user, repo, role) = self.create_bundle('read-test-role', 'read-test-user')
-        self.roleapi.add_user(role, user)
-        self.assertTrue(self.roleapi.check(user, repo, RoleResourceType.REPO,
-                                           RoleActionType.READ))
-        
-    
-    def create_bundle(self, rolename, username):
-        role = self.create_role(rolename)
-        repo = self.repoapi.create('perm-test-repo', 'perm-test', 'i386')
-        user = self.userapi.create('perm-test-user')
-        self.roleapi._add_instance(repo, role)
-        return (user, repo, role)  
-    
-    # Util Method
-    def create_role(self, name):
-        desc = 'test desc for role'
-        action_type = [RoleActionType.CREATE, RoleActionType.WRITE, RoleActionType.READ]
-        resource_type = RoleResourceType.REPO
-        role = self.roleapi.create(name, desc, action_type, resource_type)
-        return role
-    
-        
+        n = 'delete_role'
+        r1 = self.role_api.create(n)
+        self.assertFalse(r1 is None)
+        authorization.delete_role(n)
+        r2 = self.role_api.role(n)
+        self.assertTrue(r2 is None)
+
+    def test_add_user(self):
+        u = self._create_user()
+        r = self._create_role()
+        authorization.add_user_to_role(r['name'], u['login'])
+        user_names = [u['login'] for u in authorization.list_users_in_role(r['name'])]
+        self.assertTrue(u['login'] in user_names)
+
+    def test_remove_user(self):
+        u = self._create_user()
+        r = self._create_role()
+        authorization.add_user_to_role(r['name'], u['login'])
+        authorization.remove_user_from_role(r['name'], u['login'])
+        user_names = [u['login'] for u in authorization.list_users_in_role(r['name'])]
+        self.assertFalse(u['login'] in user_names)
+
+    # test role permissions
+
+    def test_role_create(self):
+        u1 = self._create_user()
+        u2 = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.CREATE
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u1['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u1, o))
+        self.assertFalse(authorization.is_authorized(s, u2, o))
+
+    def test_role_read(self):
+        u1 = self._create_user()
+        u2 = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u1['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u1, o))
+        self.assertFalse(authorization.is_authorized(s, u2, o))
+
+    def test_role_update(self):
+        u1 = self._create_user()
+        u2 = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.UPDATE
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u1['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u1, o))
+        self.assertFalse(authorization.is_authorized(s, u2, o))
+
+    def test_role_delete(self):
+        u1 = self._create_user()
+        u2 = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.DELETE
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u1['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u1, o))
+        self.assertFalse(authorization.is_authorized(s, u2, o))
+
+    def test_role_execute(self):
+        u1 = self._create_user()
+        u2 = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.EXECUTE
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u1['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u1, o))
+        self.assertFalse(authorization.is_authorized(s, u2, o))
+
+    def test_role_permission_revoke(self):
+        u = self._create_user()
+        r = self._create_role()
+        s = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r['name'], u['login'])
+        authorization.grant_permission_to_role(s, r['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u, o))
+        authorization.revoke_permissions_from_role(s, r['name'], [n])
+        self.assertFalse(authorization.is_authorized(s, u, o))
+
+    # test multi-role/permission interaction
+
+    def test_non_unique_permission_revoke(self):
+        u = self._create_user()
+        r1 = self._create_role()
+        r2 = self._create_role()
+        s = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r1['name'], u['login'])
+        authorization.add_user_to_role(r2['name'], u['login'])
+        authorization.grant_permission_to_role(s, r1['name'], [n])
+        authorization.grant_permission_to_role(s, r2['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u, o))
+        authorization.revoke_permissions_from_role(s, r1['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u, o))
+
+    def test_non_unique_permission_remove(self):
+        u = self._create_user()
+        r1 = self._create_role()
+        r2 = self._create_role()
+        s = self._create_resource()
+        o = authorization.READ
+        n = authorization.operation_to_name(o)
+        authorization.add_user_to_role(r1['name'], u['login'])
+        authorization.add_user_to_role(r2['name'], u['login'])
+        authorization.grant_permission_to_role(s, r1['name'], [n])
+        authorization.grant_permission_to_role(s, r2['name'], [n])
+        self.assertTrue(authorization.is_authorized(s, u, o))
+        authorization.remove_user_from_role(r1['name'], u['login'])
+        self.assertTrue(authorization.is_authorized(s, u, o))
+
+
 if __name__ == '__main__':
     logging.root.addHandler(logging.StreamHandler())
     logging.root.setLevel(logging.ERROR)
