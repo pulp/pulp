ece2a9ae8b3a93f29f86217a30bee404253576fd
Jason L Connor <jconnor@redhat.com>
2011-02-12 23:16:59 -0700

    ported package connection

--- src/pulp/client/api/package.py
+++ src/pulp/client/api/package.py
@@ -17,4 +17,38 @@ from pulp.client.api.base import PulpAPI
 
 
 class PackageAPI(PulpAPI):
-    pass
+
+    def clean(self):
+        path = "/packages/"
+        return self.server.DELETE(path)
+
+    def create(self, name, epoch, version, release, arch, description,
+            checksum_type, checksum, filename):
+        path = "/packages/"
+        repodata = {"name"   : name,
+                    "epoch" : epoch,
+                    "version" : version,
+                    "release" : release,
+                    "arch" : arch,
+                    "description" : description,
+                    "checksum_type" : checksum_type,
+                    "checksum": checksum,
+                    "filename": filename, }
+        return self.server.PUT(path, repodata)
+
+    def packages(self):
+        path = "/packages/"
+        return self.server.GET(path)
+
+    def package(self, id, filter=None):
+        path = "/packages/%s/" % id
+        return self.server.GET(path)
+
+    def delete(self, packageid):
+        path = "/packages/%s/" % packageid
+        return self.server.DELETE(path)
+
+    def package_by_ivera(self, name, version, release, epoch, arch):
+        path = "/packages/%s/%s/%s/%s/%s/" % (name, version, release, epoch, arch)
+        return self.server.GET(path)
+
