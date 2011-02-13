5398513831e677a2d52c621faee6ebf3dc2506a2
Jason L Connor <jconnor@redhat.com>
2011-02-12 22:34:56 -0700

    ported cds connection

--- src/pulp/client/api/cds.py
+++ src/pulp/client/api/cds.py
@@ -17,4 +17,65 @@ from pulp.client.api.base import PulpAPI
 
 
 class CDSAPI(PulpAPI):
-    pass
+    '''
+    Connection class to the CDS APIs.
+    '''
+    def cds(self, hostname):
+        path = '/cds/%s/' % hostname
+        return self.server.GET(path)
+
+    def register(self, hostname, name=None, description=None):
+        data = {'hostname': hostname,
+                'name': name,
+                'description': description}
+        path = '/cds/'
+        return self.server.PUT(path, data)
+
+    def unregister(self, hostname):
+        path = '/cds/%s/' % hostname
+        return self.server.DELETE(path)
+
+    def list(self):
+        path = '/cds/'
+        return self.server.GET(path)
+
+    def history(self, hostname,
+                event_type=None,
+                limit=None,
+                sort=None,
+                start_date=None,
+                end_date=None):
+
+        data = {}
+        if event_type:
+            data['event_type'] = event_type
+        if limit:
+            data['limit'] = limit
+        if sort:
+            data['sort'] = sort
+        if start_date:
+            data['start_date'] = start_date
+        if end_date:
+            data['end_date'] = end_date
+
+        path = '/cds/%s/history/' % hostname
+        return self.server.POST(path, data)
+
+    def associate(self, hostname, repo_id):
+        data = {'repo_id' : repo_id}
+        path = '/cds/%s/associate/' % hostname
+        return self.server.POST(path, data)
+
+    def unassociate(self, hostname, repo_id):
+        data = {'repo_id' : repo_id}
+        path = '/cds/%s/unassociate/' % hostname
+        return self.server.POST(path, data)
+
+    def sync(self, hostname):
+        data = {}
+        path = '/cds/%s/sync/' % hostname
+        return self.server.POST(path, data)
+
+    def sync_list(self, hostname):
+        path = '/cds/%s/sync/' % hostname
+        return self.server.GET(path)
