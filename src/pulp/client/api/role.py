1e0dc790a9cbc811ba1de4ce214e3c38674d5b5b
Jason L Connor <jconnor@redhat.com>
2011-02-12 23:37:58 -0700

    ported role connection

--- src/pulp/client/api/role.py
+++ src/pulp/client/api/role.py
@@ -14,7 +14,50 @@
 # in this software or its documentation.
 
 from pulp.client.api.base import PulpAPI
+from pulp.client.server.base import ServerRequestError
 
 
 class RoleAPI(PulpAPI):
-    pass
+
+    def list(self):
+        path = '/roles/'
+        return self.server.GET(path)
+
+    def info(self, rolename):
+        path = '/roles/%s/' % rolename
+        return self.server.GET(path)
+
+    def create(self, rolename):
+        path = '/roles/'
+        params = {'rolename': rolename}
+        try:
+            return self.server.PUT(path, params)
+        except ServerRequestError, e:
+            print e.args[1]
+            return False
+
+    def delete(self, rolename):
+        path = '/roles/%s/' % rolename
+        try:
+            return self.server.DELETE(path)
+        except ServerRequestError, e:
+            print e.args[1]
+            return False
+
+    def add_user(self, rolename, username):
+        path = '/roles/%s/add/' % rolename
+        params = {'username': username}
+        try:
+            return self.server.POST(path, params)
+        except ServerRequestError, e:
+            print e.args[1]
+            return False
+
+    def remove_user(self, rolename, username):
+        path = '/roles/%s/remove/' % rolename
+        params = {'username': username}
+        try:
+            return self.server.POST(path, params)
+        except ServerRequestError, e:
+            print e.args[1]
+            return False
