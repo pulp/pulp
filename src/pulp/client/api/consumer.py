980485aa1fa2c9cb30853e094dbc5649926550f6
Jason L Connor <jconnor@redhat.com>
2011-02-14 21:50:21 -0700

    fixed consumer port

--- src/pulp/client/api/consumer.py
+++ src/pulp/client/api/consumer.py
@@ -16,39 +16,131 @@
 from pulp.client.api.base import PulpAPI
 
 
+consumer_deferred_fields = ('package_profile', 'repoids')
+
+
 class ConsumerAPI(PulpAPI):
     """
-    Connection class to access consumer group related calls
+    Connection class to access repo specific calls
     """
-    def create(self, login, password=None, name=None):
-        user_data = {"login": login,
-                     "password": password,
-                     "name": name}
-        path = "/users/"
-        return self.server.PUT(path, user_data)[1]
+    def create(self, id, description, key_value_pairs={}):
+        consumerdata = {"id": id,
+                        "description": description,
+                        "key_value_pairs": key_value_pairs}
+        path = "/consumers/"
+        return self.server.PUT(path, consumerdata)[1]
 
-    def update(self, user):
-        path = "/users/%s/" % user['id']
-        return self.server.PUT(path, user)[1]
+    def update(self, consumer):
+        path = "/consumers/%s/" % consumer['id']
+        return self.server.PUT(path, consumer)[1]
 
-    def delete(self, **kwargs):
-        login = kwargs['login']
-        path = "/users/%s/" % login
+    def bulkcreate(self, consumers):
+        path = "/consumers/bulk/"
+        return self.server.POST(path, consumers)[1]
+
+    def delete(self, id):
+        path = "/consumers/%s/" % id
         return self.server.DELETE(path)[1]
 
     def clean(self):
-        path = "/users/"
+        path = "/consumers/"
         return self.server.DELETE(path)[1]
 
-    def users(self):
-        path = "/users/"
+    def consumer(self, id):
+        path = "/consumers/%s/" % str(id)
+        consumer = self.server.GET(path)[1]
+        for field in consumer_deferred_fields:
+            consumer[field] = self.server.GET('%s%s/' % (path, field))[1]
+        return consumer
+
+    def packages(self, id):
+        path = "/consumers/%s/packages/" % str(id)
         return self.server.GET(path)[1]
 
-    def user(self, login):
-        path = "/users/%s/" % str(login)
+    def certificate(self, id):
+        path = "/consumers/%s/certificate/" % str(id)
+        cert_dict = self.server.GET(path)
+        return cert_dict
+
+    def consumers(self):
+        path = "/consumers/"
+        consumers = self.server.GET(path)[1]
+        for c in consumers:
+            consumer_link = "/consumers/%s/" % str(c["id"])
+            c['repoids'] = self.server.GET('%s%s/' % (consumer_link, 'repoids'))[1]
+        return consumers
+
+    def consumers_with_package_name(self, name):
+        path = '/consumers/?package_name=%s' % name
         return self.server.GET(path)[1]
 
-    def admin_certificate(self):
-        path = '/users/admin_certificate/'
+    def bind(self, id, repoid):
+        path = "/consumers/%s/bind/" % id
+        return self.server.POST(path, repoid)[1]
+
+    def unbind(self, id, repoid):
+        path = "/consumers/%s/unbind/" % id
+        return self.server.POST(path, repoid)[1]
+
+    def add_key_value_pair(self, id, key, value):
+        key_value_dict = {'key' : key, 'value' : value}
+        path = "/consumers/%s/add_key_value_pair/" % id
+        return self.server.POST(path, key_value_dict)[1]
+
+    def delete_key_value_pair(self, id, key):
+        path = "/consumers/%s/delete_key_value_pair/" % id
+        return self.server.POST(path, key)[1]
+
+    def update_key_value_pair(self, id, key, value):
+        key_value_dict = {'key' : key, 'value' : value}
+        path = "/consumers/%s/update_key_value_pair/" % id
+        return self.server.POST(path, key_value_dict)[1]
+
+    def get_keyvalues(self, id):
+        path = "/consumers/%s/keyvalues/" % str(id)
         return self.server.GET(path)[1]
 
+    def profile(self, id, profile):
+        path = "/consumers/%s/profile/" % id
+        return self.server.POST(path, profile)[1]
+
+    def installpackages(self, id, packagenames, when=None):
+        path = "/consumers/%s/installpackages/" % id
+        body = dict(packagenames=packagenames, scheduled_time=when)
+        return self.server.POST(path, body)[1]
+
+    def installpackagegroups(self, id, packageids, when=None):
+        path = "/consumers/%s/installpackagegroups/" % id
+        body = dict(packageids=packageids, scheduled_time=when)
+        return self.server.POST(path, body)[1]
+
+    def installpackagegroupcategories(self, id, repoid, categoryids, when=None):
+        path = "/consumers/%s/installpackagegroupcategories/" % id
+        body = dict(categoryids=categoryids, repoid=repoid, scheduled_time=when)
+        return self.server.POST(path, body)[1]
+
+    def errata(self, id, types=None):
+        path = "/consumers/%s/listerrata/" % id
+        body = dict(types=types)
+        return self.server.POST(path, body)[1]
+
+    def package_updates(self, id):
+        path = "/consumers/%s/package_updates/" % id
+        return self.server.GET(path)[1]
+
+    def errata_package_updates(self, id):
+        path = "/consumers/%s/errata_package_updates/" % id
+        return self.server.GET(path)[1]
+
+    def installerrata(self, id, errataids, assumeyes=False, types=(), when=None):
+        erratainfo = {'consumerid': id,
+                      'errataids': errataids,
+                      'types':   types,
+                      'assumeyes': assumeyes,
+                      'scheduled_time': when}
+        path = "/consumers/%s/installerrata/" % id
+        return self.server.POST(path, erratainfo)[1]
+
+    def history(self, id, query_params):
+        path = "/consumers/%s/history/" % id
+        return self.server.POST(path, query_params)[1]
