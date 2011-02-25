9cbc7fdc419c650c65cfe5c34afe1d0721c7b4f1
Pradeep Kilambi <pkilambi@redhat.com>
2011-02-25 10:38:41 -0500

    strip any white spaces while reading the csv entries

--- src/pulp/client/utils.py
+++ src/pulp/client/utils.py
@@ -288,6 +288,7 @@ def parseCSV(filepath):
     reader = csv.reader(in_file)
     lines = []
     for line in reader:
+        line = [l.strip() for l in line]
         lines.append(line)
     in_file.close()
     return lines
