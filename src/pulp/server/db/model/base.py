50080eb849ce9736e435d40b5c810ff1b63280a4
Jason L Connor <jconnor@redhat.com>
2011-02-27 22:55:45 -0700

    new doc string and more comments

--- src/pulp/server/db/model/base.py
+++ src/pulp/server/db/model/base.py
@@ -23,10 +23,22 @@ from pulp.server.db.connection import get_database
 
 
 class Model(dict):
-    '''
-    Model object that has convenience methods to get and put
-    attrs into the base dict object with dot notation
-    '''
+    """
+    Model base class
+
+    Derived model classes are the representation of persistent data used by
+    pulp and are abstractions of the documents used by mongodb. These classes 
+    are used to create new documents to be stored in a document collection.
+
+    The model base class is derived from the builtin dictionary, and should be 
+    used as such by code after instantiation. It provides a mechanism to use 
+    dot notation instead of the usual dictionary key lookup. However, this is 
+    provided for convenience when declaring fields in the constructors, and 
+    should not be used by code. Documents that are retrieved from a document  
+    collection are also derivatives of dictionaries, but are not derivatives 
+    of the Model class. To ensure interchangability, make sure to use python's
+    regular dictionary key lookup when using Model instances.
+    """
 
     # The model class will know how to fetch the document collection used to
     # store the models in database. If you want a document collection to be
@@ -44,22 +56,26 @@ class Model(dict):
     # The other_indices field is only a tuple listing other model fields to be
     # indexed in the collection, but that do not need to be individually unique
     # or form unique sets of values.
+
     collection_name = None
     unique_indicies = ('id',) # note, '_id' is automatically unique and indexed
     other_indicies = ()
+
+    # -------------------------------------------------------------------------
 
     def __init__(self):
         self._id = str(uuid.uuid4())
         self.id = self._id
 
-    # dict to dot-notation mapping methods ------------------------------------
+    # dict to dot-notation mapping methods
+    # XXX only for use in constructors
 
     def __getattr__(self, attr):
         return self.get(attr, None)
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-    # class connection methods ------------------------------------------------
+    # database collection methods ---------------------------------------------
 
     @classmethod
     def _get_collection_from_db(cls):
@@ -70,9 +86,12 @@ class Model(dict):
             for index in indicies:
                 if isinstance(index, basestring):
                     index = (index,)
+                # we're using descending ordering for the arbitrary case,
+                # if you need a particular ordering, override the 
+                # _get_collection_from_db method
                 collection.ensure_index([(i, DESCENDING) for i in index],
                                         unique=unique, background=True)
-
+        # create the collection and ensure the unique and other indicies
         db = get_database()
         if db is None:
             msg = _('Cannot get collection from uninitialized database')
@@ -97,7 +116,8 @@ class Model(dict):
         @return: the document collection if associated with one, None otherwise
         """
         # not all data models are associated with a document collection
-        # provide mechanism for sub-documents
+        # provide mechanism for sub-documents by not defining the
+        # collection_name
         if cls.collection_name is None:
             return None
         # see if we have the collection cached
