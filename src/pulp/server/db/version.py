36fa69df2d354462f00d7d659d4ff4e4cbce63f9
Jay Dobies <jason.dobies@redhat.com>
2011-03-21 15:12:11 -0400

    Upped to version 7

--- src/pulp/server/db/version.py
+++ src/pulp/server/db/version.py
@@ -23,7 +23,7 @@ from pulp.server.db.model import Model
 
 # current data model version of the code base
 # increment this if you change the data model
-VERSION = 6
+VERSION = 7
 
 # this isn't anything
 _version_db = None
