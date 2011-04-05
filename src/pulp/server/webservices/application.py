b398f74e974daaf2a619bc7d84b2a80159cce048
Jason L Connor <jconnor@redhat.com>
2011-04-05 10:21:00 -0600

    updating header comment and copyright

--- src/pulp/server/webservices/application.py
+++ src/pulp/server/webservices/application.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2010 Red Hat, Inc.
+# Copyright © 2010-2011 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public License,
 # version 2 (GPLv2). There is NO WARRANTY for this software, express or
