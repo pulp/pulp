841b7b9894136d98f174a4eb0f942404dd0c9e4f
Sayli Karmarkar <skarmark@redhat.com>
2011-04-21 02:08:41 -0700

    Revert "Reverting fix for bug 695707 to move it from client side to server side"
    This reverts commit cfdbaeb9e4e9da364b587998680419147531b724.

--- src/pulp/client/core/repo.py
+++ src/pulp/client/core/repo.py
@@ -567,6 +567,11 @@ class Delete(RepoAction):
         id = self.get_required_option('id')
         self.get_repo(id)
 
+        # Check for latest sync task status to see if there is repo sync running for this repo
+        tasks = self.repository_api.sync_list(id)
+        if tasks and tasks[0]['state'] in ('waiting', 'running'):
+            system_exit(os.EX_OK, _("Repo [ %s ] cannot be deleted because of sync in progress. You can cancel ongoing sync using 'repo cancel_sync' command.") % id)
+
         cds_unassociate_succeeded, cds_unassociate_failed = \
             self.repository_api.delete(id=id)
         print _("Successful deleted repository [ %s ]") % id
