e580fc13f64c1a3b5eca4ae01f839a922caf3ae8
Jeff Ortel <jortel@redhat.com>
2011-06-09 11:12:11 -0500

    DB version 16, taken on master.

--- src/pulp/server/db/migrate/versions/seventeen.py
+++ src/pulp/server/db/migrate/versions/seventeen.py
@@ -18,7 +18,7 @@ from pulp.server.db.model import Consumer, Repo
 _log = logging.getLogger('pulp')
 
 
-version = 16
+version = 17
 
 def migrate():
     _log.info('migration to data model version %d started' % version)
