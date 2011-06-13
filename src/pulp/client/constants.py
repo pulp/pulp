a90ac112a27bb06e001eb69e7395987e3a8af422
Jay Dobies <jason.dobies@redhat.com>
2011-06-13 10:38:06 -0400

    Renamed group to cluster in CLI output

--- src/pulp/client/constants.py
+++ src/pulp/client/constants.py
@@ -174,7 +174,7 @@ CDS_INFO = '''
 Name                \t%-25s
 Hostname            \t%-25s
 Description         \t%-25s
-Group               \t%-25s
+Cluster             \t%-25s
 Sync Schedule       \t%-25s
 Repos               \t%-25s
 Last Sync           \t%-25s
