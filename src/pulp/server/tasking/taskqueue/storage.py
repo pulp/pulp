42273d032c45fc07b4da9a539f4996f807a8742c
Jason L Connor <jconnor@redhat.com>
2011-08-02 15:13:57 -0600

    727666 - fixed unpickling of private methods

--- src/pulp/server/tasking/taskqueue/storage.py
+++ src/pulp/server/tasking/taskqueue/storage.py
@@ -179,8 +179,12 @@ def _pickle_method(method):
 def _unpickle_method(func_name, obj, cls):
     func = None
     for cls in cls.mro():
+        # handle public, protected, and private method names
+        lookup = func_name
+        if func_name.starts_with('__'):
+            lookup = '_%s%s' % (cls.__name__, func_name)
         try:
-            func = cls.__dict__[func_name]
+            func = cls.__dict__[lookup]
         except KeyError:
             pass
         else:
