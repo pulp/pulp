7905850653801329295ee79f964439a3075d6ba2
Jason L Connor <jconnor@redhat.com>
2011-08-04 02:59:35 -0600

    changed plugin manager to use hard-coded conventions changed conflicting plugin behavior to raise and exception isntead of just log added copying of config files instead of returning reference added enabled/disabled detection for plugins

--- src/pulp/server/content/manager.py
+++ src/pulp/server/content/manager.py
@@ -12,6 +12,7 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import copy
+import itertools
 import logging
 import os
 import re
@@ -20,6 +21,8 @@ from gettext import gettext as _
 
 from pulp.server import config
 from pulp.server.content.distributor.base import Distributor
+from pulp.server.content.exception import (
+    ConflictingPluginError, PluginNotFoundError)
 from pulp.server.content.importer.base import Importer
 from pulp.server.content.module import import_module
 
@@ -33,6 +36,14 @@ _top_level_config_dir = '/etc/pulp'
 _importers_config_dir = os.path.join(_top_level_config_dir, 'importers')
 _distributors_config_dir = os.path.join(_top_level_config_dir, 'distributors')
 
+_top_level_plugin_dir = os.path.dirname(__file__)
+_importers_plugin_dir = os.path.join(_top_level_plugin_dir, 'importers')
+_distributors_plugin_dir = os.path.join(_top_level_plugin_dir, 'distributors')
+
+_top_level_plugins_package = 'pulp.server.content'
+_importers_plugins_package = '.'.join((_top_level_plugins_package, 'importers'))
+_distributor_plugins_package = '.'.join((_top_level_plugins_package, 'distributors'))
+
 # manager class ----------------------------------------------------------------
 
 class Manager(object):
@@ -41,164 +52,158 @@ class Manager(object):
     plugin with content types.
     """
     def __init__(self):
-        self.importer_paths = {}
-        self.distributor_paths = {}
-        self.importers = {}
-        self.distributors = {}
-        self.configured_importers = self.__configured_importers()
-        self.configured_distributors = self.__configured_distributors()
+        self.importer_config_paths = []
+        self.importer_plugin_paths = {}
+        self.distributor_config_paths = []
+        self.distributor_plugin_paths = {}
+
         self.importer_configs = {}
+        self.importer_plugins = {}
         self.distributor_configs = {}
+        self.distributor_plugins = {}
 
-    def __configured_importers(self):
-        cfg = {}
-        if not config.config.has_section('importers'):
-            return cfg
-        for content_type in config.config.options('importers'):
-            cfg[content_type] = config.config.getboolean('importers', content_type)
-        return cfg
-
-    def __configured_distributors(self):
-        cfg = {}
-        if not config.config.has_section('distributors'):
-            return cfg
-        for distribution_type in config.config.options('distributors'):
-            cfg[distribution_type] = config.config.getboolean('distributors', distribution_type)
-        return cfg
-
-    def add_importer_path(self, path, package_name=None):
+    def add_importer_config_path(self, path):
         """
-        Add a path for the importer modules.
-        @type path: str
-        @param path: filesystem path to python package containing importer modules
-        @type package_name: None or str
-        @param package_name: optional fully qualified package name, defaults to ''
-        @raises ValueError: if the path is not found or cannot be read
+        """
+        if not os.access(path, os.F_OK | os.R_OK):
+            raise ValueError(_('Cannot find path %s') % path)
+        self.importer_config_paths.append(path)
+
+    def add_importer_plugin_path(self, path, package_name=None):
+        """
         """
         if not os.access(path, os.F_OK | os.R_OK):
             raise ValueError(_('Cannot find path %s') % path)
         self.importer_paths[path] = package_name or ''
 
-    def add_distributor_path(self, path, package_name=None):
+    def add_distributor_config_path(self, path):
         """
-        Add a path for the distributor modules.
-        @type path: str
-        @param path: filesystem path to python package containing distributor modules
-        @type package_name: None or str
-        @param package_name: optional fully qualified package name, defaults to ''
-        @raises ValueError: if the path is not found or cannot be read
+        """
+        if not os.access(path, os.F_OK | os.R_OK):
+            raise ValueError(_('Cannot find path %s') % path)
+        self.distributor_config_paths.append(path)
+
+    def add_distributor_plugin_path(self, path, package_name=None):
+        """
         """
         if not os.access(path, os.F_OK | os.R_OK):
             raise ValueError(_('Cannot find path %s') % path)
         self.distributor_paths[path] = package_name or ''
 
-    def _load_modules(self, paths, skip=None):
+    def _load_configs(self, config_paths):
         """
-        Helper method that loads modules from the given package paths.
-        @type paths: dict
-        @param path: a mapping of package directory paths to package names
-        @type skip: tuple or list
-        @param skip: list of module names to *not* load
-        @rtype: list of modules
-        @return: list of all modules in all packages in the paths
         """
-        assert isinstance(paths, dict)
+        configs = {}
+        files_regex = re.compile('.*\.conf$')
+        for path in config_paths:
+            files = os.listdir(path)
+            for file_name in filter(files_regex.match, files):
+                name = file_name.rsplit('.', 1)[0]
+                if name in configs:
+                    raise ConflictingPluginError(_('More than one configuration file found for %s') % name)
+                parser = SafeConfigParser()
+                parser.read(os.path.join(path, file_name))
+                configs[name] = parser
+        return configs
+
+    def _load_plugins(self, plugin_paths, skip=None):
+        """
+        """
         skip = skip or ('__init__',)
-        files_regex = re.compile('(?!(%s)).*\.py$') % '|'.join(skip)
+        files_regex = re.compile('(?!(%s))\.py$') % '|'.join(skip)
         modules = []
         for path, package_name in paths.items():
             files = os.listdir(path)
             for file_name in filter(files_regex.match, files):
-                name = '.'.join((package_name, file_name.rsplit('.', 1)[0]))
-                module = import_module(name)
+                name = file_name.rsplit('.', 1)[0]
+                module_name = '.'.join((package_name, name))
+                module = import_module(module_name)
                 modules.append(module)
         return modules
+
+    def _is_plugin_enabled(self, config):
+        """
+        Grok through a config parser and see if the plugin is not disabled.
+        @type config: SafeConfigParser instance
+        @param config: plugin config
+        @rtype: bool
+        @return: True if the plugin is enabled, False otherwise
+        """
+        if config is None:
+            return True
+        if not config.has_section(plugin_name):
+            return True
+        if not config.has_option(plugin_name, 'enabled'):
+            return True
+        return config.getboolean(plugin_name, 'enabled')
 
     def load_importers(self):
         """
         Load all importer modules and associate them with their supported types.
         """
         assert not self.importers
-        modules = self._load_modules(self.importer_paths, ('__init__', 'base'))
+        configs = self._load_configs(self.importer_config_paths)
+        modules = self._load_modules(self.importer_plugin_paths, ('__init__', 'base'))
         for module in modules:
+            config = configs.get(module.__name__, None)
+            if not self._is_plugin_enabled(config):
+                continue
             for attr in dir(module):
                 if not issubclass(attr, Importer):
                     continue
-                cfg = SafeConfigParser()
-                cfg.read(attr.config_files)
                 for content_type in attr.types:
                     if content_type in self.importers:
-                        _log.warn(_('Importer for %s already found, not loading %s') %
-                                  (content_type, repr(attr)))
-                        continue
-                    self.importers[content_type] = attr
-                    self.importer_configs[content_type] = cfg
+                        raise ConflictingPluginError(_('Duplicate importer %s for content type %s found') %
+                                                     (attr.__name__, content_type))
+                    self.importer_plugins[content_type] = attr
+                    self.importer_configs[content_type] = config or SafeConfigParser()
 
     def load_distributors(self):
         """
         Load all distributor modules and associate them with their supported types.
         """
         assert not self.distributors
-        modules = self._load_modules(self.distributor_paths, ('__init__', 'base'))
+        configs = self._load_configs(self.distributor_config_paths)
+        modules = self._load_modules(self.distributor_plugin_paths, ('__init__', 'base'))
         for module in modules:
+            config = configs.get(module.__name__, None)
+            if not self._is_plugin_enabled(config):
+                continue
             for attr in dir(module):
                 if not issubclass(attr, Distributor):
                     continue
-                cfg = SafeConfigParser()
-                cfg.read(attr.config_files)
                 for distribution_type in attr.types:
                     if distribution_type in self.distributors:
-                        _log.warn(_('Distributor for %s already found, not loading %s') %
-                                  (distribution_type, repr(attr)))
-                        continue
-                    self.distributors[distribution_type] = attr
-                    self.distributor_configs[distribution_type] = cfg
+                        raise ConflictingPluginError(_('Duplicate distributor %s for distribution type %s found') %
+                                                     (attr.__name__, distribution_type))
+                    self.distributor_plugins[distribution_type] = attr
+                    self.distributor_configs[distribution_type] = config or SafeConfigParser()
 
     def lookup_importer_class(self, content_type):
         """
-        Retrieve an importer class associated with the given content type.
-        @type content_type: str
-        @param content_type: content type label
-        @rtype: Importer class or None
-        @return: importer class associated with the the content type or None if
-                 no associated importer class is found
         """
-        if not self.configured_importers.get(content_type, True):
-            return None
-        return self.importers.get(content_type, None)
+        return self.importer_plugins.get(content_type, None)
 
     def lookup_importer_config(self, content_type):
         """
-        Return the (potentially empty) importer config for the given content type.
-        @type content_type: str
-        @param content_type: content type to get importer config for
-        @rtype: SafeConfigParser instance
-        @return: importer config for given content type
         """
-        return self.importer_configs.get(content_type, SafeConfigParser())
+        config = self.importer_configs.get(content_type, None)
+        if config is None:
+            return None
+        return copy.deepcopy(config)
 
     def lookup_distributor_class(self, distribution_type):
         """
-        Retrieve a distributor class associated with the given distribution type.
-        @type distribution_type: str
-        @param distribution_type: content type label
-        @rtype: Distributor class or None
-        @return: distributor class associated with the the distribution type or
-                 None if no associated distributor class is found
         """
-        if not self.configured_distributors.get(distribution_type, True):
-            return None
-        return self.distributors.get(distribution_type, None)
+        return self.distributor_plugins.get(distribution_type, None)
 
     def lookup_distributor_config(self, distributor_type):
         """
-        Return the (potentially empty) distributor config for the given content type.
-        @type distributor_type: str
-        @param distributor_type: content type to get distributor config for
-        @rtype: SafeConfigParser instance
-        @return: distributor config for given content type
         """
-        return self.distributor_configs.get(distributor_type, SafeConfigParser())
+        config = self.distributor_configs.get(distributor_type, None)
+        if config is None:
+            return None
+        return copy.deepcopy(config)
 
 # manager api ------------------------------------------------------------------
 
@@ -207,11 +212,13 @@ def _create_manager():
     _manager = Manager()
 
 
-def _add_paths(importer_paths, distributor_paths):
-    for p, n in importer_paths.items():
-        _manager.add_importer_path(p, n)
-    for p, n in distributor_paths.items():
-        _manager.add_distributor_path(p, n)
+def _add_paths():
+    _manager.add_importer_config_path(_importers_config_dir)
+    _manager.add_importer_plugin_path(_importers_plugin_dir,
+                                      _importers_plugins_package)
+    _manager.add_distributor_config_path(_distributors_config_dir)
+    _manager.add_distributor_plugin_path(_distributors_plugin_dir,
+                                         _distributor_plugins_package)
 
 
 def _load_plugins():
@@ -228,14 +235,8 @@ def initialize():
     # control flows on startup
     global _manager
     assert _manager is None
-    local_path = os.path.dirname(__file__)
-    # TODO configurable plugin directories
-    importer_paths = {os.path.join(local_path, 'importer'):
-                      'pulp.server.content.importer'}
-    distributor_paths = {os.path.join(local_path, 'distributor'):
-                         'pulp.server.content.distributor'}
     _create_manager()
-    _add_paths(importer_paths, distributor_paths)
+    _add_paths()
     _load_plugins()
 
 
