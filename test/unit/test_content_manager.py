4a3e30a55bf358fee419658a1ad0e65c16d504c1
Jason L Connor <jconnor@redhat.com>
2011-08-22 22:24:45 -0600

    added data generation and importer loading test

--- test/unit/test_content_manager.py
+++ test/unit/test_content_manager.py
@@ -23,6 +23,117 @@ import testutil
 
 from pulp.server.content import manager
 
+# test data and data generation api --------------------------------------------
+
+excellent_importer = '''
+from pulp.server.content.importer import base
+class ExcellentImporter(base.Importer):
+    @classmethod
+    def metadata(cls):
+        data = {'name': 'Excellent',
+                'version': 1,
+                'types': ['excellent_type'],
+                'conf_file': 'excellent.conf'}
+        return data
+'''
+
+less_excellent_importer = '''
+from pulp.server.content.importer import base
+class LessExcellentImporter(base.Importer):
+    @classmethod
+    def metadata(cls):
+        data = {'name': 'Excellent',
+                'version': 0,
+                'types': ['excellent_type']}
+        return data
+'''
+
+bogus_importer_1 = '''
+from pulp.server.content.importer import base
+class BogusOneImporter(base.Importer):
+    @classmethod
+    def metadata(cls):
+        return {}
+'''
+
+bogus_importer_2 = '''
+from pulp.server.content.importer import base
+class BogusTwoImporter(base.Importer):
+    @classmethod
+    def metadata(cls):
+        return None
+'''
+
+excellent_importer_config_1 = '''
+[ExcellentImporter]
+enabled = true
+'''
+
+excellent_importer_config_2 = '''
+[ExcellentImporter]
+enabled = false
+'''
+
+http_distibutor = '''
+from pulp.server.content.distributor import base
+class HTTPDistributor(base.Distributor):
+    @classmethod
+    def metadata(cls):
+        data = {'name': 'HTTP',
+                'version': 1.1,
+                'types': ['http', 'https'],
+                'conf_file': 'http.conf'}
+        return data
+'''
+
+http_conf = '''
+[HTTPDistributor]
+enabled: yes
+'''
+
+def gen_excellent_importer(enabled=True):
+    path = tempfile.mkdtemp()
+    mod_handle = open(os.path.join(path, 'excellent.py'), 'w')
+    mod_handle.write(excellent_importer)
+    mod_handle.close()
+    cfg_handle = open(os.path.join(path, 'excellent.conf'), 'w')
+    if enabled:
+        cfg_handle.write(excellent_importer_config_1)
+    else:
+        cfg_handle.write(excellent_importer_config_2)
+    cfg_handle.close()
+    return path
+
+def gen_less_excellent_importer():
+    path = tempfile.mkdtemp()
+    mod_handle = open(os.path.join(path, 'less.py'), 'w')
+    mod_handle.write(less_excellent_importer)
+    mod_handle.close()
+    return path
+
+def gen_bogus_importer(version=1):
+    path = tempfile.mkdtemp()
+    handle = open(os.path.join(path, 'bogus_%d.py' % version), 'w')
+    if version == 1:
+        handle.write(bogus_importer_1)
+    elif version == 2:
+        handle.write(bogus_importer_2)
+    else:
+        raise Exception('Are you kidding me?')
+    handle.close()
+    return path
+
+def gen_http_distributor():
+    path = tempfile.mkdtemp()
+    mod_handle = open(os.path.join(path, 'http.py'), 'w')
+    mod_handle.write(http_distibutor)
+    mod_handle.close()
+    cfg_handle = open(os.path.join(path, 'http.conf'), 'w')
+    cfg_handle.write(http_conf)
+    cfg_handle.close()
+    return path
+
+# unit tests -------------------------------------------------------------------
 
 class ManagerInstanceTest(testutil.PulpTest):
 
@@ -63,4 +174,12 @@ class ManagerPathTest(testutil.PulpTest):
 
 
 class ManagerLoadTest(ManagerPathTest):
-    pass
+
+    def test_enabled_excellent_importer(self):
+        path = gen_excellent_importer(enabled=True)
+        self.manager.add_importer_plugin_path(path)
+        self.manager.add_importer_config_path(path)
+        self.manager.load_importers()
+        self.assertTrue('ExcellentImporter' in self.manager.importer_plugins)
+        self.assertTrue('ExcellentImporter' in self.manager.importer_configs)
+        shutil.rmtree(path)
