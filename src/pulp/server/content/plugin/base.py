78791f9d177b7c75971cc1f7d0fc856ab0213ef7
Jason L Connor <jconnor@redhat.com>
2011-08-29 17:25:17 -0600

    reworked plugin base class and decorator for json config handling

--- src/pulp/server/content/plugin/base.py
+++ src/pulp/server/content/plugin/base.py
@@ -12,49 +12,48 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import copy
-import itertools
-from ConfigParser import SafeConfigParser
 
 from pulp.server.compat import wraps
 
+# content plugin base class ----------------------------------------------------
 
 class ContentPlugin(object):
+    """
+    Base plugin class for the generic content system
+    """
 
     def __init__(self, config):
-        self._config = config
-        self._method_config = None
+        assert isinstance(config, dict)
+        self.__config = config
         self.__current_config = None
 
     @property
     def config(self):
-        def _merge_configs(orig, override):
-            config = SafeConfigParser()
-            for section in set(itertools.chain(orig.sections(), override.sections())):
-                config.add_section(section)
-                for option in set(itertools.chain(orig.options(section), override.options(section))):
-                    value = None
-                    if override.has_option(section, option):
-                        value = override.get(section, option)
-                    else:
-                        value = orig.get(section, option)
-                    config.set(section, option, value)
-            return config
-
-        if self.__current_config is not None:
-            return self.__current_config
-        if self._method_config is None:
-            self.__current_config = self._config
-        else:
-            self.__current_config = _merge_configs(self._config, self._method_config)
+        if self.__current_config is None:
+            return self.__config
         return self.__current_config
+
+    def set_current_config(self, config):
+        assert isinstance(config, dict)
+        current_config = copy.copy(self.__config)
+        current_config.update(config)
+        self.__current_config = current_config
+
+    def unset_current_config(self):
+        self.__current_config = None
 
     @classmethod
     def metadata(cls):
         return {}
 
+# config override decorator ----------------------------------------------------
 
-def config_override(method):
+def allow_config_override(method):
     @wraps(method)
     def _config_override_decorator(self, *args, **kwargs):
-        return method(self, *args, **kwargs)
+        override_config = kwargs.get('config', {})
+        self.set_current_config(override_config)
+        return_value = method(self, *args, **kwargs)
+        self.unset_current_config()
+        return return_value
     return _config_override_decorator
