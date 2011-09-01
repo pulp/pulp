1b42ee3e6717f5127084ef6bc37942721612203a
Jay Dobies <jason.dobies@redhat.com>
2011-09-01 15:49:00 -0400

    Updated API for new config semantics and correct docs typos

--- src/pulp/server/content/importer/base.py
+++ src/pulp/server/content/importer/base.py
@@ -21,21 +21,21 @@ class Importer(object):
     def metadata(cls):
         return {}
 
-    def sync(self, repo_data, sync_conduit, config=None, options=None):
+    def sync(self, repo_data, sync_conduit, importer_config=None, repo_config=None):
         """
         Sync content into a repository.
         @param repo_data: metadata that describes a pulp repository
         @type repo_data: dict
         @param sync_conduit: api instance that provides limited pulp functionality
         @type sync_conduit: L{ContentPluginHook} instance
-        @param config: configuration for importer instance
-        @type config: None or dict
-        @param options: individual sync call options
-        @type options: None or dict
+        @param importer_config: configuration for the importer instance
+        @type importer_config: None or dict
+        @param repo_config: configuration for a specific repo
+        @type repo_config: None or dict
         """
         raise NotImplementedError()
 
-    def pre_import_unit(self, repo_data, unit_data, config=None, options=None):
+    def pre_import_unit(self, repo_data, unit_data, importer_config=None, repo_config=None):
         """
         Optional content unit pre-processing before it is imported into a
         repository.
@@ -43,14 +43,14 @@ class Importer(object):
         @type repo_data: dict
         @param unit_data: metadata that describes a content unit
         @type unit_data: dict
-        @param config: configuration for importer instance
-        @type options: None or dict
-        @param options: individual pre_import_unit call options
-        @type options: None or dict
+        @param importer_config: configuration for the importer instance
+        @type importer_config: None or dict
+        @param repo_config: configuration for a specific repo
+        @type repo_config: None or dict
         """
         pass
 
-    def import_unit(self, repo_data, unit_data, temp_location, config=None, options=None):
+    def import_unit(self, repo_data, unit_data, temp_location, importer_config=None, repo_config=None):
         """
         Import a unit of content into a repository.
         @param repo_data: metadata that describes a pulp repository
@@ -59,28 +59,28 @@ class Importer(object):
         @type unit_data: dict
         @param temp_location: full path to content unit on disk
         @type temp_location: str
-        @param config: configuration for importer instance
-        @type config: None or dict
-        @param options: individual import_unit call options
-        @type options: None or dict
+        @param importer_config: configuration for the importer instance
+        @type importer_config: None or dict
+        @param repo_config: configuration for a specific repo
+        @type repo_config: None or dict
         """
         raise NotImplementedError()
 
-    def delete_repo(self, repo_data, delete_conduit, config=None, options=None):
+    def delete_repo(self, repo_data, delete_conduit, importer_config=None, repo_config=None):
         """
         Delete a repository and its content.
         @param repo_data: metadata that describes a pulp repository
         @type repo_data: dict
         @param delete_conduit: api instance that provides limited pulp functionality
         @type delete_conduit: L{ContentPluginHook} instance
-        @param config: configuration for importer instance
-        @type config: None or dict
-        @param options: individual delete_repo call options
-        @type options: None or dict
+        @param importer_config: configuration for the importer instance
+        @type importer_config: None or dict
+        @param repo_config: configuration for a specific repo
+        @type repo_config: None or dict
         """
         raise NotImplementedError()
 
-    def clone_repo(self, repo_data, clone_data, clone_conduit, config=None, options=None):
+    def clone_repo(self, repo_data, clone_data, clone_conduit, importer_config=None, repo_config=None):
         """
         Clone a repository.
         @param repo_data: metadata that describes a pulp repository
@@ -89,9 +89,9 @@ class Importer(object):
         @type clone_data: dict
         @param clone_conduit: api instance that provides limited pulp functionality
         @type clone_conduit: L{ContentPluginHook} instance
-        @param config: configuration for importer instance
-        @type config: None or dict
-        @param options: individual clone_repo call options
-        @type options: None or dict
+        @param importer_config: configuration for the importer instance
+        @type importer_config: None or dict
+        @param repo_config: configuration for a specific repo
+        @type repo_config: None or dict
         """
         raise NotImplementedError()
