a1cbd4cb7c5c0aff415d6fad639e247b04f2f506
Jason L Connor <jconnor@redhat.com>
2011-09-02 00:13:13 -0600

    implementation of crud manager

--- src/pulp/server/managers/content/crud.py
+++ src/pulp/server/managers/content/crud.py
@@ -11,26 +11,90 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
+import os
+import uuid
+
+from pulp.server.constants import LOCAL_STORAGE
+from pulp.server.content.types import database as content_types_db
+from pulp.server.pexceptions import PulpException
+
+
+class ContentUnitNotFound(PulpException):
+    pass
+
 
 class ContentManager(object):
+    """
+    """
 
-    def add_content_unit(self, content_type, unit_id, unit_metadata, unit_path):
-        pass
+    def add_content_unit(self, content_type, unit_id, unit_metadata):
+        """
+        """
+        # XXX we should store the TypeDefinitions in their own collection in
+        # the db so that we can validate the metadata
+        collection = _get_type_collection(content_type)
+        if unit_id is None:
+            unit_id = str(uuid.uuid4())
+        unit_model = {'_id': unit_id}
+        unit_model.update(unit_metadata)
+        collection.insert(unit_model)
 
     def list_content_units(self, content_type, db_spec=None, model_fields=None):
-        pass
+        """
+        """
+        collection = _get_type_collection(content_type)
+        cursor = collection.find(db_spec, fields=model_fields)
+        # delima: retrun the cursor itself?
+        return tuple(cursor)
 
     def get_content_unit_keys(self, content_type, unit_ids):
-        pass
+        """
+        """
+        collection = _get_type_collection(content_type)
+        # do the content_unit_keys_dict correspond to the unique indexes?
+        return []
 
     def get_content_unit_ids(self, content_type, unit_keys):
-        pass
+        """
+        """
+        collection = _get_type_collection(content_type)
+        # do the content_unit_keys_dict correspond to the unique indexes?
+        return []
 
     def get_root_content_dir(self, content_type):
-        pass
+        """
+        """
+        # I'm paritioning the content on the file system based on content type
+        return os.path.join(LOCAL_STORAGE, content_type)
+
+    def request_content_unit_file_path(self, content_type, relative_path):
+        """
+        """
+        return os.path.join(self.get_root_content_dir(content_type), relative_path)
 
     def update_content_unit(self, content_type, unit_id, unit_metadata_delta):
-        pass
+        """
+        """
+        # XXX we should store the TypeDefinitions in their own collection in
+        # the db so that we can validate the metadata delta
+        collection = _get_type_collection(content_type)
+        content_unit = collection.find_one({'_id': unit_id})
+        if content_unit is None:
+            raise ContentUnitNotFound()
+        content_unit = dict(content_unit)
+        content_unit.update(unit_metadata_delta)
+        collection.save(content_unit)
 
     def remove_content_unit(self, content_type, unit_id):
-        pass
+        """
+        """
+        collection = _get_type_collection(content_type)
+        collection.remove({'_id': unit_id})
+
+# utility methods --------------------------------------------------------------
+
+def _get_type_collection(type_id):
+    """
+    """
+    collection = content_types_db.type_units_collection(type_id)
+    return collection
