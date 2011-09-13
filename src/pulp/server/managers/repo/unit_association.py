3a124b217dd70b6b49e1d40435cd2d029c94e3d3
Jay Dobies <jason.dobies@redhat.com>
2011-09-13 09:31:04 -0400

    Removed queries; these will live in the query manager

--- src/pulp/server/managers/repo/unit_association.py
+++ src/pulp/server/managers/repo/unit_association.py
@@ -35,25 +35,6 @@ class RepoUnitAssociationManager:
 
     # -- association manipulation ---------------------------------------------
 
-    def associate_unit_by_key(self, repo_id, unit_type_id, unit_key):
-        """
-        Creates an association between the given repository and a content unit
-        with the given key and type. This call takes the extra step of looking
-        up the unit's ID based on the provided key.
-
-        If there is already an association between the given repo and content
-        unit, this call has no effect.
-
-        Both repo and unit must exist in the database prior to this call,
-        however this call will not verify that for performance reasons. Care
-        should be taken by the caller to preserve the data integrity.
-        """
-
-        # TODO: call to content manager to get ID by key
-        unit_id = ''
-
-        self.associate_unit_by_id(repo_id, unit_type_id, unit_id)
-
     def associate_unit_by_id(self, repo_id, unit_type_id, unit_id):
         """
         Creates an association between the given repository and content unit.
@@ -75,21 +56,6 @@ class RepoUnitAssociationManager:
         association = RepoContentUnit(repo_id, unit_id, unit_type_id)
         RepoContentUnit.get_collection().save(association)
 
-    def unassociate_unit_by_key(self, repo_id, unit_type_id, unit_key):
-        """
-        Removes the association between a repo and the unit identified by the
-        given unit key. This call takes the extra step of looking up the unit's
-        ID based on the provided key.
-
-        If no association exists between the repo and unit, this call has no
-        effect.
-        """
-
-        # TODO: call to content manager to get ID by key
-        unit_id = ''
-
-        self.unassociate_unit_by_id(repo_id, unit_type_id, unit_id)
-
     def unassociate_unit_by_id(self, repo_id, unit_type_id, unit_id):
         """
         Removes the association between a repo and the given unit.
@@ -98,37 +64,3 @@ class RepoUnitAssociationManager:
         effect.
         """
         RepoContentUnit.get_collection().remove({'repo_id' : repo_id, 'unit_id' : unit_id, 'unit_type_id' : unit_type_id})
-
-    # -- association queries --------------------------------------------------
-
-    def unit_associations_for_repo(self, repo_id):
-        """
-        Returns all unit associations for the given repo. The full association
-        linking object will be returned for each unit.
-
-        @param repo_id: identifies the repo
-        @type  repo_id: str
-
-        @return: list of DB objects describing all of the units associated units
-                 to the repo; empty list if there are no associations
-        @rtype:  list of dict
-        """
-
-        # This call isn't meant to be a public facing call, so there is no
-        # translation between database object and public object model.
-
-        results = list(RepoContentUnit.get_collection().find({'repo_id' : repo_id}))
-        return results
-
-    def unit_keys_for_repo(self, repo_id):
-        """
-        Returns a list of all unit keys for units associated with the given repo.
-
-        @param repo_id: identifies the repo
-        @type  repo_id: str
-
-        @return: list of unit keys for all associated units; empty list if there
-                 are no associations
-        @rtype:  list of dict
-        """
-        pass
