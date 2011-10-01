7e1e1625bc8703c305e286860bc0b1f72f8f1785
Jason L Connor <jconnor@redhat.com>
2011-10-01 16:29:35 -0600

    uri and href implmentations for repo serialization

--- src/pulp/server/webservices/serialization/repo.py
+++ src/pulp/server/webservices/serialization/repo.py
@@ -3,6 +3,46 @@
 Module for repo serialization.
 """
 
+from pulp.server.cds import round_robin
+from pulp.server.webservices import http
 
-def uri(repo_id):
-    pass
+# contstants -------------------------------------------------------------------
+
+# XXX these shouls be stored in a common module
+API_HREF = '/pulp/api'
+API_V2_HREF = API_HREF + '/v2'
+
+REPO_URI_PATH = '/pub/repos'
+
+COLLECTION_HREF = API_HREF + '/repositories'
+RESOURCE_HREF = COLLECTION_HREF + '/%s'
+
+# repo serialization api -------------------------------------------------------
+
+def href(repo):
+    href = RESOURCE_HREF % repo['id']
+    return ensure_ending_slash(href)
+
+
+def uri(repo):
+    # not published: no repo uri
+    if not repo['publish']:
+        return None
+    # use cds uri first
+    cds = round_robin._find_association(repo['id'])
+    if cds is not None:
+        return cds['next_permutation'][0]
+    # no cds association: build local uri
+    request_uri = http.request_url()
+    uri_prefix = request_uri.split(API_HREF)[0]
+    uri = '/'.join((uri_prefix, REPO_URI_PATH, repo['id']))
+    return ensure_ending_slash(uri)
+
+
+# utility methods --------------------------------------------------------------
+
+# XXX this should be stored in a utility module
+def ensure_ending_slash(uri_or_path):
+    if not uri_or_path.endswith('/'):
+        uri_or_path += '/'
+    return uri_or_path
