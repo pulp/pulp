6e84b73ddb4a85ab57045cb28f8fd6a8a05c1345
Jason L Connor <jconnor@redhat.com>
2011-10-02 23:54:16 -0600

    added error serialization module

---
+++ src/pulp/server/webservices/serialization/error.py
@@ -0,0 +1,51 @@
+
+"""
+Pulp error serialization.
+"""
+
+import copy
+import httplib
+import traceback
+
+
+_ERROR_OBJ_SKEL = {
+    'http_status': httplib.INTERNAL_SERVER_ERROR,
+    'error_messege': None,
+    'exception': None,
+    'traceback': None,
+}
+
+
+def serialize_exception(e, tb=None, msg=None):
+    """
+    Serialize an (possibly unhandled) exception.
+    @param e: exception
+    @type e: Exception
+    @param tb: traceback
+    @type tb: traceback
+    @param msg: error messege
+    @type msg: str
+    @return: serialized error
+    @rtype: dict
+    """
+    error_obj = copy.copy(_ERROR_OBJ_SKEL)
+    error_obj['error_messege'] = msg
+    error_obj['exception'] = repr(e)
+    error_obj['traceback'] = traceback.format_tb(tb)
+    return error_obj
+
+
+def serialize_http_error(http_status, msg=None):
+    """
+    Serialize an http error.
+    @param http_status: valid http status number
+    @type http_status: int
+    @param msg: error messege
+    @type: str
+    @return: serialized error
+    @rtype: dict
+    """
+    error_obj = copy.copy(_ERROR_OBJ_SKEL)
+    error_obj['http_status'] = http_status
+    error_obj['error_messege'] = msg
+    return error_obj
