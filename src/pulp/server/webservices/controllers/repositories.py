4cab29a17ad090d69cd5fb820cd0aec869f86eaf
Sayli Karmarkar <skarmark@redhat.com>
2011-10-23 23:39:10 -0700

    748324 - Fixed pulp-admin errata list not checking for non-existing consumer

--- src/pulp/server/webservices/controllers/repositories.py
+++ src/pulp/server/webservices/controllers/repositories.py
@@ -547,6 +547,7 @@ class RepositoryDeferredFields(JSONController):
         valid_filters = ('type', 'severity')
         types = self.filters(valid_filters).get('type', [])
         severity = self.filters(valid_filters).get('severity', [])
+        
         if types:
             errata = api.errata(id, [types])
         elif severity:
