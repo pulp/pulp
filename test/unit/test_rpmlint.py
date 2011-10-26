e33461a67242ad9f0b39deb2192586b2067a42c9
Jay Dobies <jason.dobies@redhat.com>
2011-10-26 14:01:43 -0400

    Added unit test to run rpmlint on pulp.spec

---
+++ test/unit/test_rpmlint.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2011 Red Hat, Inc.
+#
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+# Python
+import os
+import subprocess
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + "/../common/")
+import testutil
+
+class SpecFileTests(testutil.PulpTest):
+
+    def test_rpmlint(self):
+        """
+        Verifies rpmlint does not indicate any errors in pulp.spec.
+        """
+
+        # Setup
+        unit_test_dir = os.path.abspath(os.path.dirname(__file__))
+        spec_file = unit_test_dir + '/../../pulp.spec'
+
+        # Test
+        p = subprocess.Popen('/usr/bin/rpmlint %s' % spec_file, shell=True, stdout=subprocess.PIPE)
+        p.wait()
+
+        # Verify
+        #   Print the output in case the caller wants to see the full rpmlint output
+        print(p.stdout.read())
+        
+        self.assertEqual(0, p.returncode, msg='rpmlint indicated one or more errors in pulp.spec')
+        
