4b1862eab1f703f59e9b3ce8f03f8abcef10ea76
John Matthews <jmatthews@redhat.com>
2011-12-06 15:47:32 -0500

    SELinux: leveraging apache_content_template(pulp) macro to simplify rules Updates for developer setup/uninstall scripts

--- selinux/server/uninstall.sh
+++ selinux/server/uninstall.sh
@@ -5,9 +5,10 @@ SELINUX_VARIANTS="mls strict targeted"
 MODULE_TYPE="apps"
 INSTALL_DIR="/usr/share"
 
-for selinuxvariant in ${selinux_variants}
+for selinuxvariant in ${SELINUX_VARIANTS}
 do
-    /usr/sbin/semodule -s ${selinuxvariant} -r ${NAME} &> /dev/null || :
+    #/usr/sbin/semodule -s ${selinuxvariant} -r ${NAME} &> /dev/null || :
+    /usr/sbin/semodule -s ${selinuxvariant} -r ${NAME}
     rm -f ${INSTALL_DIR}/${selinuxvariant}/${NAME}.pp
 done
 
