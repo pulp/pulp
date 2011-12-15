e61b72a5ab304a21dfe00cf6762cfdb2fce44f01
Jason L Connor <jconnor@redhat.com>
2011-12-14 21:20:15 -0700

    had presentation twice instead of progress

--- src/pulp/server/dispatch/constants.py
+++ src/pulp/server/dispatch/constants.py
@@ -34,7 +34,7 @@ CALL_PROGRESS_CONTROL_HOOK = 1
 CALL_PRESENTATION_CONTROL_HOOK = 2
 
 CALL_CONTROL_HOOKS = (CALL_CANCEL_CONTROL_HOOK,
-                      CALL_PRESENTATION_CONTROL_HOOK,
+                      CALL_PROGRESS_CONTROL_HOOK,
                       CALL_PRESENTATION_CONTROL_HOOK)
 
 # execution responses ----------------------------------------------------------
