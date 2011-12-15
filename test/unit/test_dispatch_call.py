37b1f429c8888455e892735afa663edf62a92d55
Jason L Connor <jconnor@redhat.com>
2011-12-14 21:14:33 -0700

    added more unittests

--- test/unit/test_dispatch_call.py
+++ test/unit/test_dispatch_call.py
@@ -18,14 +18,12 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../common/'))
 
 import testutil
 
+from pulp.server.dispatch import constants as dispatch_constants
 from pulp.server.dispatch.call import CallReport, CallRequest
 
 # call test api ----------------------------------------------------------------
 
-class Call(object):
-    """
-    Test call functor
-    """
+class Functor(object):
 
     def __init__(self):
         self.args = []
@@ -35,13 +33,114 @@ class Call(object):
         self.args.extend(args)
         self.kwargs.update(kwargs)
 
+
+class Class(object):
+
+    def method(self, *args, **kwargs):
+        pass
+
+
+def function(*args, **kwargs):
+    pass
+
 # call request testing ---------------------------------------------------------
 
 class CallRequestTests(testutil.PulpTest):
 
-    def test_call_request_instantiation(self):
+    def test__instantiation(self):
+        call = Functor()
         try:
-            call = Call()
             call_request = CallRequest(call)
+        except Exception, e:
+            self.fail(e.message)
+
+    def test_args(self):
+        call = Functor()
+        args = ['fee', 'fie', 'foe', 'foo']
+        try:
+            call_request = CallRequest(call, args)
+        except Exception, e:
+            self.fail(e.message)
+
+    def test_kwargs(self):
+        call = Functor()
+        kwargs = {'one': 'foo', 'two': 'bar', 'three': 'baz'}
+        try:
+            call_request = CallRequest(call, kwargs=kwargs)
+        except Exception, e:
+            self.fail(e.message)
+
+    def test_function_str(self):
+        name = 'function'
+        call_request = CallRequest(function)
+        self.assertTrue(call_request.callable_name() == name)
+
+    def test_method_str(self):
+        name = 'Class.method'
+        instance = Class()
+        call_request = CallRequest(instance.method)
+        self.assertTrue(call_request.callable_name() == name)
+
+    def test_call_request_str(self):
+        expected = "CallRequest: function('fee', 'fie', 'foe', 'foo', one='foo', three='baz', two='bar')"
+        args = ['fee', 'fie', 'foe', 'foo']
+        kwargs = {'one': 'foo', 'two': 'bar', 'three': 'baz'}
+        call_request = CallRequest(function, args, kwargs)
+        call_request_str = str(call_request)
+        self.assertTrue(call_request_str == expected, '"%s" != "%s"' % (call_request_str, expected))
+
+    def test_control_hooks(self):
+        call_request = CallRequest(function)
+        for key in dispatch_constants.CALL_CONTROL_HOOKS:
+            self.assertTrue(call_request.control_hooks[key] is None)
+        for key in dispatch_constants.CALL_CONTROL_HOOKS:
+            call_request.add_control_hook(key, function)
+            self.assertTrue(call_request.control_hooks[key] is function)
+
+    def test_execution_hooks(self):
+        call_request = CallRequest(function)
+        for key in dispatch_constants.CALL_EXECUTION_HOOKS:
+            self.assertTrue(isinstance(call_request.execution_hooks[key], list))
+            self.assertTrue(len(call_request.execution_hooks[key]) == 0)
+            call_request.add_execution_hook(key, function)
+            self.assertTrue(isinstance(call_request.execution_hooks[key], list))
+            self.assertTrue(len(call_request.execution_hooks[key]) == 1)
+
+    def test_serialize_deserialize(self):
+        args = ['fee', 'fie', 'foe', 'foo']
+        kwargs = {'one': 'foo', 'two': 'bar', 'three': 'baz'}
+        call_request = CallRequest(function, args, kwargs)
+        data = call_request.serialize()
+        self.assertTrue(isinstance(data, dict))
+        call_request_2 = CallRequest.deserialize(data)
+        self.assertTrue(isinstance(call_request_2, CallRequest), str(type(call_request_2)))
+
+    def test_serialize_deserialize_with_control_hook(self):
+        key = dispatch_constants.CALL_CANCEL_CONTROL_HOOK
+        call_request = CallRequest(function)
+        call_request.add_control_hook(key, function)
+        data = call_request.serialize()
+        self.assertTrue(isinstance(data, dict))
+        call_request_2 = CallRequest.deserialize(data)
+        self.assertTrue(isinstance(call_request_2, CallRequest))
+        self.assertTrue(call_request_2.control_hooks[key] == function)
+
+    def test_serialize_deserialize_with_execution_hook(self):
+        key = dispatch_constants.CALL_CANCEL_EXECUTION_HOOK
+        call_request = CallRequest(function)
+        call_request.add_execution_hook(key, function)
+        data = call_request.serialize()
+        self.assertTrue(isinstance(data, dict))
+        call_request_2 = CallRequest.deserialize(data)
+        self.assertTrue(isinstance(call_request_2, CallRequest))
+        self.assertTrue(call_request_2.execution_hooks[key][0] == function)
+
+# call report testing ----------------------------------------------------------
+
+class CallReportTests(testutil.PulpTest):
+
+    def test_instantiation(self):
+        try:
+            call_report = CallReport()
         except Exception, e:
             self.fail(e.message)
