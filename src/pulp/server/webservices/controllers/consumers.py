77ee8df6d598995b2b56f71e373fae0ca159d79b
Sayli Karmarkar <skarmark@redhat.com>
2012-01-04 05:18:57 -0800

    Minor consumer api fixes correcting return codes

--- src/pulp/server/webservices/controllers/consumers.py
+++ src/pulp/server/webservices/controllers/consumers.py
@@ -182,7 +182,7 @@ class Consumer(JSONController):
         """
         consumer = consumer_api.consumer(id)
         if consumer is None:
-            return self.conflict('Consumer [%s] does not exist' % id)
+            return self.not_found('Consumer [%s] does not exist' % id)
         user = user_api.user(id)
         if user is not None:
             revoke_all_permissions_from_user(user['login'])
