428960971592a3a5b615b513f6fd93fd69f35f2c
Jason L Connor <jconnor@redhat.com>
2012-01-17 15:14:32 -0700

    757825 add only copies of the task to the history so that editing them does not edit the task

--- src/pulp/server/db/model/persistence.py
+++ src/pulp/server/db/model/persistence.py
@@ -11,6 +11,7 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
+import copy
 import pickle
 from gettext import gettext as _
 
@@ -79,7 +80,7 @@ class TaskHistory(Model):
         for attr in ('id', 'class_name', 'method_name', 'args', 'kwargs',
                      'state', 'progress', 'result', 'exception', 'traceback',
                      'consecutive_failures', 'job_id'):
-            setattr(self, attr, getattr(task, attr))
+            setattr(self, attr, copy.copy(getattr(task, attr)))
         # remove the kwargs that can't be stored in the database
         for arg in ('synchronizer', 'progress_callback'):
             self.kwargs.pop(arg, None)
