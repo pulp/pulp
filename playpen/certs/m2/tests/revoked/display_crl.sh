17f245b6de310c529a65c70091d1c943aaf4e429
John Matthews <jmatthews@redhat.com>
2012-01-18 10:21:19 -0500

    Updated generation of CRL/CA/Cert data for m2crypto unit tests

--- playpen/certs/m2/tests/revoked/display_crl.sh
+++ playpen/certs/m2/tests/revoked/display_crl.sh
@@ -1,5 +1,9 @@
+export CERT_DIR=./certs
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 crl"
-    exit 1
+    export CRL_FILE=${CERT_DIR}/revoking_crl.pem
+    echo "Defaulting to look at CRL: ${CRL_FILE}"
+else
+    export CRL_FILE=$1
 fi
-openssl crl -text -in $1 -noout
+
+openssl crl -text -in ${CRL_FILE} -noout
--- playpen/certs/m2/tests/revoked/verify_ca_and_cert.sh
+++
@@ -1,6 +0,0 @@
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 ca_cert cert"
-    exit 1
-fi
-openssl verify -CAfile $1 $2
-
