061e82d2fae52ed904109e43d239a169ccce2359
John Matthews <jmatthews@redhat.com>
2012-01-23 14:58:27 -0500

    Add example to parse a single string into a list of X509 certificates

--- playpen/certs/chain_example/test_chain_verify.py
+++ playpen/certs/chain_example/test_chain_verify.py
@@ -69,5 +69,91 @@ class ChainVerifyTestCase(unittest.TestCase):
         store_ctx.init(store, test_cert)
         self.assertFalse(store_ctx.verify_cert())
 
+    def load_certs(self, bio, certs=None):
+        if not certs:
+            certs = []
+        try:
+            cert = X509.load_cert_bio(bio)
+            certs.append(cert)
+            return self.load_certs(bio, certs)
+        except X509.X509Error, e:
+            return certs
+
+    def load_chain_from_string(self, data):
+        """
+        @param data: A single string of concatenated X509 Certificates in PEM format
+        @type data: str
+        """
+        # Refer to OpenSSL crypto/x509/by_file.c
+        # Function: X509_load_cert_file() to see how they parse a chain file and add
+        # the certificates to a X509_Store.  Below follows a similar procedure.
+
+        from M2Crypto import BIO
+        bio = BIO.MemoryBuffer(data)
+        certs = []
+        try:
+            while True:
+                # Read one cert at a time, 'bio' stores the last location read
+                # Exception is raised when no more cert data is available
+                cert = X509.load_cert_bio(bio)
+                if not cert:
+                    break
+                certs.append(cert)
+        except X509.X509Error, e:
+            # This is the normal return path.
+            # X509.load_cert_bio will throw an exception after reading all of the cert
+            # data
+            return certs
+        return certs
+
+
+    def test_load_chain_empty(self):
+        certs = self.load_chain_from_string("")
+        self.assertEquals(len(certs), 0)
+
+        certs = self.load_chain_from_string(None)
+        self.assertEquals(len(certs), 0)
+
+    def test_load_chain_malformed(self):
+        certs = self.load_chain_from_string("BAD_DATA")
+        self.assertEquals(len(certs), 0)
+
+    def test_load_chain(self):
+        data = open(CA_CHAIN).read()
+        certs = self.load_chain_from_string(data)
+        self.assertEquals(len(certs), 2)
+
+        data = open(SUB_CA).read()
+        certs = self.load_chain_from_string(data)
+        self.assertEquals(len(certs), 1)
+
+        data = open(TEST_CERT).read()
+        certs = self.load_chain_from_string(data)
+        self.assertEquals(len(certs), 1)
+
+    def test_chain_verify_from_string(self):
+        data = open(CA_CHAIN).read()
+        ca_chain = self.load_chain_from_string(data)
+        test_cert = X509.load_cert(TEST_CERT)
+
+        store = X509.X509_Store()
+        for ca in ca_chain:
+            store.add_x509(ca)
+        store_ctx = X509.X509_Store_Context()
+        store_ctx.init(store, test_cert)
+        self.assertTrue(store_ctx.verify_cert())
+
+    def test_incomplete_chain_verify_from_string(self):
+        data = open(SUB_CA).read()
+        ca_chain = self.load_chain_from_string(data)
+        test_cert = X509.load_cert(TEST_CERT)
+
+        store = X509.X509_Store()
+        for ca in ca_chain:
+            store.add_x509(ca)
+        store_ctx = X509.X509_Store_Context()
+        store_ctx.init(store, test_cert)
+        self.assertFalse(store_ctx.verify_cert())
+
 if __name__ == "__main__":
     unittest.main()
