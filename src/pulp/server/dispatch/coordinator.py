d2468dbacec2173bc872dda59eb90aed64b1c80f
Jason L Connor <jconnor@redhat.com>
2012-01-30 14:37:06 -0700

    added coordinator task model

--- src/pulp/server/dispatch/coordinator.py
+++ src/pulp/server/dispatch/coordinator.py
@@ -10,3 +10,21 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+from pulp.server.db.model.dispatch import CoordinatorTask
+from pulp.server.dispatch import call, task, taskqueue
+from pulp.server.dispatch import constants as dispatch_constants
+from pulp.server.dispatch import exceptions as dispatch_exceptions
+
+# coordinator class ------------------------------------------------------------
+
+class Coordinator(object):
+
+    def __init__(self, task_queue):
+        assert isinstance(task_queue, taskqueue.TaskQueue)
+        self.task_queue = task_queue
+        self.task_collection = CoordinatorTask.get_collection()
+
+# conflicting operation detection ----------------------------------------------
+
+OPERATION_MATRIX = (())
