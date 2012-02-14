e39e13be93bf17722b451a2a883844ab62fa2c67
Jason L Connor <jconnor@redhat.com>
2012-02-14 14:59:30 -0700

    added some docstrings

--- playpen/mongo/mic.py
+++ playpen/mongo/mic.py
@@ -10,11 +10,13 @@ from pymongo.son_manipulator import AutoReference, NamespaceInjector
 _connection = None
 _db = None
 
-db_name = 'test' # you can re-assign this *before* calling connect
-
 # client api -------------------------------------------------------------------
 
-def connect():
+def connect(db_name='test'):
+    """
+    Connect to a test database.
+    Optional argument, db_name, specifies the name of the database.
+    """
     global _connection, _db
     _connection = pymongo.Connection()
     _db = getattr(_connection, db_name)
@@ -23,6 +25,9 @@ def connect():
     
 
 def clean():
+    """
+    Drop all user-created collections in the database.
+    """
     for name in _db.collection_names():
         if name.startswith(u'system.'):
             continue
@@ -30,6 +35,11 @@ def clean():
 
 
 def collection(name, unique_indices=None, search_indicies=None):
+    """
+    Get a collection corresponding to the given name.
+    Indice arguments must be a list of (index or indices tuple, direction)
+    tuples. The same format accepted by Collection.ensure_index.
+    """
     c = Collection(_db, name)
     if unique_indices:
         c.ensure_index(unique_indices, unique=True, background=True)
