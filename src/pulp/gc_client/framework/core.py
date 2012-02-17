7a24ab02bf4bb3a780b055f56deb213289dae47c
Jay Dobies <jason.dobies@redhat.com>
2012-02-17 10:12:24 -0500

    Initial implementation of rendering methods

--- src/pulp/gc_client/framework/core.py
+++ src/pulp/gc_client/framework/core.py
@@ -18,13 +18,90 @@ for the individual components that belong to each UI style
 further subclassed by extensions.
 """
 
-from okaara.cli import Cli
-from okaara.prompt import Prompt, WIDTH_TERMINAL
+import math
+
+from   okaara.cli import Cli
+from   okaara.progress import ProgressBar, Spinner
+import okaara.prompt
+from   okaara.prompt import Prompt, WIDTH_TERMINAL
+
+# -- constants ----------------------------------------------------------------
+
+# Values used for tags in each of the rendering calls; these should be used
+# in unit tests to verify the correct write call was made
+TAG_TITLE = 'title'
+TAG_DESCRIPTION = 'description'
+TAG_MAP_LIST_ITEM = 'map_list_item'
+
+COLOR_HEADER = okaara.prompt.COLOR_LIGHT_BLUE
+COLOR_IN_PROGRESS = okaara.prompt.COLOR_LIGHT_YELLOW
+COLOR_COMPLETED = okaara.prompt.COLOR_LIGHT_GREEN
+
+BAR_PERCENTAGE = .66
 
 # -- classes ------------------------------------------------------------------
 
 class PulpPrompt(Prompt):
-    pass
+
+    def render_spacer(self, lines=1):
+        for i in range(0, lines):
+            self.write('')
+
+    def render_title(self, title):
+
+        if self.wrap_width is WIDTH_TERMINAL:
+            width = self.terminal_size()[0]
+        else:
+            width = self.wrap_width
+
+        divider = '+' + ('-' * (width - 2)) + '+'
+
+        self.write(divider)
+        self.write(title, center=True, color=COLOR_HEADER, tag=TAG_TITLE)
+        self.write(divider)
+        self.render_spacer()
+
+    def render_description(self, description):
+
+        self.write(description, tag=TAG_DESCRIPTION)
+        self.render_spacer()
+
+    def render_list_of_dict(self, items, filters=None, spaces_between_cols=2):
+
+        # Generate template
+        max_key_length = len(max(items, key=len))
+        line_template = '%-' + str(max_key_length) + 's:' + (' ' * spaces_between_cols) + '%s'
+
+        # Apply the filters if specified, making sure to not destroy the
+        # caller's object in the process
+        if filters is not None:
+            items = dict([(k, v) for k, v in items if k in filters])
+
+        # Print each item
+        for k, v in items:
+            line = line_template % (str(k), str(v))
+            self.write(line, tag=TAG_MAP_LIST_ITEM)
+            self.render_spacer()
+
+        self.render_spacer()
+
+    def create_progress_bar(self, show_trailing_percentage=True):
+
+        if self.wrap_width is WIDTH_TERMINAL:
+            width = self.terminal_size()[0]
+        else:
+            width = self.wrap_width
+
+        width = int(math.floor(BAR_PERCENTAGE * width))
+
+        pb = ProgressBar(self, show_trailing_percentage=show_trailing_percentage,
+                         in_progress_color=COLOR_IN_PROGRESS, completed_color=COLOR_COMPLETED)
+        return pb
+
+    def create_spinner(self):
+
+        spinner = Spinner(self)
+        return spinner
 
 class PulpCli(Cli):
     pass
