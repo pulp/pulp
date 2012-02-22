5cec43b3b784e6240a0df688e95729009f1a4305
Jay Dobies <jason.dobies@redhat.com>
2012-02-22 14:59:45 -0500

    Added client test to v2 tests

--- run-v2-tests.sh
+++ run-v2-tests.sh
@@ -3,11 +3,13 @@
 # Please keep the following in alphabetical order so it's easier to determine
 # if something is in the list
 
-PACKAGES="pulp.server.managers,pulp.server.content,pulp.server.db.model.gc_repository,pulp.server.db.model.gc_content,pulp.server.webservices.controllers.gc_contents,pulp.server.webservices.controllers.gc_plugins,pulp.server.webservices.controllers.gc_repositories"
+PACKAGES="pulp.gc_client.framework.loader,pulp.gc_client.framework.core,pulp.server.managers,pulp.server.content,pulp.server.db.model.gc_repository,pulp.server.db.model.gc_content,pulp.server.webservices.controllers.gc_contents,pulp.server.webservices.controllers.gc_plugins,pulp.server.webservices.controllers.gc_repositories"
 TESTS="test/unit/test_base_distributor_conduit.py \
        test/unit/test_base_importer_conduit.py \
+       test/unit/test_client_framework_core.py \
        test/unit/test_content_managers.py \
        test/unit/test_content_plugin_loader.py \
+       test/unit/test_extensions_loader.py \
        test/unit/test_repo_controller.py \
        test/unit/test_repo_importer_manager.py \
        test/unit/test_repo_manager.py \
