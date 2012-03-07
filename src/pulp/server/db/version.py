95315f675291170de132fdeb3de32fd85fe2a820
James Slagle <jslagle@redhat.com>
2012-03-07 14:55:37 -0500

    801161 Add missing db migration

--- src/pulp/server/db/version.py
+++ src/pulp/server/db/version.py
@@ -21,7 +21,7 @@ from pulp.server.db.model import Model
 
 # current data model version of the code base
 # increment this if you change the data model
-VERSION = 37
+VERSION = 38
 
 # this isn't anything
 _version_db = None
