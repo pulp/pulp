82f44b9891e52bfd5eb3173f36a422d6654d6857
Pradeep Kilambi <pkilambi@redhat.com>
2012-03-27 15:28:41 -0400

    unit tests for drpm sync

---
+++ test/plugins/yum_importer/test_drpms.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2011 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+import os
+import sys
+import mock
+import unittest
+import tempfile
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + "/../../../src/")
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)) + "/../../../plugins/importers/yum_importer/")
+import drpm
+import importer_rpm
+import importer_mocks
+from importer import YumImporter
+from importer import YUM_IMPORTER_TYPE_ID
+from pulp.server.content.plugins.model import Repository, Unit
+
+class TestDRPMS(unittest.TestCase):
+
+    def setUp(self):
+        super(TestDRPMS, self).setUp()
+        self.temp_dir = tempfile.mkdtemp()
+        self.working_dir = os.path.join(self.temp_dir, "working")
+        self.pkg_dir = os.path.join(self.temp_dir, "packages")
+        self.data_dir = os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), "../data"))
+
+    def tearDown(self):
+        super(TestDRPMS, self).tearDown()
+
+    def test_metadata(self):
+        metadata = YumImporter.metadata()
+        self.assertEquals(metadata["id"], YUM_IMPORTER_TYPE_ID)
+        self.assertTrue(drpm.DRPM_TYPE_ID in metadata["types"])
+
+    def test_drpm_sync(self):
+        feed_url = "http://repos.fedorapeople.org/repos/pulp/pulp/demo_repos/test_drpm_repo/"
+        importer = YumImporter()
+        repo = mock.Mock(spec=Repository)
+        repo.working_dir = self.working_dir
+        repo.id = "test_repo"
+        sync_conduit = importer_mocks.get_sync_conduit()
+        config = importer_mocks.get_basic_config(feed_url=feed_url)
+        status, summary, details = importer_rpm._sync(repo, sync_conduit, config)
+        self.assertTrue(status)
+        self.assertTrue(summary is not None)
+        self.assertTrue(details is not None)
+        self.assertEquals(summary["num_synced_new_drpms"], 18)
+        self.assertEquals(summary["num_resynced_drpms"], 0)
+        self.assertEquals(summary["num_orphaned_drpms"], 0)
