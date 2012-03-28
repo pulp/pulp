735e019fafe27c63c1ce212630ad4dada72e8e2c
Jason L Connor <jconnor@redhat.com>
2012-03-28 09:42:09 -0600

    added actual docstrings to link serialization  module

--- src/pulp/server/webservices/serialization/link.py
+++ src/pulp/server/webservices/serialization/link.py
@@ -13,6 +13,11 @@
 
 """
 Generation of link objects for REST object serialization.
+
+link object:
+{
+    "_href": <uri path to resource or collection>
+}
 """
 
 import copy
@@ -26,18 +31,41 @@ _LINK_OBJ_SKEL = {
 
 
 def link_obj(href):
+    """
+    Create a link object for an arbitrary path.
+    @param href: uri path
+    @type  href: str
+    @return: link object
+    @rtype:  dict
+    """
     link = copy.copy(_LINK_OBJ_SKEL)
     link['_href'] = href
     return link
 
 
 def current_link_obj():
+    """
+    Create a link object for the path for the current request.
+    @return: link object
+    @rtype:  dict
+    """
     link = copy.copy(_LINK_OBJ_SKEL)
     link['_href'] = http.uri_path()
     return link
 
 
 def child_link_obj(*path_elements):
+    """
+    Create a link object that appends the given elements to the path of the
+    current request.
+    Example: current request path = '/foo/bar/baz/'
+             path elements = ['fee', 'fie']
+             returned path = '/foo/bar/baz/fee/fie/'
+    @param path_elements: path elements to append
+    @type  path_elements: *str
+    @return: link object
+    @rtype:  dict
+    """
     suffix = '/'.join(path_elements)
     link = copy.copy(_LINK_OBJ_SKEL)
     link['_href'] = http.extend_uri_path(suffix)
@@ -45,6 +73,15 @@ def child_link_obj(*path_elements):
 
 
 def sibling_link_obj(*path_replacements):
+    """
+    Create a link object that replaces the end elements in the current request
+    path with the provided replacements.
+    Example: current request path = '/fee/fie/foe/foo/'
+             path replacements = ['bar', 'baz']
+             returned path = '/fee/fie/bar/baz/'
+    @return: link object
+    @rtype:  dict
+    """
     link = copy.copy(_LINK_OBJ_SKEL)
     link['_href'] = http.sub_uri_path(*path_replacements)
     return link
