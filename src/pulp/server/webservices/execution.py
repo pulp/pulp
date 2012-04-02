5d5ba170966de9850f64150e803e8f02b2919704
Jason L Connor <jconnor@redhat.com>
2012-04-02 16:39:33 -0600

    re-implemented execute and execute_sync that now return the call result if all goes well

--- src/pulp/server/webservices/execution.py
+++ src/pulp/server/webservices/execution.py
@@ -20,6 +20,25 @@ from pulp.server.webservices import serialization
 
 # execution wrapper api --------------------------------------------------------
 
+def execute(call_request):
+    """
+    Execute a call request through the controller and return the result iff the
+    call was executed immediately.
+    @param call_request: call request to execute
+    @type  call_request: pulp.server.dispatch.call.CallRequest
+    @return: return value from call in call request
+    """
+    coordinator = dispatch_factory.coordinator()
+    call_report = coordinator.execute_call(call_request)
+    if call_report.response is dispatch_constants.CALL_REJECTED_RESPONSE:
+        raise ConflictingOperation(call_report.reasons)
+    if call_report.response in dispatch_constants.CALL_INCOMPLETE_STATES:
+        raise OperationPostponed(call_report)
+    if call_report.state is dispatch_constants.CALL_ERROR_STATE:
+        raise call_report.exception, None, call_report.traceback
+    return call_report.result
+
+
 def execute_ok(controller, call_request):
     """
     Execute a call request via the coordinator.
@@ -64,7 +83,6 @@ def execute_created(controller, call_request, location):
     return controller.created(location, call_report.result)
 
 
-
 def execute_async(controller, call_request):
     """
     Execute a call request asynchronously via the coordinator.
@@ -79,6 +97,24 @@ def execute_async(controller, call_request):
     if call_report.response is dispatch_constants.CALL_REJECTED_RESPONSE:
         raise ConflictingOperation(call_report.reasons)
     raise OperationPostponed(call_report)
+
+
+def execute_sync(call_request, timeout=timedelta(seconds=20)):
+    """
+    Execute a call request synchronously via the coordinator.
+    @param call_request: call request to execute
+    @type  call_request: pulp.server.dispatch.call.CallRequest
+    @param timeout: time to wait for task to start before raising and exception
+    @type  timeout: datetime.timedelta
+    @return: return value from call in call request
+    """
+    coordinator = dispatch_factory.coordinator()
+    call_report = coordinator.execute_call_synchronously(call_request, timeout=timeout)
+    if call_report.response is dispatch_constants.CALL_REJECTED_RESPONSE:
+        raise ConflictingOperation(call_report.reasons)
+    if call_report.state is dispatch_constants.CALL_ERROR_STATE:
+        raise call_report.exception, None, call_report.traceback
+    return call_report.result
 
 
 def execute_sync_ok(controller, call_request, timeout=timedelta(seconds=20)):
@@ -101,7 +137,6 @@ def execute_sync_ok(controller, call_request, timeout=timedelta(seconds=20)):
     return controller.ok(call_report.result)
 
 
-
 def execute_sync_created(controller, call_request, location, timeout=timedelta(seconds=20)):
     """
     Execute a call request synchronously via the coordinator.
@@ -122,4 +157,3 @@ def execute_sync_created(controller, call_request, location, timeout=timedelta(s
     if call_report.state is dispatch_constants.CALL_ERROR_STATE:
         raise call_report.exception, None, call_report.traceback
     return controller.created(location, call_report.result)
-
