b33c1f4893ceac540d510b432e308d0152dee608
Jason L Connor <jconnor@redhat.com>
2012-04-03 10:31:03 -0600

    removed unused import

--- src/pulp/server/dispatch/call.py
+++ src/pulp/server/dispatch/call.py
@@ -11,7 +11,6 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-import inspect
 import itertools
 import logging
 import pickle
