54ff9b24d9d265b0ed2c97129c9f76996e16d954
Jason L Connor <jconnor@redhat.com>
2012-04-27 08:50:09 -0600

    added base schedule testing setUp and tearDown along with non sync/publish tests

--- test/unit/test_schedule_cud_manager.py
+++ test/unit/test_schedule_cud_manager.py
@@ -17,8 +17,12 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../common/'))
 
 import testutil
+import mock_plugins
 
+from pulp.server import exceptions as pulp_exceptions
+from pulp.server.db.model.gc_repository import Repo, RepoDistributor, RepoImporter
 from pulp.server.dispatch import factory as dispatch_factory
+from pulp.server.managers import factory as managers_factory
 from pulp.server.managers.schedule.cud import ScheduleManager
 
 # schedule tests base class ----------------------------------------------------
@@ -27,14 +31,79 @@ class ScheduleTests(testutil.PulpCoordinatorTest):
 
     def setUp(self):
         super(ScheduleTests, self).setUp()
+
+        mock_plugins.install()
+        self._repo_manager = managers_factory.repo_manager()
+        self._distributor_manager = managers_factory.repo_distributor_manager()
+        self._importer_manager = managers_factory.repo_importer_manager()
+
+        self.repo_id = 'scheduled-repo'
+        self.distributor_type_id = 'mock-distributor'
+        self.distributor_id = 'scheduled-distributor'
+        self.importer_type_id = 'mock-importer'
+
+        self._repo_manager.create_repo(self.repo_id)
+        self._distributor_manager.add_distributor(self.repo_id, self.distributor_type_id, {}, False, distributor_id=self.distributor_id)
+        self._importer_manager.set_importer(self.repo_id, self.importer_type_id, {})
+
         self.schedule_manager = ScheduleManager()
 
     def tearDown(self):
         super(ScheduleTests, self).tearDown()
+        mock_plugins.reset()
+        self._repo_manager = None
+        self._distributor_manager = None
+        self._importer_manager = None
         self.schedule_manager = None
+
+    def clean(self):
+        super(ScheduleTests, self).clean()
+        Repo.get_collection().remove(safe=True)
+        RepoDistributor.get_collection().remove(safe=True)
+        RepoImporter.get_collection().remove(safe=True)
+
+# schedule manager tests -------------------------------------------------------
+
+class ScheduleManagerTests(testutil.PulpTest):
+
+    def test_instantiation(self):
+        schedule_manager = ScheduleManager()
+
+    def test_validate_valid_keys(self):
+        valid_keys = ('one', 'two', 'three')
+        options = {'one': 1, 'two': 2, 'three': 3}
+        schedule_manager = ScheduleManager()
+        try:
+            schedule_manager._validate_keys(options, valid_keys)
+        except Exception, e:
+            self.fail(str(e))
+
+    def test_validate_invalid_superfluous_keys(self):
+        valid_keys = ('yes', 'ok')
+        options = {'ok': 1, 'not': 0}
+        schedule_manager = ScheduleManager()
+        self.assertRaises(pulp_exceptions.InvalidValue,
+                          schedule_manager._validate_keys,
+                          options, valid_keys)
+
+    def test_validate_invalid_missing_keys(self):
+        valid_keys = ('me', 'me_too')
+        options = {'me': 'only'}
+        schedule_manager = ScheduleManager()
+        self.assertRaises(pulp_exceptions.MissingValue,
+                          schedule_manager._validate_keys,
+                          options, valid_keys, True)
 
 # sync schedule tests ----------------------------------------------------------
 
 class ScheduledSyncTests(ScheduleTests):
-    pass
+
+    def test_create_schedule(self):
+        pass
+
+    def test_delete_schedule(self):
+        pass
+
+    def test_update_schedule(self):
+        pass
 
