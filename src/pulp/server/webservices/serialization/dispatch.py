e7bf16006e2c301f963d5823e99f8f27a4cf3139
Jason L Connor <jason.connor@gmail.com>
2012-05-01 23:12:42 -0600

    fixed up scheduled call serialization

--- src/pulp/server/webservices/serialization/dispatch.py
+++ src/pulp/server/webservices/serialization/dispatch.py
@@ -11,11 +11,9 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-from bson.objectid import ObjectId
+from datetime import datetime
 
 from pulp.common import dateutils
-from pulp.server.db.model.dispatch import ScheduledCall
-from pulp.server.dispatch.call import CallRequest
 
 
 def task_href(call_report):
@@ -38,12 +36,20 @@ def scheduled_call_obj(scheduled_call):
         'failure_threshold': scheduled_call['failure_threshold'],
         'enabled': scheduled_call['enabled'],
         '_consecutive_failures': scheduled_call['consecutive_failures'],
-        '_remaining_runs': scheduled_call['remaining_runs']
+        '_remaining_runs': scheduled_call['remaining_runs'],
+        '_first_run': None,
+        '_last_run': None,
+        '_next_run': None,
     }
-    last_run = dateutils.format_iso8601_datetime(scheduled_call['last_run'].replace(tzinfo=dateutils.utc_tz()))
-    obj['_last_run'] = last_run
-    next_run = dateutils.format_iso8601_datetime(scheduled_call['next_run'].replace(tzinfo=dateutils.utc_tz()))
-    obj['_next_run'] = next_run
+    first_run = scheduled_call['first_run']
+    if isinstance(first_run, datetime):
+        obj['_first_run'] = dateutils.format_iso8601_datetime(first_run.replace(tzinfo=dateutils.utc_tz()))
+    last_run = scheduled_call['last_run']
+    if isinstance(last_run, datetime):
+        obj['_last_run'] = dateutils.format_iso8601_datetime(last_run.replace(tzinfo=dateutils.utc_tz()))
+    next_run = scheduled_call['next_run']
+    if isinstance(next_run, datetime):
+        obj['_next_run'] = dateutils.format_iso8601_datetime(next_run.replace(tzinfo=dateutils.utc_tz()))
     return obj
 
 
