b0787af224007bad81dc69de6d21d885a17ec745
Jason L Connor <jason.connor@gmail.com>
2012-05-04 10:17:13 -0600

    changed aynchronous support to only set success or failure if the task is actually running changed asynchronous support to guaruntee only 1 call into coordinator changed asynchronous support to allow multiple calls into controllers per unittest so long as set_success or set_failure is called before each asynchronous controller is called

--- test/common/testutil.py
+++ test/common/testutil.py
@@ -416,39 +416,41 @@ class PulpV2WebserviceTest(PulpCoordinatorTest, PulpWebserviceTest):
         self.exception = None
         self.traceback = None
 
-    def tearDown(self):
-        super(PulpV2WebserviceTest, self).tearDown()
-        self.success_failure = None
-        self.result = None
-        self.exception = None
-        self.traceback = None
-
     def _do_request(self, request_type, uri, params, additional_headers, serialize_json=True):
         """
         Override the base class controller to allow for less deterministic
         responses due to integration with the dispatch package.
         """
         def _poll_async_request(status, body):
-            if self.success_failure is not None:
+            if self.success_failure is not None and body['state'] == dispatch_constants.CALL_RUNNING_STATE:
                 task_id = body['_href'].split('/')[-2]
                 if self.success_failure == 'success':
                     self.coordinator.complete_call_success(task_id, self.result)
                 else:
                     self.coordinator.complete_call_failure(task_id, self.exception, self.traceback)
-            while status in (httplib.ACCEPTED, httplib.OK) and \
-                  body['state'] not in dispatch_constants.CALL_COMPLETE_STATES:
+                self._reset_success_failure()
+
+            while status in (httplib.ACCEPTED, httplib.OK) and body['state'] not in dispatch_constants.CALL_COMPLETE_STATES:
                 uri = body['_href'][9:]
                 status, body = self.get(uri) # cool recursive call
+
             if status != httplib.OK:
                 return status, None
             if body['state'] == dispatch_constants.CALL_ERROR_STATE:
                 return status, body['exception']
             return status, body['result']
 
+        # _do_request actually starts here
         status, body = PulpWebserviceTest._do_request(self, request_type, uri, params, additional_headers, serialize_json=serialize_json)
         if status == httplib.ACCEPTED:
             return _poll_async_request(status, body)
         return status, body
+
+    def _reset_success_failure(self):
+        self.success_failure = None
+        self.result = None
+        self.exception = None
+        self.traceback = None
 
     def set_success(self, result=None):
         self.success_failure = 'success'
