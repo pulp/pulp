92a41db3c9f8faa93df04b4b11990a1d1f69b961
Jeff Ortel <jortel@redhat.com>
2012-05-24 15:47:47 -0500

    Add linux system handler to git.

---
+++ handlers/linux.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2011 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+import os
+from pulp.gc_client.agent.lib.handler import SystemHandler
+from pulp.gc_client.agent.lib.report import RebootReport
+
+
+class LinuxHandler(SystemHandler):
+    """
+    Linux system handler
+    """
+
+    def reboot(self, options={}):
+        """
+        Schedule a system reboot.
+        """
+        report = RebootReport()
+        apply = options.get('apply', True)
+        if apply:
+            minutes = options.get('minutes', 1)
+            command = 'shutdown -r +%d' % minutes
+            log.info(command)
+            os.system(command)
+        report.succeeded()
+        return report
