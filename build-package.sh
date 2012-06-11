b17f31b804bf8cdc1ea7248377bcd1bed1f88832
Jay Dobies <jason.dobies@redhat.com>
2012-06-11 13:53:09 -0400

    Removed unused scripts

--- build-package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd $1
-echo BUILDING $1
-tito build --$2
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf /tmp/tito/*
-find -name "*.spec" -printf "%h"\\n |xargs -n 1 -I foo ./build-package.sh foo rpm
