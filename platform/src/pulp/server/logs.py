329bc7de4f7ac80c93b31e2ad3898413d2297a6a
Jason L Connor <jason.connor@gmail.com>
2012-06-14 17:40:24 -0600

    remove auditing logging

--- platform/src/pulp/server/logs.py
+++ platform/src/pulp/server/logs.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2010 Red Hat, Inc.
+# Copyright © 2010-2012 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
@@ -14,31 +14,8 @@
 import logging
 import logging.config
 import os
-import sys
-from logging import handlers
 
 from pulp.server import config
-
-
-TIME = '%(asctime)s'
-LEVEL = ' [%(levelname)s]'
-THREAD = '[%(threadName)s]'
-FUNCTION = ' %(funcName)s()'
-FILE = ' @ %(filename)s'
-LINE = ':%(lineno)d'
-MSG = ' - %(message)s'
-
-if sys.version_info < (2,5):
-    FUNCTION = ''
-
-FMT = \
-    ''.join((TIME,
-            LEVEL,
-            THREAD,
-            FUNCTION,
-            FILE,
-            LINE,
-            MSG,))
 
 
 # logging configuration -------------------------------------------------------
@@ -80,34 +57,6 @@ def configure_pulp_logging():
     logging.config.fileConfig(log_config_filename)
     _enable_all_loggers() # Hack needed for RHEL-5
 
-def configure_audit_logging():
-    """
-    Pull the audit logging configuration from the global config and/or default
-    config and initialize pulp's audit logging.
-    """
-    file = config.config.get('auditing', 'events_file')
-    check_log_file(file)
-    units = 'D'
-    backups = config.config.getint('auditing', 'backups')
-    lifetime = config.config.getint('auditing', 'lifetime')
-
-    # the logging module will get into an infinite loop if the interval is 0
-    if lifetime <= 0:
-        units = 'H'
-        lifetime = 1
-
-    # NOTE, this cannot be a descendant of the pulp log as it will inherit
-    # pulp's rotating log and handler and log to both files. Yes, I've tried
-    # removing the handler to no avail...
-    logger = logging.getLogger('auditing')
-    logger.setLevel(logging.INFO)
-    logger.propagate = 0 # Disables auditing going to regular log output
-    handler = handlers.TimedRotatingFileHandler(file,
-                                                when=units,
-                                                interval=lifetime,
-                                                backupCount=backups)
-    logger.addHandler(handler)
-
 # pulp logging api ------------------------------------------------------------
 
 started = False
@@ -121,7 +70,6 @@ def start_logging():
     if started:
         return
     configure_pulp_logging()
-    configure_audit_logging()
     started = True
 
 
