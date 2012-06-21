2ad6928f75abc368fc4552cd6811b38abedb40c6
John Matthews <jmatthews@redhat.com>
2012-06-21 12:14:58 -0400

    Update playpen mongo script to use pulp_database

--- playpen/mongodb/display_repos.py
+++ playpen/mongodb/display_repos.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     options, args = parser.parse_args()
 
     connection = Connection()
-    db = connection._database
+    db = connection.pulp_database
     if options.autoref:
         db.add_son_manipulator(NamespaceInjector())
         db.add_son_manipulator(AutoReference(db))
@@ -29,24 +29,6 @@ if __name__ == "__main__":
     if options.num_repos:
         found_slice = found[:int(options.num_repos)]
     for r in found_slice:
-        print "\nRepo: "
-        print "\tid=%s name=%s arch=%s" % (r["id"], r["name"], r["arch"])
-        print "\tsource=%s" % (r["source"])
-        print "\t# groups=%s" % (len(r["packagegroups"]))
-        print "\t# categories=%s" % (len(r["packagegroupcategories"]))
-        print "\t%s packages" % (len(r["packages"]))
-        for key in r["packages"].keys()[:int(options.num_packages)]:
-            pkg = r["packages"][key]
-            #While the model is changing we will try to detect what data is in mongo
-            #later when we have stabilized on the model we shall use these if checks 
-            #should be removed
-            if type(pkg) == type({}):
-                if pkg.has_key("repoid"):
-                    print "\tPackage: <%s> in repo <%s>" % (pkg["packageid"], pkg["repoid"])
-                else:
-                    print "\tPackage: <%s> in repo <%s>" % (pkg["packageid"], r["id"])
-                print "\tversions = \n\t\t%s" % (pkg["versions"])
-            else:
-                print "\tPackage: %s" % (pkg)
+        print "\nRepo: ", r
 
 
