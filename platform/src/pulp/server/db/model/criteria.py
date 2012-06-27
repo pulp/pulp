5e011af2b59713f19fe962f6bd2991e217992f51
Jason L Connor <jason.connor@gmail.com>
2012-06-26 20:53:57 -0600

    preliminary implementation of Criteria model

---
+++ platform/src/pulp/server/db/model/criteria.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the License
+# (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied, including the
+# implied warranties of MERCHANTABILITY, NON-INFRINGEMENT, or FITNESS FOR A
+# PARTICULAR PURPOSE.
+# You should have received a copy of GPLv2 along with this software; if not,
+# see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+import copy
+import re
+import sys
+from types import NoneType
+
+import pymongo
+
+from pulp.server import exceptions as pulp_exceptions
+from pulp.server.db.model.base import Model
+
+
+_VALID_SORT_DIRECTIONS = (pymongo.ASCENDING, pymongo.DESCENDING)
+
+# criteria model ---------------------------------------------------------------
+
+class Criteria(Model):
+
+    collection_name = 'criteria'
+
+    def __init__(self, filters=None, sort=None, limit=None, skip=None, fields=None):
+        super(Criteria, self).__init__()
+
+        assert isinstance(filters, (dict, NoneType))
+        assert isinstance(sort, (tuple, NoneType))
+        assert isinstance(limit, (int, NoneType))
+        assert isinstance(skip, (int, NoneType))
+        assert isinstance(fields, (list, NoneType))
+
+        self.filters = filters
+        self.sort = sort
+        self.limit = limit
+        self.skip = skip
+        self.fields = fields
+
+    @classmethod
+    def from_json_doc(cls, doc):
+        doc = copy.copy(doc)
+        filters = _validate_filters(doc.pop('filters', None))
+        sort = _validate_sort(doc.pop('sort', None))
+        limit = _validate_limit(doc.pop('limit', None))
+        skip = _validate_skip(doc.pop('skip', None))
+        fields = _validate_fields(doc.pop('fields', None))
+        if doc:
+            raise pulp_exceptions.InvalidValue(doc.keys())
+        return cls(filters, sort, limit, skip, fields)
+
+    @property
+    def spec(self):
+        if self.filters is None:
+            return None
+
+        def _compile_regexs_for_not(spec):
+            if not isinstance(spec, (dict, list, tuple)):
+                return
+            if isinstance(spec, (list, tuple)):
+                map(_compile_regexs_for_not, spec)
+                return
+            for key, value in spec.items():
+                if key == '$not' and isinstance(value, basestring):
+                    # XXX is this the right kind of compilation?
+                    # shouldn't we be supporting Perl-compatible regexs?
+                    spec[key] = re.compile(value)
+                _compile_regexs_for_not(value)
+
+        spec = copy.copy(self.filters)
+        _compile_regexs_for_not(spec)
+        return spec
+
+# validation helper functions --------------------------------------------------
+
+def _validate_filters(filters):
+    if filters is None:
+        return None
+    if not isinstance(filters, dict):
+        raise pulp_exceptions.InvalidValue(['filters'])
+    return filters
+
+
+def _validate_sort(sort):
+    if sort is None:
+        return None
+    try:
+        valid_sort = []
+        for entry in sort:
+            if not isinstance(entry[0], basestring):
+                raise TypeError()
+            direction = None
+            if entry[1].lower() in ('ascending', '1'):
+                direction = pymongo.ASCENDING
+            if entry[1].lower() in ('descending', '-1'):
+                direction = pymongo.DESCENDING
+            if direction is None:
+                raise ValueError()
+            valid_sort.append((entry[0], direction))
+    except (TypeError, ValueError):
+       raise pulp_exceptions.InvalidValue(['sort']), None, sys.exc_info()[2]
+    else:
+        return tuple(valid_sort)
+
+
+def _validate_limit(limit):
+    if limit is None:
+        return None
+    try:
+        limit = int(limit)
+        if limit < 1:
+            raise TypeError()
+    except TypeError:
+        raise pulp_exceptions.InvalidValue(['limit']), None, sys.exc_info()[2]
+    else:
+        return limit
+
+
+def _validate_skip(skip):
+    if skip is None:
+        return None
+    try:
+        skip = int(skip)
+        if skip < 0:
+            raise TypeError()
+    except TypeError:
+        raise pulp_exceptions.InvalidValue(['skip']), None, sys.exc_info()[2]
+    else:
+        return skip
+
+
+def _validate_fields(fields):
+    if fields is None:
+        return None
+    try:
+        fields = list(fields)
+        for f in fields:
+            if not isinstance(f, basestring):
+                raise TypeError()
+    except TypeError:
+        raise pulp_exceptions.InvalidValue(['fields']), None, sys.exc_info()[2]
+    return fields
+
