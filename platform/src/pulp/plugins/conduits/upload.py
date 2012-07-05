2350008b446f31f4d6c92eb86a33f97220c9fcae
Pradeep Kilambi <pkilambi@redhat.com>
2012-07-05 16:42:44 -0400

    fixing upload to include get units and needed summary info

--- platform/src/pulp/plugins/conduits/upload.py
+++ platform/src/pulp/plugins/conduits/upload.py
@@ -11,11 +11,12 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-from pulp.plugins.conduits.mixins import AddUnitMixin
+from pulp.plugins.conduits.mixins import AddUnitMixin, GetRepoUnitsMixin
 
-class UploadConduit(AddUnitMixin):
+class UploadConduit(AddUnitMixin, GetRepoUnitsMixin):
 
     def __init__(self, repo_id, importer_id, association_owner_type,
                  association_owner_id):
         AddUnitMixin.__init__(self, repo_id, importer_id,
                               association_owner_type, association_owner_id)
+        GetRepoUnitsMixin.__init__(self, repo_id)
