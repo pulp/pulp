25cd4d9c3a5c6ad6473548e79ae851e22101c355
Jeff Ortel <jortel@redhat.com>
2012-07-13 10:12:49 -0500

    Refit yum plugin for v2.

--- rpm-support/usr/lib/yum-plugins/pulp-profile-update.py
+++ rpm-support/usr/lib/yum-plugins/pulp-profile-update.py
@@ -10,67 +10,67 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
+
 import os
 from yum.plugins import TYPE_CORE
-from pulp.client.consumer.credentials import Consumer as ConsumerBundle
-from pulp.client.api.consumer import ConsumerAPI
 from rhsm.profile import get_profile
-from pulp.client.api.server import PulpServer, set_active_server
-from pulp.client.consumer.config import ConsumerConfig
+from pulp.bindings.server import PulpConnection
+from pulp.bindings.bindings import Bindings
+from pulp.common.bundle import Bundle as BundleImpl
+from pulp.common.config import Config
 
 requires_api_version = '2.5'
 plugin_type = (TYPE_CORE,)
+cfg = Config('/etc/pulp/consumer/consumer.conf')
+cfg = cfg.graph()
 
-def get_consumer():
-    """
-    Get consumer bundle
-    """
-    bundle = ConsumerBundle()
-    return bundle
+#
+# Pulp Integration
+#
 
-def pulpserver():
+class Bundle(BundleImpl):
     """
-    Pulp server configuration
+    Consumer certificate (bundle)
     """
-    cfg = ConsumerConfig()
-    bundle = get_consumer()
-    pulp = PulpServer(cfg.server.host, timeout=10)
-    pulp.set_ssl_credentials(bundle.crtpath())
-    set_active_server(pulp)
 
-def update_consumer_profile(cid):
+    def __init__(self):
+        path = os.path.join(
+            cfg.filesystem.id_cert_dir,
+            cfg.filesystem.id_cert_filename)
+        BundleImpl.__init__(self, path)
+
+
+class PulpBindings(Bindings):
     """
-    Updates consumer package profile information
-    @param cid: Consumer ID
-    @type cid: str
+    Pulp (REST) API.
     """
-    pulpserver()
-    capi = ConsumerAPI()
-    pkginfo = get_profile("rpm").collect()
-    capi.package_profile(cid, pkginfo)
+
+    def __init__(self):
+        host = cfg.server.host
+        port = int(cfg.server.port)
+        cert = os.path.join(
+            cfg.filesystem.id_cert_dir,
+            cfg.filesystem.id_cert_filename)
+        connection = PulpConnection(host, port, cert_filename=cert)
+        Bindings.__init__(self, connection)
+
+#
+# yum plugin
+#
 
 def posttrans_hook(conduit):
     """
-    Update Package Profile for available consumer.
+    Send content unit profile to Pulp.
     """
-    #get configuration
-    verbose = conduit.confBool("main", "verbose", default=1)
-    if os.getuid() != 0:
-        if verbose:
-            conduit.info(2, 'Not root, Pulp consumer profile not updated')
-        return
-    if hasattr(conduit, 'registerPackageName'):
-        conduit.registerPackageName("pulp-client")
     try:
-        bundle = get_consumer()
-        cid = bundle.getid()
-        if not cid:
-            if verbose:
-                conduit.info(2, "Consumer Id could not be found. Cannot update consumer profile.")
-            return
-        update_consumer_profile(cid)
-        if verbose:
-            conduit.info(2, "Profile updated successfully for consumer [%s]" % cid)
+        bundle = Bundle()
+        myid = bundle.cn()
+        if not myid:
+            return # not registered
+        bindings = PulpBindings()
+        profile = get_profile('rpm').collect()
+        http = bindings.profile.send(myid, 'rpm', profile)
+        msg = 'pulp: profile sent, status=%d' % http.response_code
+        conduit.info(2, msg)
     except Exception, e:
         conduit.error(2, str(e))
-
