5f787776afdc3e5b0ae8f8cc2621f5939f38db11
Jay Dobies <jason.dobies@redhat.com>
2012-07-19 14:03:34 -0400

    Updated test to remove mixin functionality tests

--- platform/test/unit/test_repo_publish_conduit.py
+++ platform/test/unit/test_repo_publish_conduit.py
@@ -12,7 +12,6 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-# Python
 import datetime
 import mock
 
@@ -21,23 +20,10 @@ import mock_plugins
 
 from pulp.common import dateutils
 from pulp.plugins.conduits.mixins import DistributorConduitException
-from pulp.plugins.conduits.repo_publish import RepoPublishConduit
-from pulp.plugins.model import Unit, PublishReport
-import pulp.plugins.types.database as types_database
-import pulp.plugins.types.model as types_model
-from pulp.server.db.model.repository import Repo, RepoContentUnit, RepoDistributor
-import pulp.server.managers.repo.cud as repo_manager
-import pulp.server.managers.repo.distributor as distributor_manager
-import pulp.server.managers.repo.publish as publish_manager
-import pulp.server.managers.repo.unit_association as association_manager
-import pulp.server.managers.repo.unit_association_query as association_query_manager
-import pulp.server.managers.content.cud as content_manager
-import pulp.server.managers.content.query as query_manager
-
-# constants --------------------------------------------------------------------
-
-TYPE_1_DEF = types_model.TypeDefinition('type_1', 'Type 1', 'One', ['key-1'], ['search-1'], ['type-2'])
-TYPE_2_DEF = types_model.TypeDefinition('type_2', 'Type 2', 'Two', ['key-2a', 'key-2b'], [], ['type-1'])
+from pulp.plugins.conduits.repo_publish import RepoPublishConduit, RepoGroupPublishConduit
+from pulp.server.db.model.repo_group import RepoGroup, RepoGroupDistributor
+from pulp.server.db.model.repository import Repo, RepoDistributor
+from pulp.server.managers import factory as manager_factory
 
 # -- test cases ---------------------------------------------------------------
 
@@ -45,23 +31,17 @@ class RepoPublishConduitTests(base.PulpServerTests):
 
     def clean(self):
         super(RepoPublishConduitTests, self).clean()
-        types_database.clean()
 
-        RepoContentUnit.get_collection().remove()
         Repo.get_collection().remove()
+        RepoDistributor.get_collection().remove()
 
     def setUp(self):
         super(RepoPublishConduitTests, self).setUp()
         mock_plugins.install()
-        types_database.update_database([TYPE_1_DEF, TYPE_2_DEF])
+        manager_factory.initialize()
 
-        self.repo_manager = repo_manager.RepoManager()
-        self.distributor_manager = distributor_manager.RepoDistributorManager()
-        self.publish_manager = publish_manager.RepoPublishManager()
-        self.association_manager = association_manager.RepoUnitAssociationManager()
-        self.association_query_manager = association_query_manager.RepoUnitAssociationQueryManager()
-        self.content_manager = content_manager.ContentManager()
-        self.query_manager = query_manager.ContentQueryManager()
+        self.repo_manager = manager_factory.repo_manager()
+        self.distributor_manager = manager_factory.repo_distributor_manager()
 
         # Populate the database with a repo with units
         self.repo_manager.create_repo('repo-1')
@@ -69,43 +49,11 @@ class RepoPublishConduitTests(base.PulpServerTests):
 
         self.conduit = RepoPublishConduit('repo-1', 'dist-1')
 
-        for i in range(0, 100):
-            unit_id = 'unit_%d' % i
-            self.content_manager.add_content_unit(TYPE_1_DEF.id, unit_id, {'key-1' : 'value_%d' % i})
-            self.association_manager.associate_unit_by_id('repo-1', TYPE_1_DEF.id, unit_id, association_manager.OWNER_TYPE_IMPORTER, 'test-importer')
-
     def test_str(self):
         """
         Makes sure the __str__ implementation doesn't crash.
         """
         str(self.conduit)
-
-    def test_get_units(self):
-        """
-        Ensures all the units are returned and in the correct transfer object.
-        """
-
-        # Test
-        units = self.conduit.get_units()
-
-        # Verify
-        self.assertEqual(100, len(units))
-        self.assertTrue(isinstance(units[0], Unit)) # make sure its the transfer object
-
-    def test_get_units_no_units(self):
-        """
-        Makes sure nothing weird happens when there are no associated units.
-        """
-
-        # Setup
-        for i in range(0, 100):
-            self.association_manager.unassociate_unit_by_id('repo-1', TYPE_1_DEF.id, 'unit_%d' % i, association_manager.OWNER_TYPE_IMPORTER, 'test-importer')
-
-        # Test
-        units = self.conduit.get_units()
-
-        # Verify
-        self.assertEqual(0, len(units))
 
     def test_last_publish(self):
         """
@@ -127,35 +75,60 @@ class RepoPublishConduitTests(base.PulpServerTests):
         self.assertTrue(isinstance(found, datetime.datetime)) # check returned format
         self.assertEqual(repo_dist['last_publish'], dateutils.format_iso8601_datetime(found))
 
-    def test_build_report(self):
-        """
-        Tests the conduit utility method for putting together the publish report.
-        """
-
-        # Test
-        report = self.conduit.build_success_report('summary', 'details')
-
-        # Verify
-        self.assertTrue(isinstance(report, PublishReport))
-        self.assertEqual(report.summary, 'summary')
-        self.assertEqual(report.details, 'details')
-
-    # -- errors tests ---------------------------------------------------------
-
-    @mock.patch('pulp.server.managers.repo.unit_association_query.RepoUnitAssociationQueryManager.get_units_across_types')
-    def test_get_units_with_error(self, mock_query):
-        # Setup
-        mock_query.side_effect = Exception()
-
-        # Test
-        try:
-            self.conduit.get_units()
-            self.fail('Exception expected')
-        except DistributorConduitException, e:
-            print(e) # for coverage
-
     @mock.patch('pulp.server.managers.repo.publish.RepoPublishManager.last_publish')
     def test_last_publish_with_error(self, mock_call):
+        # Setup
+        mock_call.side_effect = Exception()
+
+        # Test
+        self.assertRaises(DistributorConduitException, self.conduit.last_publish)
+
+class RepoGroupPublishConduitTests(base.PulpServerTests):
+    def clean(self):
+        super(RepoGroupPublishConduitTests, self).clean()
+
+        RepoGroup.get_collection().remove()
+        RepoGroupDistributor.get_collection().remove()
+
+    def setUp(self):
+        super(RepoGroupPublishConduitTests, self).setUp()
+        mock_plugins.install()
+        manager_factory.initialize()
+
+        self.group_manager = manager_factory.repo_group_manager()
+        self.distributor_manager = manager_factory.repo_group_distributor_manager()
+
+        self.group_id = 'conduit-group'
+        self.distributor_id = 'conduit-distributor'
+
+        self.group_manager.create_repo_group(self.group_id)
+        self.distributor_manager.add_distributor(self.group_id, 'mock-group-distributor', {}, distributor_id=self.distributor_id)
+
+        self.conduit = RepoGroupPublishConduit(self.group_id, self.distributor_id)
+
+    def test_str(self):
+        str(self.conduit) # make sure no exception is raised
+
+    def test_last_publish(self):
+        # Test - Unpublished
+        unpublished = self.conduit.last_publish()
+        self.assertTrue(unpublished is None)
+
+        # Setup - Publish
+        last_publish = datetime.datetime.now()
+        repo_group_dist = self.distributor_manager.get_distributor(self.group_id, self.distributor_id)
+        repo_group_dist['last_publish'] = dateutils.format_iso8601_datetime(last_publish)
+        RepoGroupDistributor.get_collection().save(repo_group_dist, safe=True)
+
+        # Test
+        found = self.conduit.last_publish()
+        self.assertTrue(isinstance(found, datetime.datetime))
+
+        last_publish = dateutils.parse_iso8601_datetime(repo_group_dist['last_publish']) # simulate the DB encoding
+        self.assertEqual(last_publish, found)
+
+    @mock.patch('pulp.server.managers.repo.group.publish.RepoGroupPublishManager.last_publish')
+    def test_last_publish_server_error(self, mock_call):
         # Setup
         mock_call.side_effect = Exception()
 
