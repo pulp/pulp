84d70998e52129c8bb5de243ea852570ec5f0082
Jay Dobies <jason.dobies@redhat.com>
2012-07-19 10:03:24 -0400

    841584 - Temporarily turn the generic bind extension into RPM

--- builtins/extensions/consumer/pulp_consumer/pulp_cli.py
+++ builtins/extensions/consumer/pulp_consumer/pulp_cli.py
@@ -206,7 +206,8 @@ class BindCommand(PulpCliCommand):
         self.prompt = context.prompt
 
         self.add_option(PulpCliOption('--repo-id', 'repository id', required=True))
-        self.add_option(PulpCliOption('--distributor-id', 'distributor id', required=True))
+        # Temporarily disabling until I have time to make a proper RPM consumer extension
+        # self.add_option(PulpCliOption('--distributor-id', 'distributor id', required=True))
 
     def bind(self, **kwargs):
         consumer_id = load_consumer_id(self.context)
@@ -214,7 +215,9 @@ class BindCommand(PulpCliCommand):
             self.prompt.render_failure_message("This consumer is not registered to the Pulp server.")
             return
         repo_id = kwargs['repo-id']
-        distributor_id = kwargs['distributor-id']
+        # Temporarily disabling until I have time to make a proper RPM consumer extension
+        # distributor_id = kwargs['distributor-id']
+        distributor_id = 'yum_distributor'
         try:
             self.context.server.bind.bind(consumer_id, repo_id, distributor_id)
             self.prompt.render_success_message('Consumer [%s] successfully bound to repository distributor [%s : %s]' % (consumer_id, repo_id, distributor_id))
