5cac63cd0fba10fcfac0b63f8529c94d0c4a9f7c
Wes Hayutin <whayutin@redhat.com>
2012-07-30 12:06:59 -0400

    Revert "842859 adding a default value for the ssl_ca_cert for rhui on the rhui branch"
    This reverts commit f2464d6aebfcd8973be431d99e317172b405c769.
    
    The proper fix was determined to be in screens/client.py :
            answers['ca_cert'] = self.config.get("rhua", "ssl_ca_cert", '/etc/pki/rhua/rhua-ssl-ca-cert.crt')
            self.prompt.write('')

--- src/pulp/server/api/cds.py
+++ src/pulp/server/api/cds.py
@@ -16,7 +16,6 @@ import datetime
 import logging
 import re
 import sys
-import os
 
 # Pulp
 from pulp.common import dateutils
@@ -523,8 +522,6 @@ class CdsApi(BaseApi):
         server_ca_certificate = None
         if config.config.has_option('security', 'ssl_ca_certificate'):
             ca_cert_file = config.config.get('security', 'ssl_ca_certificate')
-        elif os.path.isfile('/etc/pki/rhua/rhua-ssl-ca-cert.crt'):
-            ca_cert_file = '/etc/pki/rhua/rhua-ssl-ca-cert.crt'
 
             try:
                 f = open(ca_cert_file, 'r')
