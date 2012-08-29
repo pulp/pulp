f127db7c455d97d63a69a5f035a24ba118f22aa4
Jay Dobies <jason.dobies@redhat.com>
2012-08-29 12:40:22 -0400

    Fixed PEP-8 issues

--- puppet-support/test/unit/base_cli.py
+++ puppet-support/test/unit/base_cli.py
@@ -10,18 +10,19 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-import logging
 
+import logging
 import mock
 import unittest
+
 import okaara
 
 from pulp.bindings.bindings import Bindings
 from pulp.bindings.server import PulpConnection
 from pulp.client.extensions.core import PulpPrompt, ClientContext, PulpCli
 from pulp.client.extensions.exceptions import ExceptionHandler
-
 from pulp.common.config import Config
+
 
 class ExtensionTests(unittest.TestCase):
     """
