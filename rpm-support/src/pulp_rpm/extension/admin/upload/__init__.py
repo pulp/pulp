f78994b59f4066d579fc654e2df25f9895d4f9d0
Jay Dobies <jason.dobies@redhat.com>
2012-09-24 10:53:38 -0400

    Refactored upload-related commands into site-packages

--- rpm-support/src/pulp_rpm/extension/admin/upload/__init__.py
+++ rpm-support/src/pulp_rpm/extension/admin/upload/__init__.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2012 Red Hat, Inc.
+# Copyright (c) 2012 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
@@ -10,5 +8,3 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
-PRIORITY = 4 # after built-ins, before default
