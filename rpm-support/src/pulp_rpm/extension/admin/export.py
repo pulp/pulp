fc714dc189a84b9629a022626c5756e64e23f866
Sayli Karmarkar <skarmark@redhat.com>
2012-09-25 10:25:23 -0700

    removing utf-8 encoding of files according to PEP-8 standard

--- rpm-support/src/pulp_rpm/extension/admin/export.py
+++ rpm-support/src/pulp_rpm/extension/admin/export.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2012 Red Hat, Inc.
+# Copyright (c) 2012 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
