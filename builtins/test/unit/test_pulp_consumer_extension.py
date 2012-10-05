43a44c46b559608eecd18c74a0b6ff809f01bebf
Jeff Ortel <jortel@redhat.com>
2012-10-04 19:07:46 -0500

    860425 - fix consumer update() passing display-name instead of display_name.

--- builtins/test/unit/test_pulp_consumer_extension.py
+++ builtins/test/unit/test_pulp_consumer_extension.py
@@ -10,6 +10,7 @@
 # NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
 import os
 import sys
 
@@ -17,16 +18,18 @@ import mock
 
 import base_builtins
 
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)),
-    '../../extensions/admin/'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../extensions/admin/'))
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../extensions/consumer/'))
 
-from pulp_admin_consumer import pulp_cli
-from okaara.cli import CommandUsage
+from pulp_admin_consumer import pulp_cli as admin_client
+from pulp_consumer import pulp_cli as consumer_client
+
 
 class TestConsumerSearch(base_builtins.PulpClientTests):
+
     def setUp(self):
         super(TestConsumerSearch, self).setUp()
-        self.consumer_section = pulp_cli.AdminConsumerSection(self.context)
+        self.consumer_section = admin_client.AdminConsumerSection(self.context)
 
     def test_has_command(self):
         """
@@ -55,3 +58,58 @@ class TestConsumerSearch(base_builtins.PulpClientTests):
 
     def test_invalid_input(self):
         self.assertRaises(ValueError, self.consumer_section.search, x=2)
+
+
+class TestAdminConsumer(base_builtins.PulpClientTests):
+
+    CONSUMER_ID = 'elvis'
+    DISPLAY_NAME = 'The King'
+    DESCRIPTION = 'The King, Elvis Presley'
+    NOTES = {'age':'99', 'status':'dead'}
+
+    @mock.patch('pulp.bindings.consumer.ConsumerAPI.update')
+    def test_update(self, mock_binding):
+        # Setup
+        section = admin_client.AdminConsumerSection(self.context)
+        options = {
+            'consumer-id' : self.CONSUMER_ID,
+            'display-name' : self.DISPLAY_NAME,
+            'description' : self.DESCRIPTION,
+            'note' : ['='.join(n) for n in self.NOTES.items()],
+        }
+        # Test
+        section.update(**options)
+        # Verify
+        passed = dict(
+            display_name=self.DISPLAY_NAME,
+            description=self.DESCRIPTION,
+            notes=self.NOTES)
+        mock_binding.assert_called_with(self.CONSUMER_ID, passed)
+
+
+class TestConsumerConsumer(base_builtins.PulpClientTests):
+
+    CONSUMER_ID = 'elvis'
+    DISPLAY_NAME = 'The King'
+    DESCRIPTION = 'The King, Elvis Presley'
+    NOTES = {'age':'99', 'status':'dead'}
+
+    @mock.patch('pulp.bindings.consumer.ConsumerAPI.update')
+    @mock.patch('pulp_consumer.pulp_cli.load_consumer_id', return_value=CONSUMER_ID)
+    def test_update(self, mock_utils, mock_binding):
+        # Setup
+        command = consumer_client.UpdateCommand(self.context, '', '')
+        options = {
+            'display-name' : self.DISPLAY_NAME,
+            'description' : self.DESCRIPTION,
+            'note' : ['='.join(n) for n in self.NOTES.items()],
+            }
+        # Test
+        command.update(**options)
+        # Verify
+        # Verify
+        passed = dict(
+            display_name=self.DISPLAY_NAME,
+            description=self.DESCRIPTION,
+            notes=self.NOTES)
+        mock_binding.assert_called_with(self.CONSUMER_ID, passed)
