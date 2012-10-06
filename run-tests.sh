bc94df12384e6cf9534f83a641711411856daf5f
Michael Hrivnak <mhrivnak@redhat.com>
2012-10-06 16:30:39 -0400

    deleting puppet support

--- run-tests.sh
+++ run-tests.sh
@@ -12,20 +12,7 @@ PACKAGES="$PACKAGES,pulp_repo"
 PACKAGES="$PACKAGES,pulp_server_info"
 PACKAGES="$PACKAGES,pulp_tasks"
 
-# RPM Support
-PACKAGES="$PACKAGES,rpm_admin_consumer"
-PACKAGES="$PACKAGES,rpm_repo"
-PACKAGES="$PACKAGES,rpm_sync"
-PACKAGES="$PACKAGES,rpm_units_copy"
-PACKAGES="$PACKAGES,rpm_units_search"
-PACKAGES="$PACKAGES,rpm_upload"
-PACKAGES="$PACKAGES,yum_distributor"
-PACKAGES="$PACKAGES,yum_importer"
-
-# Puppet Support
-PACKAGES="$PACKAGES,pulp_puppet"
-
 # Test Directories
-TESTS="platform/test/unit builtins/test/unit rpm-support/test/unit puppet-support/test/unit"
+TESTS="platform/test/unit builtins/test/unit "
 
 nosetests --with-coverage --cover-html --cover-erase --cover-package $PACKAGES $TESTS
