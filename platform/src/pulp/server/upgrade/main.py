3d301ce63a8f0502b8294e14ef1baa9ec7472e36
Jay Dobies <jason.dobies@redhat.com>
2012-10-18 16:33:40 -0400

    Basic tests for the upgrade main script

--- platform/src/pulp/server/upgrade/main.py
+++ platform/src/pulp/server/upgrade/main.py
@@ -17,7 +17,6 @@ Methods that drive the entire v1 to v2 upgrade process.
 from gettext import gettext as _
 import logging
 import os
-import sys
 
 from okaara.prompt import Prompt
 from okaara.progress import ThreadedSpinner
@@ -47,7 +46,6 @@ DEFAULT_SEEDS = 'localhost:27017'
 
 _LOG = logging.getLogger(__name__)
 
-# -- classes ------------------------------------------------------------------
 
 class UpgradeStepReport(object):
     """
@@ -84,115 +82,115 @@ class InvalidStepReportException(Exception): pass
 class StepException(Exception):
 
     def __init__(self, step_description):
-        Exception.__init__(step_description)
+        Exception.__init__(self, step_description)
         self.step_description = step_description
 
-# -- upgrade operations -------------------------------------------------------
-
-def upgrade(stream_file=STREAM_FILE,
-            db_name=PULP_DATABASE_NAME,
-            db_seeds=DEFAULT_SEEDS,
-            db_upgrade_calls=DB_UPGRADE_CALLS):
+class Upgrader(object):
     """
-    Performs the upgrade process if the installation qualifies to be upgraded.
-
-    :param stream_file: full location to the file containing stream information;
-           the file does not necessarily have to exist
-    :type  stream_file: str
-    :param db_upgrade_calls: dictates which database upgrade steps will be performed;
-           see DB_UPGRADE_CALLS comment above for a description of the entries
-    :type  db_upgrade_calls: list
-
-    :return: nothing if the upgrade was successful
-
-    :raise InvalidStepReportException: if any of the steps does not properly
-           indicate its success or failure; should only occur during development
-    :raise StepException: if a step raises an error or indicates it failed
+    :ivar stream_file: full location to the file containing stream information;
+          the file does not necessarily have to exist
+    :type stream_file: str
+    :ivar db_upgrade_calls: dictates which database upgrade steps will be performed;
+          see DB_UPGRADE_CALLS comment above for a description of the entries
+    :type db_upgrade_calls: list
     """
 
-    prompt = Prompt(output=_UberWriter())
+    def __init__(self,
+                 stream_file=STREAM_FILE,
+                 db_name=PULP_DATABASE_NAME,
+                 db_seeds=DEFAULT_SEEDS,
+                 db_upgrade_calls=DB_UPGRADE_CALLS):
+        self.stream_file = stream_file
 
-    if not _is_v1(stream_file):
-        prompt.write(_('Pulp installation is already upgraded to the latest version'))
-        return
+        self.db_name = db_name
+        self.db_seeds = db_seeds
+        self.db_upgrade_calls = db_upgrade_calls
 
-    _upgrade_database(prompt, db_name, db_seeds, db_upgrade_calls)
+        self.prompt = Prompt()
 
+    def upgrade(self):
+        """
+        Performs the upgrade process if the installation qualifies to be upgraded.
 
-def _upgrade_database(prompt, name, seeds, upgrade_calls):
-    prompt.write(_('Upgrading Database'))
+        :return: nothing if the upgrade was successful
 
-    database = _database(name, seeds)
+        :raise InvalidStepReportException: if any of the steps does not properly
+               indicate its success or failure; should only occur during development
+        :raise StepException: if a step raises an error or indicates it failed
+        """
 
-    for db_call, description in upgrade_calls:
-        prompt.write(_('Upgrading: %(d)') % description)
-        spinner = ThreadedSpinner(prompt)
-        spinner.start()
+        if not self._is_v1():
+            self._print(_('Pulp installation is already upgraded to the latest version'))
+            return
 
-        report = UpgradeStepReport()
-        try:
-            db_call(database, report)
-        except:
+        self._upgrade_database()
+
+    def _upgrade_database(self):
+        self._print(_('Upgrading Database'))
+
+        database = self._database(self.db_name, self.db_seeds)
+
+        for db_call, description in self.db_upgrade_calls:
+            self._print(_('Upgrading: %(d)s') % {'d' : description})
+            spinner = ThreadedSpinner(self.prompt)
+            spinner.start()
+
+            try:
+                report = db_call(database)
+            except:
+                spinner.stop()
+                spinner.clear() # temporary until okaara supports this
+                raise
+
             spinner.stop()
-            raise
+            spinner.clear() # temporary until okaara supports this
 
-        spinner.stop()
+            if report is None or report.success is None:
+                # This should only happen during development if the script writer
+                # didn't properly configure the report and must be fixed before
+                # release
+                self._print(_('Database upgrade script did not indicate the result of the step'))
+                raise InvalidStepReportException()
 
-        if report.success is None:
-            # This should only happen during development if the script writer
-            # didn't properly configure the report and must be fixed before
-            # release
-            prompt.write(_('Database upgrade script did not indicate the result of the step'))
-            raise InvalidStepReportException()
+            if report.success:
+                self._print_report_data(_('Messages'), report.messages)
+                self._print_report_data(_('Warnings'), report.warnings)
+            else:
+                self._print_report_data(_('Warnings'), report.warnings)
+                self._print_report_data(_('Errors'), report.errors)
+                raise StepException(description)
 
-        if report.success:
-            _print_report_data(prompt, _('Messages'), report.messages)
-            _print_report_data(prompt, _('Warnings'), report.warnings)
-        else:
-            _print_report_data(prompt, _('Warnings'), report.warnings)
-            _print_report_data(prompt, _('Errors'), report.errors)
-            raise StepException(description)
+            self.prompt.write('')
 
-        prompt.write('')
+    # -- utilities ----------------------------------------------------------------
 
-# -- utilities ----------------------------------------------------------------
-
-def _database(db_name, seeds):
-    connection = Connection(seeds)
-    database = getattr(connection, db_name)
-    database.add_son_manipulator(NamespaceInjector())
-    database.add_son_manipulator(AutoReference(_database))
-    return database
-
-
-def _print_report_data(prompt, title, items):
-    if len(items) > 0:
-        prompt.write(title)
-        for i in items:
-            prompt.write('  %s' % i)
-
-
-def _is_v1(stream_file):
-    """
-    Returns whether or not the current installation is a v1 stream build.
-
-    :return: True if the installation is a v1 build, False otherwise
-    :rtype:  bool
-    """
-
-    # Eventually this will check the contents of that file, but for now the
-    # quickest solution is just to check its existence
-
-    return not os.path.exists(stream_file)
-
-
-class _UberWriter(object):
-    """
-    Writer implementation to pass to an okaara prompt to log to both a log
-    and standard output.
-    """
-
-    def write(self, line):
-        sys.stdout.write(line)
+    def _print(self, line):
         _LOG.info(line)
+        self.prompt.write(line)
 
+    def _database(self, db_name, seeds):
+        connection = Connection(seeds)
+        database = getattr(connection, db_name)
+        database.add_son_manipulator(NamespaceInjector())
+        database.add_son_manipulator(AutoReference(database))
+        return database
+
+    def _print_report_data(self, title, items):
+        if len(items) > 0:
+            self._print(title)
+            for i in items:
+                self._print('  %s' % i)
+
+    def _is_v1(self):
+        """
+        Returns whether or not the current installation is a v1 stream build.
+
+        :return: True if the installation is a v1 build, False otherwise
+        :rtype:  bool
+        """
+
+        # Eventually this will check the contents of that file, but for now the
+        # quickest solution is just to check its existence
+
+        result = not os.path.exists(self.stream_file)
+        return result
---
+++ platform/test/upgrade/test_main.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+import logging
+import os
+import tempfile
+import unittest
+
+import mock
+
+from pulp.server.upgrade import main
+
+
+TEST_DATABASE_NAME = 'pulp_upgrade_db'
+
+LOG_FILENAME = '/tmp/pulp-upgrate-unit-tests.log'
+STREAM_FILENAME = '/tmp/pulp-upgrade-stream'
+
+
+class MainTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(MainTests, cls).setUpClass()
+
+        logging.getLogger('pulp').addHandler(logging.FileHandler(LOG_FILENAME, mode='w'))
+
+    def setUp(self):
+        super(MainTests, self).setUp()
+
+        self.mock_upgrade_call_1 = mock.MagicMock().upgrade
+        self.mock_upgrade_call_1.return_value = main.UpgradeStepReport()
+        self.mock_upgrade_call_1.return_value.succeeded()
+        self.mock_upgrade_call_1.return_value.message('Upgrading collection 1')
+        self.mock_upgrade_call_1.return_value.warning('Small problem with collection 3')
+
+        self.mock_upgrade_calls = [(self.mock_upgrade_call_1, 'Mock 1')]
+
+        self.upgrader = main.Upgrader(db_name=TEST_DATABASE_NAME,
+                                      db_upgrade_calls=self.mock_upgrade_calls)
+
+    def tearDown(self):
+        super(MainTests, self).tearDown()
+
+        if os.path.exists(STREAM_FILENAME):
+            os.remove(STREAM_FILENAME)
+
+    def test_main(self):
+        # Test
+        self.upgrader.upgrade()
+
+        # Verify
+        self.assertTrue(os.path.exists(LOG_FILENAME))
+        self.assertEqual(1, self.mock_upgrade_call_1.call_count)
+
+    def test_main_with_error(self):
+        # Setup
+        self.mock_upgrade_call_1.return_value.failed()
+
+        # Test
+        try:
+            self.upgrader.upgrade()
+            self.fail()
+        except main.StepException, e:
+            self.assertEqual('Mock 1', e[0])
+
+        # Verify
+        self.assertEqual(1, self.mock_upgrade_call_1.call_count)
+
+    def test_main_with_partial_error(self):
+        # Setup
+        m = mock.MagicMock()
+        m.call_1.return_value = main.UpgradeStepReport()
+        m.call_1.return_value.succeeded()
+        m.call_2.return_value = main.UpgradeStepReport()
+        m.call_2.return_value.failed()
+        m.call_3.return_value = main.UpgradeStepReport()
+        m.call_3.return_value.succeeded()
+
+        self.upgrader.db_upgrade_calls = (
+            (m.call_1, 'Mock 1'),
+            (m.call_2, 'Mock 2'),
+            (m.call_3, 'Mock 3'),
+        )
+
+        # Test
+        try:
+            self.upgrader.upgrade()
+            self.fail()
+        except main.StepException, e:
+            self.assertEqual('Mock 2', e[0])
+
+        # Verify
+        self.assertEqual(1, m.call_1.call_count)
+        self.assertEqual(1, m.call_2.call_count)
+        self.assertEqual(0, m.call_3.call_count)
+
+    def test_already_upgraded(self):
+        # Setup
+        f = open(STREAM_FILENAME, 'w')
+        f.write('2')
+        f.close()
+        self.upgrader.stream_file = STREAM_FILENAME
+
+        # Test
+        self.upgrader.upgrade()
+
+        # Verify
+        self.assertEqual(0, self.mock_upgrade_call_1.call_count)
