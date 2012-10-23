a3945ad4e39b6935dab1257a86dbb825b1dd2799
Jay Dobies <jason.dobies@redhat.com>
2012-10-23 18:04:10 -0400

    Implementation and testing for consumer DB upgrade

--- platform/src/pulp/server/upgrade/db/consumers.py
+++ platform/src/pulp/server/upgrade/db/consumers.py
@@ -12,9 +12,102 @@
 
 from pulp.server.upgrade.model import UpgradeStepReport
 
+# Since v1 was so tightly coupled to RPMs, there's no reason to have plugin
+# hooks for the upgrade. However, we have to account for the fact that the
+# repos are in fact RPM repositories. So this reference to a particular plugin
+# in the platform makes sense given it's the only code that is specifically
+# handling the v1 approach.
+YUM_DISTRIBUTOR_ID = 'yum_distributor'
+
 
 def upgrade(v1_database, v2_database):
+
+    # Instead of having to worry about having the consumers and bindings added
+    # atomically, they will be added as separate steps. There's a slight
+    # duplication in queries with this approach, but it's very minimal and
+    # beats having to account for the case where a consumer was ported but
+    # something stopped its bindings.
+
+    _consumer_history(v1_database, v2_database)
+    _consumers(v1_database, v2_database)
+
     report = UpgradeStepReport()
     report.succeeded()
     return report
 
+
+def _consumer_history(v1_database, v2_database):
+    v1_coll = v1_database.consumer_history
+    v2_coll = v2_database.consumer_history
+
+    # Idempotency: The _id values are retained in the migration, so resolve
+    # which ones have not yet been migrated and only load those.
+    v2_ids = [x['_id'] for x in list(v2_coll.find({}, {'_id' : 1}))]
+    missing_v1_entries = list(v1_coll.find({'_id' : {'$nin' : v2_ids}}))
+
+    v2_entries = []
+    for v1_entry in missing_v1_entries:
+        v2_entry = {
+            '_id' : v1_entry['_id'], # for the uniqueness check in this script
+            'id' : v1_entry['_id'], # for weird backward compatibility
+            'consumer_id' : v1_entry['consumer_id'],
+            'originator' : v1_entry['originator'],
+            'type' : v1_entry['type_name'], # intentional rename here for v2
+            'details' : v1_entry['details'],
+            'timestamp' : v1_entry['timestamp'],
+        }
+        v2_entries.append(v2_entry)
+
+    if v2_entries:
+        v2_coll.insert(v2_entries)
+
+
+def _consumers(v1_database, v2_database):
+    v1_coll = v1_database.consumers
+    v2_coll = v2_database.consumers
+
+    # Idempotency: consumer_id is unique, so only process consumers whose ID is
+    # not in v2 already
+    v2_ids = [x['id'] for x in list(v2_coll.find({}, {'id' : 1}))]
+    missing_v1_consumers = list(v1_coll.find({'id' : {'$nin' : v2_ids}}))
+
+    for v1_consumer in missing_v1_consumers:
+        v2_consumer = {
+            'id' : v1_consumer['id'],
+            'display_name' : v1_consumer['id'],
+            'description' : v1_consumer['description'],
+            'notes' : v1_consumer['key_value_pairs'],
+            'capabilities' : v1_consumer['capabilities'],
+            'certificate' : v1_consumer['certificate'],
+        }
+        v2_coll.insert(v2_consumer)
+
+        # Ideally, this should be atomic with the consumer. That's also horribly
+        # complicated to attempt to honor. So while there's a small chance the
+        # following call could be interrupted and the bindings for this consumer
+        # lost, the chance is low enough that I'm willing to take it.
+        # jdob, Oct 23, 2012
+
+        _consumer_bindings(v2_database, v1_consumer)
+
+
+def _consumer_bindings(v2_database, v1_consumer):
+    v2_coll = v2_database.consumer_bindings
+    consumer_id = v1_consumer['id']
+    repo_ids = v1_consumer['repoids']
+
+    # Idempotency: Uniqueness is determined by the tuple of consumer and repo ID
+    bound_repo_ids = [x['repo_id'] for x in list(v2_coll.find({'consumer_id' : consumer_id}))]
+    unbound_repo_ids = set(repo_ids) - set(bound_repo_ids)
+
+    new_bindings = []
+    for repo_id in unbound_repo_ids:
+        binding = {
+            'consumer_id' : consumer_id,
+            'repo_id' : repo_id,
+            'distributor_id' : YUM_DISTRIBUTOR_ID,
+        }
+        new_bindings.append(binding)
+
+    if new_bindings:
+        v2_coll.insert(new_bindings)
---
+++ platform/test/upgrade/test_db_consumers.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public
+# License as published by the Free Software Foundation; either version
+# 2 of the License (GPLv2) or (at your option) any later version.
+# There is NO WARRANTY for this software, express or implied,
+# including the implied warranties of MERCHANTABILITY,
+# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
+# have received a copy of GPLv2 along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+from pymongo.objectid import ObjectId
+
+from base_db_upgrade import BaseDbUpgradeTests
+from pulp.server.upgrade.model import UpgradeStepReport
+from pulp.server.upgrade.db import consumers
+
+
+class ConsumersUpgradeTests(BaseDbUpgradeTests):
+
+    def test_consumers(self):
+        # Test
+        report = consumers.upgrade(self.v1_test_db.database, self.tmp_test_db.database)
+
+        # Verify
+        self.assertTrue(isinstance(report, UpgradeStepReport))
+        self.assertTrue(report.success)
+
+        # - Consumer History -
+        v1_entries = list(self.v1_test_db.database.consumer_history.find())
+        v2_entries = list(self.tmp_test_db.database.consumer_history.find())
+        self.assertEqual(len(v1_entries), len(v2_entries))
+
+        for v1_entry, v2_entry in zip(v1_entries, v2_entries):
+            self.assertEqual(v1_entry.pop('type_name'), v2_entry.pop('type'))
+            self.assertEqual(v1_entry, v2_entry)
+
+        # - Consumers -
+        v1_consumers = list(self.v1_test_db.database.consumers.find())
+        v2_consumers = list(self.tmp_test_db.database.consumers.find())
+        self.assertEqual(len(v1_consumers), len(v2_consumers))
+
+        for v1_consumer, v2_consumer in zip(v1_consumers, v2_consumers):
+            self.assertTrue(isinstance(v2_consumer['_id'], ObjectId))
+            self.assertEqual(v1_consumer['id'], v2_consumer['id'])
+            self.assertEqual(v1_consumer['id'], v2_consumer['display_name'])
+            self.assertEqual(v1_consumer['key_value_pairs'], v2_consumer['notes'])
+            self.assertEqual(v1_consumer['capabilities'], v2_consumer['capabilities'])
+            self.assertEqual(v1_consumer['certificate'], v2_consumer['certificate'])
+
+        # - Consumer Bindings -
+        binding_coll = self.tmp_test_db.database.consumer_bindings
+        for v1_consumer in v1_consumers:
+            for repo_id in v1_consumer['repoids']:
+                binding = binding_coll.find_one({'consumer_id' : v1_consumer['id'],
+                                                 'repo_id' : repo_id})
+                self.assertTrue(binding is not None)
+                self.assertEqual(binding['distributor_id'], consumers.YUM_DISTRIBUTOR_ID)
+
+    def test_consumers_resumed(self):
+        # Setup
+        consumers.upgrade(self.v1_test_db.database, self.tmp_test_db.database)
+        new_consumer = {
+            'id' : 'fake-consumer',
+            'description' : 'fake-description',
+            'key_value_pairs' : {'a' : 'a'},
+            'capabilities' : {},
+            'certificate' : 'ABCDE',
+            'repoids' : ['repo-1', 'repo-2']
+        }
+        self.v1_test_db.database.consumers.insert(new_consumer)
+
+        new_consumer_history = {
+            'consumer_id' : 'fake-consumer',
+            'originator' : 'admin',
+            'type_name' : 'install',
+            'details' : 'installed stuff',
+            'timestamp' : '10-23-2012',
+        }
+        self.v1_test_db.database.consumer_history.insert(new_consumer_history)
+
+        # Test
+        report = consumers.upgrade(self.v1_test_db.database, self.tmp_test_db.database)
+
+        # Verify
+        self.assertTrue(isinstance(report, UpgradeStepReport))
+        self.assertTrue(report.success)
+
+        v1_entries = list(self.v1_test_db.database.consumer_history.find())
+        v2_entries = list(self.tmp_test_db.database.consumer_history.find())
+        self.assertEqual(len(v1_entries), len(v2_entries))
+
+        v1_consumers = list(self.v1_test_db.database.consumers.find())
+        v2_consumers = list(self.tmp_test_db.database.consumers.find())
+        self.assertEqual(len(v1_consumers), len(v2_consumers))
+
+        binding_coll = self.tmp_test_db.database.consumer_bindings
+        for v1_consumer in v1_consumers:
+            for repo_id in v1_consumer['repoids']:
+                binding = binding_coll.find_one({'consumer_id' : v1_consumer['id'],
+                                                 'repo_id' : repo_id})
+                self.assertTrue(binding is not None)
+                self.assertEqual(binding['distributor_id'], consumers.YUM_DISTRIBUTOR_ID)
+
+    def test_consumers_idempotency(self):
+        # Setup
+        consumers.upgrade(self.v1_test_db.database, self.tmp_test_db.database)
+
+        # Test
+        report = consumers.upgrade(self.v1_test_db.database, self.tmp_test_db.database)
+
+        # Verify
+        self.assertTrue(isinstance(report, UpgradeStepReport))
+        self.assertTrue(report.success)
+
+        v1_entries = list(self.v1_test_db.database.consumer_history.find())
+        v2_entries = list(self.tmp_test_db.database.consumer_history.find())
+        self.assertEqual(len(v1_entries), len(v2_entries))
+
+        v1_consumers = list(self.v1_test_db.database.consumers.find())
+        v2_consumers = list(self.tmp_test_db.database.consumers.find())
+        self.assertEqual(len(v1_consumers), len(v2_consumers))
+
+        binding_coll = self.tmp_test_db.database.consumer_bindings
+        for v1_consumer in v1_consumers:
+            for repo_id in v1_consumer['repoids']:
+                binding = binding_coll.find_one({'consumer_id' : v1_consumer['id'],
+                                                 'repo_id' : repo_id})
+                self.assertTrue(binding is not None)
+                self.assertEqual(binding['distributor_id'], consumers.YUM_DISTRIBUTOR_ID)
