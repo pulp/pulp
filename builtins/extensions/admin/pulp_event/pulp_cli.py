619bb53b07d3db526f4d4542a677b1300b6ac1cf
Michael Hrivnak <mhrivnak@redhat.com>
2012-11-02 09:42:49 -0400

    fixing help text typo

--- builtins/extensions/admin/pulp_event/pulp_cli.py
+++ builtins/extensions/admin/pulp_event/pulp_cli.py
@@ -225,7 +225,7 @@ class AMQPSection(GenericSection):
         super(AMQPSection, self).__init__(context, 'amqp',
             _('manage amqp listeners'))
 
-        m = _('optional name of an exchange that overrides the setting from'
+        m = _('optional name of an exchange that overrides the setting from '
               'server.conf')
         self.exchange_option = PulpCliOption('--exchange', m, required=False)
 
