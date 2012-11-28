f0b921ce3ba657184dbdec7dccb535a1173737df
Sayli Karmarkar <skarmark@redhat.com>
2012-11-28 04:33:39 -0800

    adding a flag to all restorecon commands in relabel.sh script to ignore files that don't exist on a system

--- platform/selinux/server/relabel.sh
+++ platform/selinux/server/relabel.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-/sbin/restorecon -R /etc/httpd/conf.d/pulp.conf
-/sbin/restorecon -R /etc/pulp
-/sbin/restorecon -R /etc/pki/pulp
-/sbin/restorecon -R /srv/pulp
-/sbin/restorecon -R /usr/bin/pulp-admin
-/sbin/restorecon -R /usr/bin/pulp-consumer
-/sbin/restorecon -R /var/lib/pulp
-/sbin/restorecon -R /var/log/pulp
+/sbin/restorecon -i -R /etc/httpd/conf.d/pulp.conf
+/sbin/restorecon -i -R /etc/pulp
+/sbin/restorecon -i -R /etc/pki/pulp
+/sbin/restorecon -i -R /srv/pulp
+/sbin/restorecon -i -R /usr/bin/pulp-admin
+/sbin/restorecon -i -R /usr/bin/pulp-consumer
+/sbin/restorecon -i -R /var/lib/pulp
+/sbin/restorecon -i -R /var/log/pulp
 
 
 
