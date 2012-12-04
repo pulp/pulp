5dd9ccc873bf8b164773372daf7b21842ecccf9f
Randy Barlow <rbarlow@redhat.com>
2012-12-04 14:41:34 -0500

    - 862187 /var/log/pulp/db.log now includes timestamps.
    Reworked logging in the pulp-manage-db module to use the same logging
    system that is used elsewhere in Pulp. This allowed us to add timestamps
    to the db log with the logging configuration file, and it also allows
    sysadmins to season the migration log format to taste. As part of this
    commit I also removed the ability to change the log file with the former
    --log-file flag, as that functionality is now made available through the
    logging configuration file.

--- platform/src/pulp/server/db/manage.py
+++ platform/src/pulp/server/db/manage.py
@@ -19,10 +19,11 @@ import sys
 from pulp.plugins.loader.api import load_content_types
 from pulp.server.db import connection
 from pulp.server.db.migrate import models
+from pulp.server import logs
+
 
 connection.initialize()
-
-_log = logging.getLogger('pulp')
+logger = None
 
 
 class DataError(Exception):
@@ -33,27 +34,17 @@ class DataError(Exception):
 
 
 def parse_args():
+    """
+    Parse the command line arguments into the flags that we accept. Returns the parsed options.
+    """
     parser = OptionParser()
     parser.add_option('--test', action='store_true', dest='test',
                       default=False,
                       help=_('Run migration, but do not update version'))
-    parser.add_option('--log-file', dest='log_file',
-                      default='/var/log/pulp/db.log',
-                      help=_('File for log messages'))
-    parser.add_option('--log-level', dest='log_level', default='info',
-                      help=_('Level of logging (debug, info, error, critical)'))
     options, args = parser.parse_args()
     if args:
         parser.error(_('Unknown arguments: %s') % ', '.join(args))
     return options
-
-
-def start_logging(options):
-    level = getattr(logging, options.log_level.upper(), logging.INFO)
-    logger = logging.getLogger('pulp') # imitate the pulp log handler
-    logger.setLevel(level)
-    handler = logging.FileHandler(options.log_file)
-    logger.addHandler(handler)
 
 
 def migrate_database(options):
@@ -70,45 +61,54 @@ def migrate_database(options):
                                 'p': migration_package.name, 'v': migration_package.current_version,
                                 'a': migration_package.latest_available_version}))
         if migration_package.current_version == migration_package.latest_available_version:
-            print _('Migration package %(p)s is up to date at version %(v)s'%({
+            message = _('Migration package %(p)s is up to date at version %(v)s'%({
                 'p': migration_package.name,
                 'v': migration_package.latest_available_version}))
+            logger.info(message)
+            print message
             continue
 
         try:
             for migration in migration_package.unapplied_migrations:
-                print _('Applying %(p)s version %(v)s'%({
+                message = _('Applying %(p)s version %(v)s'%({
                     'p': migration_package.name, 'v': migration.version}))
+                print message
+                logger.info(message)
                 # We pass in !options.test to stop the apply_migration method from updating the
                 # package's current version when the --test flag is set
                 migration_package.apply_migration(migration,
                                                   update_current_version=not options.test)
-                print _('Migration to %(p)s version %(v)s complete.'%(
+                message = _('Migration to %(p)s version %(v)s complete.'%(
                     {'p': migration_package.name, 'v': migration_package.current_version}))
+                print message
+                logger.info(message)
         except Exception, e:
             # If an Exception is raised while applying the migrations, we should log and print it,
             # and then continue with the other packages.
             error_message = _('Applying migration %(m)s failed.')%(
                               {'m': migration.name})
             print >> sys.stderr, str(error_message), _(' See log for details.')
-            _log.critical(error_message)
-            _log.critical(str(e))
-            _log.critical(''.join(traceback.format_exception(*sys.exc_info())))
+            logger.critical(error_message)
+            logger.critical(str(e))
+            logger.critical(''.join(traceback.format_exception(*sys.exc_info())))
 
 
 def main():
+    """
+    This is the high level entry method. It does logging if any Exceptions are raised.
+    """
     try:
         options = parse_args()
-        start_logging(options)
+        _start_logging()
         _auto_manage_db(options)
     except DataError, e:
-        _log.critical(str(e))
-        _log.critical(''.join(traceback.format_exception(*sys.exc_info())))
+        logger.critical(str(e))
+        logger.critical(''.join(traceback.format_exception(*sys.exc_info())))
         print >> sys.stderr, str(e)
         return os.EX_DATAERR
     except Exception, e:
-        _log.critical(str(e))
-        _log.critical(''.join(traceback.format_exception(*sys.exc_info())))
+        logger.critical(str(e))
+        logger.critical(''.join(traceback.format_exception(*sys.exc_info())))
         print >> sys.stderr, str(e)
         return os.EX_SOFTWARE
 
@@ -120,11 +120,28 @@ def _auto_manage_db(options):
 
     :param options: The command line parameters from the user.
     """
-    print _('Beginning database migrations.')
+    message = _('Beginning database migrations.')
+    print message
+    logger.info(message)
     migrate_database(options)
-    print _('Database migrations complete.')
+    message = _('Database migrations complete.')
+    print message
+    logger.info(message)
 
-    print _('Loading content types.')
+    message = _('Loading content types.')
+    print message
+    logger.info(message)
     load_content_types()
-    print _('Content types loaded.')
+    message = _('Content types loaded.')
+    print message
+    logger.info(message)
     return os.EX_OK
+
+
+def _start_logging():
+    """
+    Call into Pulp to get the logging started, and set up the logger to be used in this module.
+    """
+    global logger
+    logs.start_logging()
+    logger = logging.getLogger('db')
