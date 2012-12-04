41a0b0f9bb797febfa143e2c272cc8aebe43db42
Michael Hrivnak <mhrivnak@redhat.com>
2012-12-04 17:51:56 -0500

    adding a unit test that hits the database

--- platform/test/unit/server/test_migration_0002.py
+++ platform/test/unit/server/test_migration_0002.py
@@ -11,14 +11,16 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
-import unittest
 
 import mock
 
+import base
 from pulp.server.db.migrate.models import MigrationModule
+from pulp.server import managers
+from pulp.server.db.model.event import EventListener
 
 
-class TestMigration0002(unittest.TestCase):
+class TestMigration0002(base.PulpServerTests):
     @mock.patch('pulp.server.db.model.event.EventListener.get_collection')
     def test_update_called(self, mock_get_collection):
         module = MigrationModule('pulp.server.db.migrations.0002_rename_http_notifier')._module
@@ -28,3 +30,23 @@ class TestMigration0002(unittest.TestCase):
         mock_get_collection.return_value.update.assert_called_once_with(
             {'notifier_type_id': 'rest-api'}, {'$set': {'notifier_type_id': 'http'}}
         )
+
+    def test_database_integration(self):
+        # make sure the migration works on a live document in mongo
+        collection = EventListener.get_collection()
+        event_listener_id = str(collection.insert({
+            'notifier_type_id' : 'rest-api',
+            'event_types' : ['*'],
+            'notifier_config' : {},
+        }, safe=True))
+        event_listener_factory = managers.factory.event_listener_manager()
+
+        module = MigrationModule('pulp.server.db.migrations.0002_rename_http_notifier')._module
+        module.migrate()
+
+        event_listener = event_listener_factory.get(event_listener_id)
+        self.assertEqual(event_listener['notifier_type_id'], 'http')
+
+        # cleanup
+        collection.remove()
+
