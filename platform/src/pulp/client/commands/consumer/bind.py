485a0b8ef43d7f2886298599ed0886ac46a3c976
Jason L Connor <jason.connor@gmail.com>
2013-02-05 17:30:22 -0700

    initial bind/unbind implementations

--- platform/src/pulp/client/commands/consumer/bind.py
+++ platform/src/pulp/client/commands/consumer/bind.py
@@ -10,3 +10,90 @@
 # PARTICULAR PURPOSE.
 # You should have received a copy of GPLv2 along with this software; if not,
 # see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from gettext import gettext as _
+
+from pulp.bindings.exceptions import NotFoundException
+from pulp.client.commands.options import OPTION_CONSUMER_ID, OPTION_REPO_ID
+from pulp.client.extensions.extensions import PulpCliCommand
+
+
+CONSUMER_BIND_DESCRIPTION = _('')
+CONSUMER_UNBIND_DESCRIPTION = _('')
+
+DISTRIBUTOR_OPTION_NAME = 'distributor'
+DISTRIBUTOR_OPTION_DESCRIPTION = _('')
+
+FORCE_FLAG_NAME = 'force'
+FORCE_FLAG_DESCRIPTION = _('delete the binding immediately and discontinue tracking consumer actions')
+
+NOT_FOUND_TAG = 'not-found'
+
+
+class ConsumerBindCommand(PulpCliCommand):
+
+    def __init__(self, context, name='bind', description=CONSUMER_BIND_DESCRIPTION):
+        super(self.__class__, self).__init__(name, description, self.bind)
+        self.context = context
+        self.add_option(OPTION_CONSUMER_ID)
+        self.add_option(OPTION_REPO_ID)
+        self.create_option('--' + DISTRIBUTOR_OPTION_NAME, DISTRIBUTOR_OPTION_DESCRIPTION, required=True)
+
+    def bind(self, **kwargs):
+        consumer_id = kwargs[OPTION_CONSUMER_ID.keyword]
+        repo_id = kwargs[OPTION_REPO_ID.keyword]
+        distributor_id = kwargs[DISTRIBUTOR_OPTION_NAME]
+
+        try:
+            response = self.context.server.bind.bind(consumer_id, repo_id, distributor_id)
+
+        except NotFoundException, e:
+            resources = e.extra_data['resources']
+            missing = []
+            msg = _('%(t)s [%(i)s] does not exists on the server')
+            if 'consumer' in resources:
+                missing.append((_('Consumer'), consumer_id))
+            if 'repository' in resources:
+                missing.append((_('Repository'), repo_id))
+            if 'distributor' in resources:
+                missing.append((_('Distributor'), distributor_id))
+            for option_title, option_id in missing:
+                self.context.prompt.write(msg % {'t': option_title, 'i': option_id}, tag=NOT_FOUND_TAG)
+
+        else:
+            msg = _('Bind tasks successfully created:')
+            self.context.prompt.render_success_message(msg)
+            task_dicts = [dict(('task_id', str(t.task_id))) for t in response.response_body]
+            self.context.prompt.render_document_list(task_dicts)
+
+
+class ConsumerUnbindCommand(PulpCliCommand):
+
+    def __init__(self, context, name='unbind', description=CONSUMER_UNBIND_DESCRIPTION):
+        super(self.__class__, self).__init__(name, description, self.unbind)
+        self.context = context
+        self.add_option(OPTION_CONSUMER_ID)
+        self.add_option(OPTION_REPO_ID)
+        self.create_option('--' + DISTRIBUTOR_OPTION_NAME, DISTRIBUTOR_OPTION_DESCRIPTION, required=True)
+        self.create_flag('--' + FORCE_FLAG_NAME, FORCE_FLAG_DESCRIPTION)
+
+    def unbind(self, **kwargs):
+        consumer_id = kwargs[OPTION_CONSUMER_ID.keyword]
+        repo_id = kwargs[OPTION_REPO_ID.keyword]
+        distributor_id = kwargs[DISTRIBUTOR_OPTION_NAME]
+        force = kwargs[FORCE_FLAG_NAME]
+
+        try:
+            response = self.context.server.bind.unbind(consumer_id, repo_id, distributor_id, force)
+
+        except NotFoundException:
+            msg = _('Binding [consumer: %(c)s, repository: %(r)s] does not exist on the server')
+            self.context.prompt.write(msg % {'c': consumer_id, 'r': repo_id}, tag=NOT_FOUND_TAG)
+
+        else:
+            msg = _('Unbind tasks successfully created:')
+            self.context.prompt.render_success_message(msg)
+            task_dicts = [dict(('task_id', t.task_id)) for t in response.response_body]
+            self.context.prompt.render_document_list(task_dicts)
+
+
