17df57e7a5ec5520c67952a81ec0c40ff41612d6
Jason L Connor <jason.connor@gmail.com>
2013-02-15 17:02:38 -0700

    ported content schedule unit tests from rpm implementation

--- platform/src/pulp/client/commands/consumer/content.py
+++ platform/src/pulp/client/commands/consumer/content.py
@@ -469,7 +469,7 @@ class ConsumerContentNextRunCommand(NextRunCommand):
 class ConsumerContentScheduleStrategy(ScheduleStrategy):
 
     def __init__(self, context, action):
-        super(self.__class__, self).__init__()
+        super(ConsumerContentScheduleStrategy, self).__init__()
 
         self.context = context
         self.action = action
