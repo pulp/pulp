c03abd9478441980ca97ced7b50e29828d1df69a
Jason L Connor <jason.connor@gmail.com>
2013-03-06 11:29:45 -0700

    addition of event callbacks to eventlet backend

--- platform/src/pulp/common/download/backends/event.py
+++ platform/src/pulp/common/download/backends/event.py
@@ -11,6 +11,8 @@
 # You should have received a copy of GPLv2 along with this software; if not,
 # see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from logging import getLogger
+
 from eventlet import GreenPool
 from eventlet.green import urllib2
 
@@ -19,6 +21,8 @@ from pulp.common.download.backends.base import DownloadBackend
 
 
 DEFAULT_MAX_CONCURRENT = 20
+
+_LOG = getLogger(__name__)
 
 
 class HTTPEventletDownloadBackend(DownloadBackend):
@@ -29,28 +33,56 @@ class HTTPEventletDownloadBackend(DownloadBackend):
 
     def download(self, request_list):
 
+        # fetch closure --------------------------------------------------------
+
         def _fetch(request):
+            report = report_dict[request]
+            report.state = download_report.DOWNLOAD_DOWNLOADING
+            self.fire_download_started(report)
+
             file_handle = request.destination
 
             if isinstance(request.destination, basestring):
                 file_handle = open(request.destination, 'wb')
 
-            body = urllib2.urlopen(request.url).read()
+            try:
+                body = urllib2.urlopen(request.url).read()
+                file_handle.write(body)
 
-            file_handle.write(body)
+                # XXX (jconnor 2013-03-06) need to find a more fine-grained way
+                # to do downloading and progress reporting
+                bytes = len(body)
+                report.bytes_downloaded = bytes
+                self.fire_download_progress(report)
 
-            if file_handle is not request.destination:
-                file_handle.close()
+                if file_handle is not request.destination:
+                    file_handle.close()
 
-            return request
+            except Exception, e:
+                report.state = download_report.DOWNLOAD_FAILED
+                _LOG.exception(e)
+
+            else:
+                report.state = download_report.DOWNLOAD_SUCCEEDED
+
+            return report
+
+        # download implementation ----------------------------------------------
 
         report_dict = dict((request, download_report.DownloadReport.from_download_request(request))
                            for request in request_list)
 
+        self.fire_batch_started(report_dict.itervalues())
+
         pool = GreenPool(size=self.max_concurrent)
 
-        for request in pool.imap(_fetch, request_list):
-            pass
+        for report in pool.imap(_fetch, request_list):
+            if report.state is download_report.DOWNLOAD_SUCCEEDED:
+                self.fire_download_succeeded(report)
+            else:
+                self.fire_download_failed(report)
+
+        self.fire_batch_finished(report_dict.itervalues())
 
         return report_dict.values()
 
