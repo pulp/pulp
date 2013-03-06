e525fed754303e113c42b04442a427d1b1b522bb
Jason L Connor <jason.connor@gmail.com>
2013-03-06 16:22:51 -0700

    further reporting and asynch downloading for eventlet downloader backend

--- platform/src/pulp/common/download/backends/event.py
+++ platform/src/pulp/common/download/backends/event.py
@@ -11,6 +11,8 @@
 # You should have received a copy of GPLv2 along with this software; if not,
 # see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from collections import defaultdict
+from datetime import datetime
 from logging import getLogger
 
 from eventlet import GreenPool
@@ -20,12 +22,20 @@ from pulp.common.download import report as download_report
 from pulp.common.download.backends.base import DownloadBackend
 
 
-DEFAULT_MAX_CONCURRENT = 20
+# taken from python documentation <http://docs.python.org/2.6/library/socket.html#socket.socket.recv>
+DEFAULT_BUFFER_SIZE = 4096
+
+DEFAULT_MAX_CONCURRENT = 100
 
 _LOG = getLogger(__name__)
 
+# eventlet downloader backend --------------------------------------------------
 
 class HTTPEventletDownloadBackend(DownloadBackend):
+
+    @property
+    def buffer_size(self):
+        return self.config.buffer_size or DEFAULT_BUFFER_SIZE
 
     @property
     def max_concurrent(self):
@@ -38,6 +48,7 @@ class HTTPEventletDownloadBackend(DownloadBackend):
         def _fetch(request):
             report = report_dict[request]
             report.state = download_report.DOWNLOAD_DOWNLOADING
+            report.start_time = datetime.utcnow()
             self.fire_download_started(report)
 
             file_handle = request.destination
@@ -46,17 +57,21 @@ class HTTPEventletDownloadBackend(DownloadBackend):
                 file_handle = open(request.destination, 'wb')
 
             try:
-                body = urllib2.urlopen(request.url).read()
-                file_handle.write(body)
+                urllib2_request = download_request_to_urllib2_request(self.config, request)
+                response = urllib2.urlopen(urllib2_request)
+                info = response.info()
+                set_response_info(info, report)
 
-                # XXX (jconnor 2013-03-06) need to find a more fine-grained way
-                # to do downloading and progress reporting
-                bytes = len(body)
-                report.bytes_downloaded = bytes
-                self.fire_download_progress(report)
+                while True:
+                    body = response.read(self.buffer_size)
+                    if not body:
+                        break
 
-                if file_handle is not request.destination:
-                    file_handle.close()
+                    file_handle.write(body)
+
+                    bytes = len(body)
+                    report.bytes_downloaded += bytes
+                    self.fire_download_progress(report)
 
             except Exception, e:
                 report.state = download_report.DOWNLOAD_FAILED
@@ -65,18 +80,32 @@ class HTTPEventletDownloadBackend(DownloadBackend):
             else:
                 report.state = download_report.DOWNLOAD_SUCCEEDED
 
+            finally:
+                report.finish_time = datetime.utcnow()
+
+                if file_handle is not request.destination:
+                    file_handle.close()
+
             return report
 
         # download implementation ----------------------------------------------
 
+        # XXX (jconnor 2013-03-06) is this going to blow our benefits from using
+        # generators as the request list?
         report_dict = dict((request, download_report.DownloadReport.from_download_request(request))
                            for request in request_list)
+        # it would be much cooler to have a "lazy" default dict, where the lazy
+        # instantiates the report from the request key on demand
+        #report_dict = defaultdict(download_report.DownloadReport.from_download_request).fromkeys(request_list)
 
         self.fire_batch_started(report_dict.itervalues())
 
         pool = GreenPool(size=self.max_concurrent)
 
+        # main i/o loop
         for report in pool.imap(_fetch, request_list):
+            # event callbacks called here to keep them from gumming up the
+            # concurrent fetch calls
             if report.state is download_report.DOWNLOAD_SUCCEEDED:
                 self.fire_download_succeeded(report)
             else:
@@ -85,4 +114,16 @@ class HTTPEventletDownloadBackend(DownloadBackend):
         self.fire_batch_finished(report_dict.itervalues())
 
         return report_dict.values()
+
+# utilities --------------------------------------------------------------------
+
+def download_request_to_urllib2_request(config, download_request):
+    urllib2_request = urllib2.Request(download_request.url)
+    return urllib2_request
+
+
+def set_response_info(info, report):
+    content_length = info.dict.get('content-length', 0)
+    report.total_bytes = int(content_length)
+
 
