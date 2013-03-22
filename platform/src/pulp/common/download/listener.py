7ba8a7d33281dbdda9630873610504a74b6017b5
Jason L Connor <jason.connor@gmail.com>
2013-03-22 11:25:25 -0600

    new aggregating event listener

--- platform/src/pulp/common/download/listener.py
+++ platform/src/pulp/common/download/listener.py
@@ -11,6 +11,8 @@
 # You should have received a copy of GPLv2 along with this software; if not,
 # see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+import itertools
+
 
 class DownloadEventListener(object):
     """
@@ -22,7 +24,7 @@ class DownloadEventListener(object):
     Callers are generally expected to sub-class this class in order to implement
     event-based functionality.
 
-    This class is used as a stub class in the event that no sub-classed event
+    This class is used as a no-op class in the event that no sub-classed event
     listener is provided to a download backend.
     """
 
@@ -50,4 +52,31 @@ class DownloadEventListener(object):
     def download_failed(self, report):
         pass
 
+
+class AggregatingEventListener(DownloadEventListener):
+    """
+    Event listener class that collects download reports and stores them in
+    lists based on the outcome of the download: success or failure.
+
+    NOTE: do to the ability to associate arbitrary data with the reports, using
+    this event listener may consumer a lot of memory.
+
+    :ivar succeeded_reports: list of download reports for downloads that succeeded
+    :ivar failed_reports: list of download reports for downloads that failed
+    :ivar all_reports: iterator over all reports
+    """
+
+    def __init__(self):
+        self.succeeded_reports = []
+        self.failed_reports = []
+
+    @property
+    def all_reports(self):
+        return itertools.chain(self.succeeded_reports, self.failed_reports)
+
+    def download_succeeded(self, report):
+        self.succeeded_reports.append(report)
+
+    def download_failed(self, report):
+        self.failed_reports.append(report)
 
