7fdfccf8741e9fe9f2a008262e21a7c394837baf
Randy Barlow <rbarlow@redhat.com>
2013-04-08 15:40:48 -0400

    Move an import statement.

--- platform/test/unit/server/test_common_download_backends_curl.py
+++ platform/test/unit/server/test_common_download_backends_curl.py
@@ -11,9 +11,9 @@
 # You should have received a copy of GPLv2 along with this software; if not,
 # see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-import mock
 import unittest
 
+import mock
 import pycurl
 
 from pulp.common.download.config import DownloaderConfig
