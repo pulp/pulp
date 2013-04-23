7ea84bfa2b87e76fa63f15af4838b3b65702fe23
Jeff Ortel <jortel@redhat.com>
2013-04-23 10:45:56 -0500

    Add cancel support to content itineraries.

--- platform/src/pulp/server/itineraries/consumer.py
+++ platform/src/pulp/server/itineraries/consumer.py
@@ -22,6 +22,26 @@ from pulp.server.dispatch.call import CallRequest
 from pulp.server.managers import factory as managers_factory
 
 
+# -- task callbacks ----------------------------------------------------------------------
+
+
+def cancel_agent_request(call_request, call_report):
+    """
+    Cancel the agent request associated with the task.
+    :param call_request: The call request that has been cancelled.
+    :type call_request: pulp.server.dispatch.call.CallRequest
+    :param call_report: The report associated with the call request to be cancelled.
+    :type call_report: pulp.server.dispatch.call.CallReport
+    """
+    task_id = call_report.call_request_id
+    consumer_id = call_request.args[0]
+    agent_manager = managers_factory.consumer_agent_manager()
+    agent_manager.cancel_request(consumer_id, task_id)
+
+
+# -- itineraries -------------------------------------------------------------------------
+
+
 def consumer_content_install_itinerary(consumer_id, units, options):
     """
     Create an itinerary for consumer content installation.
@@ -41,6 +61,9 @@ def consumer_content_install_itinerary(consumer_id, units, options):
     tags = [resource_tag(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id),
             action_tag('unit_install')]
     call_request = CallRequest(manager.install_content, args, kwargs, weight=weight, tags=tags, archive=True, asynchronous=True)
+    call_request.add_life_cycle_callback(
+        dispatch_constants.CALL_CANCEL_LIFE_CYCLE_CALLBACK,
+        cancel_agent_request)
     call_request.reads_resource(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id)
     return [call_request]
 
@@ -64,6 +87,9 @@ def consumer_content_update_itinerary(consumer_id, units, options):
     tags = [resource_tag(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id),
             action_tag('unit_update')]
     call_request = CallRequest(manager.update_content, args, kwargs, weight=weight, tags=tags, archive=True, asynchronous=True)
+    call_request.add_life_cycle_callback(
+        dispatch_constants.CALL_CANCEL_LIFE_CYCLE_CALLBACK,
+        cancel_agent_request)
     call_request.reads_resource(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id)
     return [call_request]
 
@@ -87,6 +113,9 @@ def consumer_content_uninstall_itinerary(consumer_id, units, options):
     tags = [resource_tag(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id),
             action_tag('unit_uninstall')]
     call_request = CallRequest(manager.uninstall_content, args, kwargs, weight=weight, tags=tags, archive=True, asynchronous=True)
+    call_request.add_life_cycle_callback(
+        dispatch_constants.CALL_CANCEL_LIFE_CYCLE_CALLBACK,
+        cancel_agent_request)
     call_request.reads_resource(dispatch_constants.RESOURCE_CONSUMER_TYPE, consumer_id)
     return [call_request]
 
