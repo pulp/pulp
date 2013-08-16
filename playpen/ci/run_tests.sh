1258f7d2a9ec31a385600cac58de0ba44ec6d9d3
Barnaby Court <bcourt@redhat.com>
2013-08-16 10:51:33 -0400

    barnabycourt-BuildScripts - Add script to run the unit tests

---
+++ playpen/ci/run_tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# This script expects to run within the Jenkins Build Process
+# The purpose of the script is to clean up after a build & test suite has been run in order to return the server to the
+# pre build/test execution state.
+
+echo "Running the tests"
+set -x
+
+cd $WORKSPACE/pulp
+nosetests platform/test/unit/ --with-xunit --xunit-file platformTest.xml --process-timeout=360
+nosetests builtins/test/unit/ --with-xunit --xunit-file builtinsTest.xml --process-timeout=360
+nosetests pulp_devel/test/unit/ --with-xunit --xunit-file builtinsTest.xml --process-timeout=360
+
+cd $WORKSPACE/pulp_rpm
+nosetests pulp_rpm/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
+
+cd $WORKSPACE/pulp_puppet
+nosetests  pulp_puppet_common/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
+nosetests  pulp_puppet_extensions_admin/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
+nosetests  pulp_puppet_extensions_consumer/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
+nosetests  pulp_puppet_handlers/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
+nosetests  pulp_puppet_plugins/test/unit/ --with-xunit --xunit-file puppetTest.xml --process-timeout=360
