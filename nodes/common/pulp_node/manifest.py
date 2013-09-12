825aa9def4bcba7594e6cecd9a31927d7ce75c48
Jeff Ortel <jortel@redhat.com>
2013-09-12 10:29:29 -0500

    formatting consistency

--- nodes/common/pulp_node/manifest.py
+++ nodes/common/pulp_node/manifest.py
@@ -177,6 +177,21 @@ class UnitWriter(object):
         self.fp = open(path, 'w+')
         self.total_units = 0
 
+    @property
+    def closed(self):
+        """
+        Determines if the file is closed or not. This exists because
+        the gzip API changed drastically from python 2.6 to 2.7.
+        :return: True if the file is closed.
+        :rtype: bool
+        """
+        try:
+            # python 2.7
+            return self.fp.closed
+        except AttributeError:
+            # python 2.6
+            return self.fp.fileobj.closed
+
     def add(self, unit):
         """
         Add (write) the specified unit to the file as a json encoded string.
@@ -200,22 +215,6 @@ class UnitWriter(object):
             self.fp.close()
             self.path = compress(self.path)
         return self.total_units
-
-    @property
-    def closed(self):
-        """
-        Determines if the file is closed or not. This exists because the file
-        object's API changed drastically from python 2.6 to 2.7.
-
-        :return:    True iff the file is closed, else False
-        :rtype:     bool
-        """
-        try:
-            # python 2.7
-            return self.fp.closed
-        except AttributeError:
-            # python 2.6
-            return self.fp.fileobj.closed
 
     def __enter__(self):
         return self
