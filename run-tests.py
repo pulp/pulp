7645fd5ec53ab6dbaa2fd7559fa20b0bafecdf6a
Barnaby Court <bcourt@redhat.com>
2013-11-01 10:03:53 -0400

    Add arguments to enable cobertura plugin on jenkins and create common module for the run-tests.py  to share content across projects.

--- run-tests.py
+++ run-tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2012 Red Hat, Inc.
+# Copyright © 2013 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public
 # License as published by the Free Software Foundation; either version
@@ -13,8 +13,8 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 import os
 import subprocess
-import sys
-import argparse
+
+from pulp.devel.test_runner import run_tests
 
 # Find and eradicate any existing .pyc files, so they do not eradicate us!
 PROJECT_DIR = os.path.dirname(__file__)
@@ -46,35 +46,4 @@ TESTS_NON_RHEL5 = [
     'server/test/unit'
 ]
 
-#add ability to specify nosetest options
-parser = argparse.ArgumentParser()
-parser.add_argument('--xunit-file')
-parser.add_argument('--with-xunit', action='store_true')
-parser.add_argument('--disable-coverage', action='store_true')
-arguments = parser.parse_args()
-
-args = [
-    'nosetests',
-]
-
-if not arguments.disable_coverage:
-    args.extend(['--with-coverage',
-    '--cover-html',
-    '--cover-erase',
-    '--cover-package',
-    ','.join(PACKAGES)])
-
-
-# don't run the server tests in RHEL5.
-if sys.version_info >= (2, 6):
-    args.extend(TESTS_NON_RHEL5)
-
-args.extend(TESTS_ALL_PLATFORMS)
-
-if arguments.with_xunit:
-    args.extend(['--with-xunit', '--process-timeout=360'])
-if arguments.xunit_file:
-    args.extend(['--xunit-file', '../test/' + arguments.xunit_file])
-
-#Call the test process
-subprocess.call(args)
+run_tests(PACKAGES, TESTS_ALL_PLATFORMS, TESTS_NON_RHEL5)
