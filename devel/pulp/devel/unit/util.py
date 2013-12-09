014132023224f3cb5a57b189f23a0e0a23cbc73c
Barnaby Court <bcourt@redhat.com>
2013-12-09 10:57:06 -0500

    Add utility to test equality of ElementTree elements.

--- devel/pulp/devel/unit/util.py
+++ devel/pulp/devel/unit/util.py
@@ -11,6 +11,7 @@
 # have received a copy of GPLv2 along with this software; if not, see
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
+from xml.etree import ElementTree
 
 def compare_dict(source, target):
     """
@@ -37,3 +38,47 @@ def compare_dict(source, target):
     for key in source_keys:
         if source[key] != target[key]:
             raise AssertionError("Dictionaries do not match.  Value mismatch for key %s" % key)
+
+
+def compare_element(source, target):
+    """
+    Utility method to recursively compare two etree elements
+
+    :param source: The source element to compare against the target
+    :type source: xml.etree.ElementTree.Element
+    :param target: The target element to compare against the source
+    :type target: xml.etree.ElementTree.Element
+    :raise AssertionError: if the elements do not match
+    """
+    if not ElementTree.iselement(source):
+        raise AssertionError("Source is not an element")
+    if not ElementTree.iselement(target):
+        raise AssertionError("Target is not an element")
+
+    if source.tag != target.tag:
+        raise AssertionError("elements do not match.  Tags are different")
+
+    #test keys
+    source_keys = set(source.keys())
+    target_keys = set(target.keys())
+
+    if source_keys != target_keys:
+        raise AssertionError("elements do not match.  Keys are different")
+
+    for key in source_keys:
+        if source.get(key) != target.get(key):
+            raise AssertionError("Key values do not match.  Value mismatch for key %s" % key)
+
+    if source.text != target.text:
+        raise AssertionError("elements do not match.  Text is different\n%s\n%s" % (source.text,
+                                                                                    target.text))
+
+    #Use the deprecated getchildren method for python 2.6 support
+    source_children = list(source.getchildren())
+    target_children = list(target.getchildren())
+    if len(source_children) != len(target_children):
+        raise AssertionError("elements do not match.  Unequal number of child elements")
+
+    for source_child, target_child in zip(source_children, target_children):
+        compare_element(source_child, target_child)
+
