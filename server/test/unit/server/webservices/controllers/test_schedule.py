92815637af8b2f063c165cf344bd32a2162bb311
Michael Hrivnak <mhrivnak@redhat.com>
2014-01-03 11:03:09 -0500

    adding tests

--- server/test/unit/server/webservices/controllers/test_schedule.py
+++ server/test/unit/server/webservices/controllers/test_schedule.py
@@ -15,17 +15,48 @@ import unittest
 
 import mock
 
+from pulp.server import exceptions
 from pulp.server.db.model.dispatch import ScheduledCall
+from pulp.server.managers.factory import initialize
 from pulp.server.webservices.controllers.schedule import ScheduleResource
 
 
+initialize()
+
+
+@mock.patch('pulp.server.managers.schedule.utils.get')
 class TestScheduleResource(unittest.TestCase):
     def setUp(self):
+        super(TestScheduleResource, self).setUp()
         self.controller = ScheduleResource()
 
-    @mock.patch('pulp.server.managers.schedule.utils.get')
-    def test_get(self, mock_utils_get):
+    @mock.patch.object(ScheduleResource, 'ok')
+    @mock.patch('pulp.server.webservices.http.uri_path', return_value='http://foo/bar')
+    def test_get(self, mock_path, mock_ok, mock_utils_get):
         call = ScheduledCall('PT1M', 'pulp.tasks.frequent')
         mock_utils_get.return_value = [call]
 
-        self.controller._get('abc123')
+        ret = self.controller._get(call.id)
+        schedule = mock_ok.call_args[0][0]
+
+        self.assertEqual(ret, mock_ok.return_value)
+        self.assertEqual(len(mock_ok.call_args[0]), 1)
+
+        # spot-check the schedule
+        self.assertEqual(schedule['_id'], call.id)
+        self.assertEqual(schedule['schedule'], 'PT1M')
+        self.assertEqual(schedule['task'], 'pulp.tasks.frequent')
+        self.assertEqual(schedule['_href'], mock_path.return_value)
+
+        # now check overall equality with the actual for_display value
+        del schedule['_href']
+        self.assertEqual(schedule, call.for_display())
+
+        # make sure we called the manager layer correctly
+        mock_utils_get.assert_called_once_with([call.id])
+
+    def test_missing(self, mock_utils_get):
+        call = ScheduledCall('PT1M', 'pulp.tasks.frequent')
+        mock_utils_get.return_value = []
+
+        self.assertRaises(exceptions.MissingResource, self.controller._get, call.id)
