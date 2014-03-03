5c49891df8b8c94c008fbf0ca444c7babad28171
Randy Barlow <rbarlow@redhat.com>
2014-03-03 14:03:52 -0500

    Raise version strings in all setup.py files to 2.4.0.

--- devel/setup.py
+++ devel/setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-devel',
-    version='2.3.0',
+    version='2.4.0',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
