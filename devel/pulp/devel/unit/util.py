0d0e202a1ae17e35630a83bc8954bdd72abf4e53
Barnaby Court <bcourt@redhat.com>
2014-05-01 10:22:20 -0400

    Move util that uses etree to the directory that isn't run on python 2.4

--- devel/pulp/devel/unit/util.py
+++ devel/pulp/devel/unit/util.py
@@ -12,7 +12,6 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import os
-from xml.etree import ElementTree
 
 
 def compare_dict(source, target):
@@ -45,51 +44,6 @@ def compare_dict(source, target):
             raise AssertionError("Dictionaries do not match.  Value mismatch for key %(key)s.  "
                                  "%(value1)s is not equal to %(value2)s" %
                                  {'key': key, 'value1': source[key], 'value2': target[key]})
-
-
-def compare_element(source, target):
-    """
-    Utility method to recursively compare two etree elements
-
-    :param source: The source element to compare against the target
-    :type source: xml.etree.ElementTree.Element
-    :param target: The target element to compare against the source
-    :type target: xml.etree.ElementTree.Element
-    :raise AssertionError: if the elements do not match
-    """
-    if not ElementTree.iselement(source):
-        raise AssertionError("Source is not an element")
-    if not ElementTree.iselement(target):
-        raise AssertionError("Target is not an element")
-
-    if source.tag != target.tag:
-        raise AssertionError("elements do not match.  Tags are different %s != %s" %
-                             (source.tag, target.tag))
-
-    #test keys
-    source_keys = set(source.keys())
-    target_keys = set(target.keys())
-
-    if source_keys != target_keys:
-        raise AssertionError("elements do not match.  Keys are different")
-
-    for key in source_keys:
-        if source.get(key) != target.get(key):
-            raise AssertionError("Key values do not match.  Value mismatch for key %s: %s != %s" %
-                                 (key, source.get(key), target.get(key)))
-
-    if source.text != target.text:
-        raise AssertionError("elements do not match.  Text is different\n%s\n%s" % (source.text,
-                                                                                    target.text))
-
-    #Use the deprecated getchildren method for python 2.6 support
-    source_children = list(source.getchildren())
-    target_children = list(target.getchildren())
-    if len(source_children) != len(target_children):
-        raise AssertionError("elements do not match.  Unequal number of child elements")
-
-    for source_child, target_child in zip(source_children, target_children):
-        compare_element(source_child, target_child)
 
 
 def assert_body_matches_async_task(body, task):
