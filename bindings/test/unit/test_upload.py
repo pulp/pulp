f8b55cf33c63058fca9cb63c2ddc19e67a035898
Sayli Karmarkar <skarmark@redhat.com>
2014-06-05 11:21:45 -0700

    removing unnecessary import and copyright declaration

--- bindings/test/unit/test_upload.py
+++ bindings/test/unit/test_upload.py
@@ -1,21 +1,8 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2014 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the License
-# (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied, including implied
-# warranties of MERCHANTABILITY, NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR
-# PURPOSE. You should have received a copy of GPLv2 along with this software;
-# if not, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
 import unittest
 
 import mock
 
 from pulp.bindings.upload import UploadAPI
-from pulp.common.compat import json
 
 
 class TestUploadAPI(unittest.TestCase):
