fcf4bf13ce2b4e228af3df7e34ff6fddec2aa9a1
Sayli Karmarkar <skarmark@redhat.com>
2014-07-14 21:27:02 -0700

    minor update in unit tests

--- bindings/test/unit/test_server.py
+++ bindings/test/unit/test_server.py
@@ -3,7 +3,7 @@ import unittest
 import mock
 from M2Crypto import httpslib, SSL
 
-from pulp.bindings.server import PulpConnection
+from pulp.bindings.server import PulpConnection, HTTPSServerWrapper
 
 
 class TestServerBindings(unittest.TestCase):
@@ -22,9 +22,10 @@ class TestServerBindings(unittest.TestCase):
     @mock.patch.object(SSL.Context, 'load_verify_locations')
     @mock.patch.object(SSL.Context, 'set_verify')
     def test_https_wrapper_with_ssl_validation(self, mock_set_verify, mock_load_verify_locations,
-                                               *mocks):
+                                               mock_getresponse, mock_request):
         pulp_connection = PulpConnection('test_host', username='username', password='password')
-        pulp_connection.DELETE("/some/path", body={})
+        https_server_wrapper = HTTPSServerWrapper(pulp_connection)
+        https_server_wrapper.request('DELETE', 'https://some/url', {})
         mock_set_verify.assert_called_once_with(SSL.verify_peer, 1)
         mock_load_verify_locations.assert_called_once_with(capath=pulp_connection.system_ca_dir)
 
@@ -32,12 +33,11 @@ class TestServerBindings(unittest.TestCase):
     @mock.patch.object(httpslib.HTTPSConnection, 'getresponse')
     @mock.patch.object(SSL.Context, 'load_verify_locations')
     @mock.patch.object(SSL.Context, 'set_verify')
-    def test_https_wrapper_without_ssl_validation(self, mock_set_verify,
-                                                  mock_load_verify_locations, *mocks):
+    def test_https_wrapper_no_ssl_validation(self, mock_set_verify, mock_load_verify_locations,
+                                             mock_getresponse, mock_request):
         pulp_connection = PulpConnection('test_host', username='username', password='password',
                                          validate_ssl_ca=False)
-        self.assertEqual(pulp_connection.validate_ssl_ca, False)
-        pulp_connection.DELETE("/some/path", body={})
+        https_server_wrapper = HTTPSServerWrapper(pulp_connection)
+        https_server_wrapper.request('DELETE', 'https://some/url', {})
         self.assertFalse(mock_set_verify.called)
         self.assertFalse(mock_load_verify_locations.called)
-
