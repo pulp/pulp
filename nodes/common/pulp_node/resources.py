2375308fd6a43d52dbea46362cf487f77e2cf4cc
Jeff Ortel <jortel@redhat.com>
2014-08-13 15:30:21 -0500

    Support defaulting when loading nodes.conf.

--- nodes/common/pulp_node/resources.py
+++ nodes/common/pulp_node/resources.py
@@ -1,47 +1,9 @@
-from pulp.common.config import ANY, BOOL, Config, REQUIRED, parse_bool
+from pulp.common.config import parse_bool
 from pulp.server.config import config as pulp_conf
 from pulp.bindings.server import PulpConnection
 from pulp.bindings.bindings import Bindings
 
-
-NODE_CONFIGURATION_PATH = '/etc/pulp/nodes.conf'
-CONSUMER_CONFIGURATION_PATH = '/etc/pulp/consumer/consumer.conf'
-
-NODE_SCHEMA = (
-    ('main', REQUIRED,
-        (
-            ('ca_path', REQUIRED, ANY),
-            ('node_certificate', REQUIRED, ANY),
-            ('verify_ssl', REQUIRED, BOOL),
-        )
-    ),
-    ('oauth', REQUIRED,
-        (
-            ('user_id', REQUIRED, ANY),
-        )
-    ),
-    ('parent_oauth', REQUIRED,
-        (
-            ('key', REQUIRED, ANY),
-            ('secret', REQUIRED, ANY),
-            ('user_id', REQUIRED, ANY),
-        )
-    ),
-)
-
-
-def node_configuration(path=NODE_CONFIGURATION_PATH):
-    """
-    Get the node configuration object.
-    The node configuration is overridden using values from the pulp
-    consumer.conf and defaulted using server.conf as appropriate.
-    :param path: The optional path to the configuration.
-    :return: The configuration object.
-    :rtype: pulp.common.config.Graph
-    """
-    cfg = Config(path)
-    cfg.validate(NODE_SCHEMA)
-    return cfg.graph()
+from pulp_node.config import read_config
 
 
 def parent_bindings(host, port=443):
@@ -54,7 +16,7 @@ def parent_bindings(host, port=443):
     :return: A pulp bindings object.
     :rtype: Bindings
     """
-    node_conf = node_configuration()
+    node_conf = read_config()
     oauth = node_conf.parent_oauth
     verify_ssl = parse_bool(node_conf.main.verify_ssl)
     ca_path = node_conf.main.ca_path
@@ -78,9 +40,9 @@ def pulp_bindings():
     :return: A pulp bindings object.
     :rtype: Bindings
     """
-    node_conf = node_configuration()
+    node_conf = read_config()
     oauth = node_conf.oauth
-    verify_ssl = False if node_conf.main.verify_ssl.lower() == 'false' else True
+    verify_ssl = parse_bool(node_conf.main.verify_ssl)
     ca_path = node_conf.main.ca_path
     host = pulp_conf.get('server', 'server_name')
     key = pulp_conf.get('oauth', 'oauth_key')
