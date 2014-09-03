8331f34067cde84fcd3518ffa93f5d77f9b5aaa5
Brian Bouterse <bmbouter@gmail.com>
2014-09-03 10:10:45 -0400

    Modifies indention for enable.sh when using <<

--- server/selinux/server/enable.sh
+++ server/selinux/server/enable.sh
@@ -10,15 +10,6 @@ else
     INSTALL_DIR=$1
 fi
 
-# httpd_can_network_connect
-#   bz 736788, allows repo sync through a proxy to work
-
-# httpd_tmp_exec
-#   Pulp is indirectly creating a script in /tmp and asking Apache to execute it 
-#   possibly from mod_wsgi?
-#   TODO: This is an area to investigate further.
-#   Ideal is to remove the ability for Apache to execute temporary files
-
 if /usr/sbin/selinuxenabled ; then
     for NAME in ${PACKAGE_NAMES[@]}
     do
@@ -28,11 +19,20 @@ if /usr/sbin/selinuxenabled ; then
         done
     done
 
-    # Turn on booleans
-    /usr/sbin/semanage -i - << _EOF
-        boolean -m --on httpd_can_network_connect
-        boolean -m --on httpd_tmp_exec
-    _EOF
+# httpd_can_network_connect
+#   bz 736788, allows repo sync through a proxy to work
+
+# httpd_tmp_exec
+#   Pulp is indirectly creating a script in /tmp and asking Apache to execute it
+#   possibly from mod_wsgi?
+#   TODO: This is an area to investigate further.
+#   Ideal is to remove the ability for Apache to execute temporary files
+
+# Turn on booleans
+/usr/sbin/semanage -i - << _EOF
+boolean -m --on httpd_can_network_connect
+boolean -m --on httpd_tmp_exec
+_EOF
 
     semanage port -l | grep amqp_port_t | grep tcp | grep 5672 > /dev/null || \
         /usr/sbin/semanage port -a -t amqp_port_t -p tcp 5672
