89a0f008611367f6877c4a1717a467498e15b1a3
Randy Barlow <rbarlow@redhat.com>
2014-09-11 12:59:41 -0400

    1131260 - Shell out to for certificate validation.
    Due to difficulty in finding an easily supportable method for
    certificate validation in Python, this commit replaces our use of
    libcrypto with a subprocess call to openssl. The libcrypto attempt was
    clean but had thread safety issues. Pulp does not work without threading
    at the moment.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1131260

--- server/pulp/server/common/openssl.py
+++ server/pulp/server/common/openssl.py
@@ -1,117 +1,76 @@
 """
-OpenSSL (libcrypto) wrapper.
+OpenSSL wrapper.
 """
-
-from ctypes import CDLL
-from ctypes.util import find_library
-from ctypes import c_char_p
-
-
-# OpenSSL lib
-openssl = CDLL(find_library('crypto'))
-
-
-class BIO(object):
-    """
-    OpenSSL BIO object.
-    :ivar ptr: A pointer to the C structure.
-    :type ptr: BIO
-    """
-
-    def __init__(self, content):
-        """
-        :param content: The BIO content.
-        :type content: str
-        """
-        self.ptr = openssl.BIO_new_mem_buf(c_char_p(content), len(content))
-
-    def __del__(self):
-        if self.ptr:
-            openssl.BIO_free(self.ptr)
-            self.ptr = 0
-
-
-class Store(object):
-    """
-    OpenSSL certificate store.
-    :ivar ptr: A pointer to the C structure.
-    :type ptr: X509_STORE
-    """
-
-    def __init__(self):
-        self.ptr = openssl.X509_STORE_new()
-
-    def add(self, certificate):
-        """
-        Add a certificate to the store.
-        :param certificate: The certificate to add.
-        :type certificate: Certificate
-        """
-        openssl.X509_STORE_add_cert(self.ptr, certificate.ptr)
-
-    def __del__(self):
-        if self.ptr:
-            openssl.X509_STORE_free(self.ptr)
-            self.ptr = 0
-
-
-class StoreContext(object):
-    """
-    OpenSSL certificate store context.
-    :ivar ptr: A pointer to the C structure.
-    :type ptr: X509_STORE_CTX
-    """
-
-    def __init__(self, store, certificate):
-        """
-        :param store: A certificate store.
-        :type store: Store
-        :param certificate: Used to initialize the context.
-        :type certificate: Certificate
-        """
-        self.ptr = openssl.X509_STORE_CTX_new()
-        openssl.X509_STORE_CTX_init(self.ptr, store.ptr, certificate.ptr, None)
-
-    def __del__(self):
-        if self.ptr:
-            openssl.X509_STORE_CTX_free(self.ptr)
-            self.ptr = 0
+import os
+import shutil
+import subprocess
+import tempfile
 
 
 class Certificate(object):
     """
     OpenSSL x.509 certificate
-    :ivar ptr: A pointer to the C structure.
-    :type ptr: X509
     """
 
     def __init__(self, pem):
         """
         :param pem: A PEM encoded certificate.
-        :type pem: str
+        :type  pem: str
         """
-        bio = BIO(pem)
-        self.ptr = openssl.PEM_read_bio_X509(bio.ptr, None, 0, None)
-        del bio
+        self._cert = pem
+        self._tempdir = None
 
     def verify(self, ca_chain):
         """
-        Verify the certificate.
+        Verify the certificate. This function will ensure that the Certificate is not expired, and
+        that it is signed by at least one of the CAs in the ca_chain. It will return True if it is
+        valid, and False otherwise.
+
         :param ca_chain: A list of CA certificates.
-        :type ca_chain: iterable
-        :return: True if verified.
-        :rtype: bool
+        :type  ca_chain: iterable
+        :return:         True if verified.
+        :rtype:          bool
         """
-        store = Store()
-        for ca in ca_chain:
-            store.add(ca)
-        ctx = StoreContext(store, self)
-        retval = openssl.X509_verify_cert(ctx.ptr)
-        del ctx
-        del store
-        return retval == 1
+        # We must be sure to cleanup no matter what happens
+        try:
+            self._tempdir = tempfile.mkdtemp()
+
+            # Write the cert to a tempfile
+            cert = tempfile.NamedTemporaryFile(mode='w', dir=self._tempdir, delete=False)
+            cert.write(self._cert)
+            cert.close()
+
+            # Check to make sure the client cert is not expired
+            args = ['openssl', 'x509', '-in', cert.name, '-noout', '-checkend', '0']
+            try:
+                subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            except subprocess.CalledProcessError:
+                # The certificate is expired
+                return False
+
+            # Write the CAs to a tempfile
+            ca = tempfile.NamedTemporaryFile(mode='w', dir=self._tempdir, delete=False)
+            ca.write('\n'.join(ca_chain))
+            ca.close()
+
+            # Now let's check to make sure the certificate is signed by one of our trusted CAs
+            args = ['openssl', 'verify', '-CAfile', ca.name, '-purpose', 'sslclient', cert.name]
+            try:
+                subprocess.check_call(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            except subprocess.CalledProcessError:
+                # The certificate is not signed by a trusted CA
+                return False
+
+            # If we've survived the above carnage, this cert is legit
+            return True
+        finally:
+            # Make sure we always clean up
+            self.__del__()
 
     def __del__(self):
-        if self.ptr:
-            openssl.X509_free(self.ptr)
-            self.ptr = 0
+        """
+        Remove the temporary files if they still exist.
+        """
+        if self._tempdir:
+            shutil.rmtree(self._tempdir)
+            self._tempdir = None
