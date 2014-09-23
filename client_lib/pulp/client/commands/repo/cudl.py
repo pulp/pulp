d089d45079e1bb07825ad2dfeb80320894a5d2c6
asmacdo <asmacdo@gmail.com>
2014-09-23 16:42:42 -0400

    Do not fail if repo_list is empty

--- client_lib/pulp/client/commands/repo/cudl.py
+++ client_lib/pulp/client/commands/repo/cudl.py
@@ -491,13 +491,14 @@ def _default_summary_view(repo_list, prompt):
 
     terminal_width = prompt.terminal_size()[0]
 
-    max_id_width = max(len(r['id']) for r in repo_list)
-    max_name_width = terminal_width - max_id_width - 1 # -1 for space between columns
+    if repo_list:
+        max_id_width = max(len(r['id']) for r in repo_list)
+        max_name_width = terminal_width - max_id_width - 1 # -1 for space between columns
 
-    line_template = '%s  %s'
+        line_template = '%s  %s'
 
-    for repo in repo_list:
-        id_value = repo['id'] + ' ' * (max_id_width - len(repo['id']))
-        name_value = repo['display_name'][0:max_name_width]
-        line = line_template % (id_value, name_value)
-        prompt.write(line, skip_wrap=True)
+        for repo in repo_list:
+            id_value = repo['id'] + ' ' * (max_id_width - len(repo['id']))
+            name_value = repo['display_name'][0:max_name_width]
+            line = line_template % (id_value, name_value)
+            prompt.write(line, skip_wrap=True)
