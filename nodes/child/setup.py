d23f9580356841fcdd376f6a02fa5afaaf2b5678
Randy Barlow <rbarlow@redhat.com>
2014-10-13 14:36:11 -0400

    1150297 - Update versions from 2.4.x to 2.5.0.
    https://bugzilla.redhat.com/show_bug.cgi?id=1150297

--- nodes/child/setup.py
+++ nodes/child/setup.py
@@ -1,17 +1,8 @@
-#!/usr/bin/python
-#
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
 from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_child',
-    version='2.4.0',
+    version='2.5.0',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
