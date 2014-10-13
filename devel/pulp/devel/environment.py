c57cc6fb25c235bd486d940f34dae30a61262ef2
Randy Barlow <rbarlow@redhat.com>
2014-10-13 15:15:04 -0400

    Allow specifying the location of manage_setup_pys.

--- devel/pulp/devel/environment.py
+++ devel/pulp/devel/environment.py
@@ -9,18 +9,21 @@ import subprocess
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..', '..', '..')
 
 
-def _manage_setup_pys(action):
+def _manage_setup_pys(action, path_to_manage_script=ROOT_DIR):
     """
     This function can install or uninstall the Pulp Python packages in developer mode.
 
-    :param action: Which action you want to perform. May be "install" or "uninstall".
-    :type  action: basestring
+    :param action:                Which action you want to perform. May be "install" or "uninstall".
+    :type  action:                basestring
+    :param path_to_manage_script: The path to the directory containing a manage_setup_pys.sh that
+                                  wish to call.
+    :type  path_to_manage_script: basestring
     """
     command = ['./manage_setup_pys.sh', 'develop']
     if action == 'uninstall':
         command.append('--uninstall')
 
     starting_cwd = os.getcwd()
-    os.chdir(ROOT_DIR)
+    os.chdir(path_to_manage_script)
     subprocess.call(command)
     os.chdir(starting_cwd)
