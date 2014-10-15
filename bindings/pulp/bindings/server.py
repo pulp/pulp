a26567d5b226d1799bd97c03a262c9c15393c917
Randy Barlow <rbarlow@redhat.com>
2014-10-15 16:06:12 -0400

    1153054 - pulp.bindings refuse to do SSLv3.
    This commit configured pulp.bindings.server to refuse to do SSLv2,
    SSLv3, and TLSv1.0.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1153054

--- bindings/pulp/bindings/server.py
+++ bindings/pulp/bindings/server.py
@@ -10,7 +10,7 @@ except ImportError:
     oauth = None
 
 from types import NoneType
-from M2Crypto import SSL, httpslib
+from M2Crypto import httpslib, m2, SSL
 
 from pulp.bindings import exceptions
 from pulp.bindings.responses import Response, Task
@@ -268,7 +268,8 @@ class HTTPSServerWrapper(object):
 
         # Create a new connection each time since HTTPSConnection has problems
         # reusing a connection for multiple calls (lame).
-        ssl_context = SSL.Context('sslv3')
+        ssl_context = SSL.Context('sslv23')
+        ssl_context.set_options(m2.SSL_OP_NO_SSLv2 | m2.SSL_OP_NO_SSLv3 | m2.SSL_OP_NO_TLSv1)
         if self.pulp_connection.validate_ssl_ca:
             ssl_context.set_verify(SSL.verify_peer, depth=100)
             # We need to stat the ca_path to see if it exists (error if it doesn't), and if so
