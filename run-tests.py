87ec45bf41aec40de54834122687601434361c3c
asmacdo <asmacdo@gmail.com>
2014-11-05 16:58:31 -0500

    root directory style cleanup

--- run-tests.py
+++ run-tests.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright © 2013 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
 import os
 import subprocess
 
@@ -24,7 +14,8 @@ subprocess.call(['find', PROJECT_DIR, '-name', '*.pyc', '-delete'])
 PACKAGES = [
     os.path.dirname(__file__),
     'pulp',
-    'pulp_node',]
+    'pulp_node',
+]
 
 
 TESTS_ALL_PLATFORMS = [
