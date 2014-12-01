fb2ed52c7e3ccbe59b17f54764d31002abc3db37
Randy Barlow <rbarlow@redhat.com>
2014-12-01 14:12:16 -0500

    PEP-8 tests for server.managers.schedule.repo.

--- server/test/unit/server/managers/schedule/test_repo.py
+++ server/test/unit/server/managers/schedule/test_repo.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright © 2014 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import functools
 import itertools
@@ -22,6 +11,7 @@ from pulp.server.db.model.dispatch import ScheduledCall
 from pulp.server.db.model.repository import RepoImporter, RepoDistributor
 from pulp.server.managers.factory import initialize
 from pulp.server.managers.schedule.repo import RepoSyncScheduleManager, RepoPublishScheduleManager
+
 
 initialize()
 
@@ -39,7 +29,8 @@ class TestSyncList(unittest.TestCase):
     def test_list(self, mock_get_by_resource, mock_validate_importer):
         ret = RepoSyncScheduleManager.list('repo1', 'importer1')
 
-        mock_get_by_resource.assert_called_once_with(RepoImporter.build_resource_tag('repo1', 'importer1'))
+        mock_get_by_resource.assert_called_once_with(
+            RepoImporter.build_resource_tag('repo1', 'importer1'))
         self.assertTrue(ret is mock_get_by_resource.return_value)
 
 
@@ -61,7 +52,7 @@ class TestSyncCreate(unittest.TestCase):
     @mock.patch('pulp.server.managers.schedule.utils.validate_initial_schedule_options')
     @mock.patch('pulp.server.managers.schedule.utils.validate_keys')
     def test_utils_validation(self, mock_validate_keys, mock_validate_options,
-                             mock_validate_importer, mock_save):
+                              mock_validate_importer, mock_save):
         RepoSyncScheduleManager.create(self.repo, self.importer, self.options, self.schedule)
 
         mock_validate_keys.assert_called_once_with(self.options, ('override_config',))
@@ -207,7 +198,8 @@ class TestPublishList(unittest.TestCase):
     def test_list(self, mock_get_by_resource, mock_validate_distributor):
         ret = RepoPublishScheduleManager.list('repo1', 'distributor1')
 
-        mock_get_by_resource.assert_called_once_with(RepoDistributor.build_resource_tag('repo1', 'distributor1'))
+        mock_get_by_resource.assert_called_once_with(
+            RepoDistributor.build_resource_tag('repo1', 'distributor1'))
         self.assertTrue(ret is mock_get_by_resource.return_value)
 
 
@@ -296,7 +288,8 @@ class TestPublishUpdate(unittest.TestCase):
     def test_validate_options(self, mock_get_distributor, mock_validate_options, mock_update):
         mock_get_distributor.return_value = {'id': self.distributor}
 
-        RepoPublishScheduleManager.update(self.repo, self.distributor, self.schedule_id, self.updates)
+        RepoPublishScheduleManager.update(self.repo, self.distributor, self.schedule_id,
+                                          self.updates)
 
         mock_validate_options.assert_called_once_with(self.updates)
 
@@ -305,7 +298,8 @@ class TestPublishUpdate(unittest.TestCase):
     def test_update(self, mock_get_distributor, mock_update):
         mock_get_distributor.return_value = {'id': self.distributor}
 
-        ret = RepoPublishScheduleManager.update(self.repo, self.distributor, self.schedule_id, self.updates)
+        ret = RepoPublishScheduleManager.update(self.repo, self.distributor, self.schedule_id,
+                                                self.updates)
 
         mock_update.assert_called_once_with(self.schedule_id, self.updates)
         # make sure it passes through the return value from utils.update
@@ -317,7 +311,7 @@ class TestPublishUpdate(unittest.TestCase):
         mock_get_distributor.return_value = None
 
         RepoPublishScheduleManager.update(self.repo, self.distributor, self.schedule_id,
-                                       {'override_config': {'foo': 'bar'}})
+                                          {'override_config': {'foo': 'bar'}})
 
         mock_update.assert_called_once_with(self.schedule_id,
                                             {'kwargs': {'overrides': {'foo': 'bar'}}})
@@ -388,13 +382,21 @@ SCHEDULES = [
         u'last_run_at': u'2013-12-17T00:35:53Z',
         u'last_updated': 1387218569.811224,
         u'next_run': u'2013-12-17T00:36:53Z',
-        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\nObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\np10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\nVadmin\np16\nsVpassword\np17\nVV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\np19\nV5220ab06e19a0010e1690589\np20\ns.",
+        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\n"
+                      u"ObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1"
+                      u"\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\n"
+                      u"p10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\n"
+                      u"Vadmin\np16\nsVpassword\np17\n"
+                      u"VV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\n"
+                      u"p19\nV5220ab06e19a0010e1690589\np20\ns.",
         u'remaining_runs': None,
         u'resource': u'pulp:distributor:demo:puppet_distributor',
-        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\nsS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\nI0\ntp10\nRp11\nsb.",
+        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\n"
+                     u"c__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\n"
+                     u"sS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\n"
+                     u"I0\ntp10\nRp11\nsb.",
         u'task': u'pulp.server.tasks.repository.publish',
-        u'total_run_count': 1087,
-        },
+        u'total_run_count': 1087},
     {
         u'_id': u'529f4bd93de3a31d0ec77339',
         u'args': [u'demo2', u'puppet_distributor'],
@@ -407,13 +409,21 @@ SCHEDULES = [
         u'last_run_at': u'2013-12-17T00:35:53Z',
         u'last_updated': 1387218500.598727,
         u'next_run': u'2013-12-17T00:36:53Z',
-        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\nObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\np10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\nVadmin\np16\nsVpassword\np17\nVV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\np19\nV5220ab06e19a0010e1690589\np20\ns.",
+        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\n"
+                      u"ObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1" +
+                      u"\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\n" +
+                      u"p10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\n" +
+                      u"Vadmin\np16\nsVpassword\np17\n" +
+                      u"VV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\n" +
+                      u"p19\nV5220ab06e19a0010e1690589\np20\ns.",
         u'remaining_runs': None,
         u'resource': u'pulp:distributor:demo:puppet_distributor',
-        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\nsS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\nI0\ntp10\nRp11\nsb.",
+        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\n" +
+                     u"c__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\n" +
+                     u"sS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\n" +
+                     u"I0\ntp10\nRp11\nsb.",
         u'task': u'pulp.server.tasks.repository.publish',
-        u'total_run_count': 1087,
-        },
+        u'total_run_count': 1087},
     {
         u'_id': u'529f4bd93de3a31d0ec77340',
         u'args': [u'demo3', u'puppet_distributor'],
@@ -426,10 +436,19 @@ SCHEDULES = [
         u'last_run_at': u'2013-12-17T00:35:53Z',
         u'last_updated': 1387218501.598727,
         u'next_run': u'2013-12-17T00:36:53Z',
-        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\nObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\np10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\nVadmin\np16\nsVpassword\np17\nVV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\np19\nV5220ab06e19a0010e1690589\np20\ns.",
+        u'principal': u"(dp0\nV_id\np1\nccopy_reg\n_reconstructor\np2\n(cbson.objectid\n" +
+                      u"ObjectId\np3\nc__builtin__\nobject\np4\nNtp5\nRp6\nS'R \\xab\\x06\\xe1" +
+                      u"\\x9a\\x00\\x10\\xe1i\\x05\\x89'\np7\nbsVname\np8\nVadmin\np9\nsVroles\n" +
+                      u"p10\n(lp11\nVsuper-users\np12\nasV_ns\np13\nVusers\np14\nsVlogin\np15\n" +
+                      u"Vadmin\np16\nsVpassword\np17\n" +
+                      u"VV76Yol1XYgM=,S/G6o5UyMrn0xAwbQCqFcrXnfXTh84RWhunanCDkSCo=\np18\nsVid\n" +
+                      u"p19\nV5220ab06e19a0010e1690589\np20\ns.",
         u'remaining_runs': 0,
         u'resource': u'pulp:distributor:demo:puppet_distributor',
-        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\nc__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\nsS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\nI0\ntp10\nRp11\nsb.",
+        u'schedule': u"ccopy_reg\n_reconstructor\np0\n(ccelery.schedules\nschedule\np1\n" +
+                     u"c__builtin__\nobject\np2\nNtp3\nRp4\n(dp5\nS'relative'\np6\nI00\n" +
+                     u"sS'nowfun'\np7\nNsS'run_every'\np8\ncdatetime\ntimedelta\np9\n(I0\nI60\n" +
+                     "I0\ntp10\nRp11\nsb.",
         u'task': u'pulp.server.tasks.repository.publish',
         u'total_run_count': 1087,
     },
