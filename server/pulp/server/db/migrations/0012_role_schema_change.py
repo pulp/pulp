f8d7b11c98d5c1d6cda55e086a177331f8a59dd2
Patrick Creech <pcreech17@gmail.com>
2014-12-04 21:54:47 -0500

    1099649 - Allow '.' in role resource permissions

---
+++ server/pulp/server/db/migrations/0012_role_schema_change.py
@@ -0,0 +1,16 @@
+from pulp.server.db.model.auth import Role
+
+
+def migrate(*args, **kwargs):
+    """
+    Move role permissions into the permissions database
+    """
+    collection = Role.get_collection()
+    for role in collection.find({}):
+        updated_permissions = []
+        if isinstance(role['permissions'], dict):
+            for resource, permission in role['permissions'].items():
+                resource_permission = dict(resource=resource, permission=permission)
+                updated_permissions.append(resource_permission)
+            role['permissions'] = updated_permissions
+            collection.save(role, safe=True)
