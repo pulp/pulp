c07566431db7da5574cb69f024d89a12245d703d
Brian Bouterse <bmbouter@gmail.com>
2014-12-10 10:17:06 -0500

    Removes CSRF middleware and sets SECRET_KEY

--- server/pulp/server/webservices/urls.py
+++ server/pulp/server/webservices/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 from pulp.server.webservices.views.tasks import TasksView
+
 
 urlpatterns = patterns('',
     url(r'^v2/tasks/$', TasksView.as_view()),
