11889f31158039070fab305dbc44f51f62817401
asmacdo <asmacdo@gmail.com>
2014-12-15 10:41:46 -0500

    merge from upstream to allow auth_required

--- server/pulp/server/webservices/views/content.py
+++ server/pulp/server/webservices/views/content.py
@@ -1,18 +1,20 @@
 from gettext import gettext as _
 import json
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views.generic import View
-from django.core.urlresolvers import reverse
 
+from pulp.server.auth import authorization
 from pulp.server.db.model.criteria import Criteria
 from pulp.server.managers import factory
 from pulp.server.webservices import serialization
 from pulp.server.webservices.controllers.base import json_encoder as pulp_json_encoder
+from pulp.server.webservices.controllers.decorators import auth_required
 
 
 class ContentTypesView(View):
-    # @auth_required(authorization.READ)
+    @auth_required(authorization.READ)
     def get(self, request, *args, **kwargs):
         """
         List the available content types.
@@ -28,13 +30,12 @@ class ContentTypesView(View):
 
 
 class ContentTypeResourceView(View):
-    # @auth_required(authorization.READ)
-    def get(self, request, *args, **kwargs):
+    @auth_required(authorization.READ)
+    def get(self, request, type_id, *args, **kwargs):
         """
         Return information about a content type. Requires type_id to be
         passed as a keyword argument.
         """
-        type_id = kwargs.get('type_id')
         cqm = factory.content_query_manager()
         content_type = cqm.get_content_type(type_id)
         if content_type is None:
@@ -57,20 +58,17 @@ class ContentTypeResourceView(View):
 class ContentUnitsCollectionView(View):
     @staticmethod
     def process_unit(unit, request):
-        # TODO (bmbouter) this function could probably be included in the base SearchAPIView
         unit = serialization.content.content_unit_obj(unit)
         # TODO(asmacdo) replace with reverseurl lookup
         unit.update({'_href': '/'.join([request.get_full_path().rstrip('/'), unit['_id'] + '/'])})
         unit.update({'children': serialization.content.content_unit_child_link_objs(unit)})
         return unit
 
-    # @auth_required(authorization.READ)
-    def get(self, request, *args, **kwargs):
+    @auth_required(authorization.READ)
+    def get(self, request, type_id, *args, **kwargs):
         """
         List all the available content units.
         """
-        type_id = kwargs.get('type_id')
-
         cqm = factory.content_query_manager()
         units = cqm.find_by_criteria(type_id, Criteria())
         return HttpResponse(json.dumps([self.process_unit(unit, request) for unit in units],
