dd9708408c2164440bc9359e10375ae5b112f6c4
Brian Bouterse <bmbouter@gmail.com>
2014-12-15 08:37:23 -0500

    Ports middleware to Django, and adds repo sync view

--- server/pulp/server/webservices/urls.py
+++ server/pulp/server/webservices/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 
 from pulp.server.webservices.views.tasks import TasksView
+from pulp.server.webservices.views.repositories import RepoSync
 
 
 urlpatterns = patterns('',
     url(r'^v2/tasks/$', TasksView.as_view()),
+    url(r'^v2/repositories/(?P<repo_id>\w+)/actions/sync/$', RepoSync.as_view()),
 )
