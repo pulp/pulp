73abdc04e1d7c1fbbf9b9d7c876da1b8574831ae
asmacdo <asmacdo@gmail.com>
2014-12-15 13:45:34 -0500

    WIP

--- server/pulp/server/webservices/urls.py
+++ server/pulp/server/webservices/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
+from pulp.server.webservices.views.content import UploadsCollectionView
 from pulp.server.webservices.views.tasks import TasksView
 from pulp.server.webservices.views.repositories import RepoSync
 
@@ -7,4 +8,5 @@ from pulp.server.webservices.views.repositories import RepoSync
 urlpatterns = patterns('',
     url(r'^v2/tasks/$', TasksView.as_view()),
     url(r'^v2/repositories/(?P<repo_id>\w+)/actions/sync/$', RepoSync.as_view()),
+    url(r'^v2/content/uploads/$', UploadsCollectionView.as_view()),
 )
