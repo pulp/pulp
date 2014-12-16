a0e0cb8ca8a8d688e4ad60bffcc25f94cb8b32f8
asmacdo <asmacdo@gmail.com>
2014-12-16 16:56:51 -0500

    PR comments and improvements

--- server/test/unit/server/webservices/views/test_content.py
+++ server/test/unit/server/webservices/views/test_content.py
@@ -13,22 +13,26 @@ from pulp.server.webservices.views.content import ContentTypesView
 class TestContentTypesView(unittest.TestCase):
 
     @mock.patch('pulp.server.webservices.views.content.factory')
-    def test_get(self, mock_factory):
+    def test_content_types_view_get(self, mock_factory):
+        """
+        ContentTypesView should return an HttpResponse. The content of the
+        response should be JSON containing the appropriate data.
+        """
         mock_cqm = mock.MagicMock()
         mock_cqm.list_content_types.return_value = ['rpm']
         mock_factory.content_query_manager.return_value = mock_cqm
         request = mock.MagicMock()
 
-        self.content_types_view = ContentTypesView()
-        response = self.content_types_view.get(request)
+        content_types_view = ContentTypesView()
+        response = content_types_view.get(request)
 
-        self.assertIsInstance(response, HttpResponse)
-        self.assertNotIsInstance(response, HttpResponseNotFound)
+        self.assertTrue(isinstance(response, HttpResponse))
+        self.assertFalse(isinstance(response, HttpResponseNotFound))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response._headers.get('content-type'), ('Content-Type', 'application/json'))
-        self.assertIn('_href', json.loads(response.content)[0])
-        self.assertIn('content_type', json.loads(response.content)[0])
+        self.assertEqual(response._headers.get('content-type'),
+                         ('Content-Type', 'application/json'))
 
+        content = json.loads(response.content)[0]
         expected_href = '/v2/content/types/rpm/'
-        self.assertEqual(expected_href, json.loads(response.content)[0]['_href'])
-        self.assertEqual('rpm', json.loads(response.content)[0]['content_type'])
+        self.assertEqual(expected_href, content['_href'])
+        self.assertEqual('rpm', content['content_type'])
