355100fe4a40a7cea73be0a7b30dfa92439eb803
Austin Macdonald <asmacdo@gmail.com>
2015-01-13 14:38:56 -0500

    add wpost for testing

--- server/pulp/server/webservices/views/content.py
+++ server/pulp/server/webservices/views/content.py
@@ -1,6 +1,3 @@
-import json
-
-from django.http import HttpResponse
 from django.views.generic import View
 
 from pulp.common import tags
@@ -8,15 +5,11 @@ from pulp.server.auth import authorization
 from pulp.server.exceptions import OperationPostponed
 from pulp.server.managers.content import orphan as orphan_manager
 from pulp.server.webservices.controllers.decorators import auth_required
-# from pulp.server.manager import factory
 
-from logging import getLogger
-
-_logger = getLogger(__name__)
 
 class DeleteOrphansActionView(View):
 
-#     # @auth_required(authorization.DELETE)
+    # @auth_required(authorization.DELETE)
     def post(self, request, *args, **kwargs):
         orphans = request.body_as_json
         task_tags = [tags.action_tag('delete_orphans'),
