867da1c95f6a939e632e5d4f7165e3af3c66bab0
Austin Macdonald <asmacdo@gmail.com>
2015-01-14 16:07:47 -0500

    create base for plugins view module

---
+++ server/test/unit/server/webservices/test_urls.py
@@ -0,0 +1,24 @@
+import unittest
+
+from django.core.urlresolvers import resolve, Resolver404
+
+
+class TestDjangoPluginsUrls(unittest.TestCase):
+
+    def test_match_distributor_resource_view(self):
+        pass
+
+    def test_match_distributors_view(self):
+        pass
+
+    def test_match_importer_resource_view(self):
+        pass
+
+    def test_match_importers_view(self):
+        pass
+
+    def test_match_type_resource_view(self):
+        pass
+
+    def test_match_types_view(self):
+        pass
