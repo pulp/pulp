7466835475c8063d87faca91770f889600956614
Austin Macdonald <asmacdo@gmail.com>
2015-01-14 17:36:10 -0500

    port plugins-types view to django

--- server/pulp/server/webservices/views/plugins.py
+++ server/pulp/server/webservices/views/plugins.py
@@ -1,11 +1,10 @@
-import json
-
-from django.http import HttpResponse
 from django.views.generic import View
 
 from pulp.server.auth import authorization
 from pulp.server.managers import factory
 from pulp.server.webservices.controllers.decorators import auth_required
+from pulp.server.webservices.views.util import (generate_json_response,
+                                                generate_json_response_with_pulp_encoder)
 
 
 class DistributorResourceView(View):
@@ -29,4 +28,26 @@ class TypeResourceView(View):
 
 
 class TypesView(View):
-    pass
+    """
+    View for dealing with all plugin types.
+    """
+
+    @auth_required(authorization.READ)
+    def get(self, request):
+        """
+        Get all type definitions
+
+        :param request: WSGI Request obect
+        :type  request: django.core.handlers.wsgi.WSGIRequest
+        :return       : Serialized list of objects representing all available content types
+        :rtype        : HttpResponse
+        """
+        manager = factory.plugin_manager()
+        type_defs = manager.types()
+
+        for type_definition in type_defs:
+            href = {'_href': '/'.join([request.get_full_path().rstrip('/'),
+                                       type_definition['id'], ''])}
+            type_definition.update(href)
+
+        return generate_json_response_with_pulp_encoder(type_defs)
--- server/test/unit/server/webservices/views/test_plugins.py
+++ server/test/unit/server/webservices/views/test_plugins.py
@@ -1,9 +1,8 @@
-import json
 import unittest
 
 import mock
-from django.http import HttpResponse, HttpResponseNotFound
 
+from .base import assert_auth_READ
 from pulp.server.webservices.views.plugins import (DistributorResourceView, DistributorsView,
                                                    ImporterResourceView, ImportersView,
                                                    TypeResourceView, TypesView)
@@ -30,4 +29,28 @@ class TestTypeResourceView(unittest.TestCase):
 
 
 class TestTypesView(unittest.TestCase):
-    pass
+    """
+    Tests for the views for all plugin types.
+    """
+
+    @mock.patch('pulp.server.webservices.controllers.decorators._verify_auth',
+                new=assert_auth_READ())
+    @mock.patch('pulp.server.webservices.views.plugins.generate_json_response_with_pulp_encoder')
+    @mock.patch('pulp.server.webservices.views.plugins.factory')
+    def test_get_types_view(self, mock_factory, mock_serializer):
+        """
+        Types View should return a list of objects representing each type.
+        """
+        mock_type = [{'id': 'mock_id'}]
+        mock_plugin_manager = mock.MagicMock()
+        mock_plugin_manager.types.return_value = mock_type
+        mock_factory.plugin_manager.return_value = mock_plugin_manager
+        request = mock.MagicMock()
+        request.get_full_path.return_value = '/mock/path'
+
+        types_view = TypesView()
+        types_view.get(request)
+
+        expected_content = [{'id': 'mock_id', '_href': '/mock/path/mock_id/'}]
+        mock_serializer.assert_called_once_with(expected_content)
+
