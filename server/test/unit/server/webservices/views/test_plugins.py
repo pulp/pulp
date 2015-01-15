59a7e763db52d67ab86379af789859271a93b14e
Austin Macdonald <asmacdo@gmail.com>
2015-01-15 14:53:50 -0500

    django conversion - plugins/importers/importerid/

--- server/test/unit/server/webservices/views/test_plugins.py
+++ server/test/unit/server/webservices/views/test_plugins.py
@@ -3,6 +3,7 @@ import unittest
 import mock
 
 from .base import assert_auth_READ
+from pulp.server.exceptions import MissingResource
 from pulp.server.webservices.views.plugins import (DistributorResourceView, DistributorsView,
                                                    ImporterResourceView, ImportersView,
                                                    TypeResourceView, TypesView)
@@ -17,7 +18,54 @@ class TestDistributorsView(unittest.TestCase):
 
 
 class TestImporterResourceView(unittest.TestCase):
-    pass
+    """
+    Tests for the views of a single importer.
+    """
+
+    @mock.patch('pulp.server.webservices.controllers.decorators._verify_auth',
+                new=assert_auth_READ())
+    @mock.patch('pulp.server.webservices.views.plugins.generate_json_response')
+    @mock.patch('pulp.server.webservices.views.plugins.factory')
+    def test_get_importer_resource_view_existing_importer(self, mock_factory, mock_serializer):
+        """
+        Importer Resource should return a generate a serialized response with importer data.
+        """
+        mock_manager = mock.MagicMock()
+        mock_manager.importers.return_value = [{'id': 'mock_importer_1'}, {'id': 'mock_importer_2'}]
+        mock_factory.plugin_manager.return_value = mock_manager
+        request = mock.MagicMock()
+        request.get_full_path.return_value = '/mock/path/'
+
+        importer_resource = ImporterResourceView()
+        response = importer_resource.get(request, 'mock_importer_2')
+
+        expected_content = {'id': 'mock_importer_2', '_href': '/mock/path/'}
+        mock_serializer.assert_called_once_with(expected_content)
+        self.assertTrue(response is mock_serializer.return_value)
+
+    @mock.patch('pulp.server.webservices.controllers.decorators._verify_auth',
+                new=assert_auth_READ())
+    @mock.patch('pulp.server.webservices.views.plugins.factory')
+    def test_get_importer_resource_view_nonexistent_importer(self, mock_factory):
+        """
+        Importer Resource should raise a MissingResource if the specified importer does not exist.
+        """
+        mock_manager = mock.MagicMock()
+        mock_manager.importers.return_value = [{'id': 'mock_importer_1'}, {'id': 'mock_importer_2'}]
+        mock_factory.plugin_manager.return_value = mock_manager
+        request = mock.MagicMock()
+
+        importer_resource = ImporterResourceView()
+        try:
+            importer_resource.get(request, 'nonexistent_importer')
+        except MissingResource, response:
+            pass
+        else:
+            raise AssertionError("MissingResource should be raised for nonexistent_importer")
+
+        self.assertEqual(response.http_status_code, 404)
+        self.assertEqual(response.error_data['resources'],
+                         {'importer_type_id': 'nonexistent_importer'})
 
 
 class TestImportersView(unittest.TestCase):
