be0176d8a24b71d903e49dd8a7e19c241d316caf
Austin Macdonald <asmacdo@gmail.com>
2015-01-19 16:27:08 -0500

    create base for repo_groups django conversion

--- server/pulp/server/webservices/urls.py
+++ server/pulp/server/webservices/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import patterns, url
 from pulp.server.webservices.views.plugins import (DistributorResourceView, DistributorsView,
                                                    ImporterResourceView, ImportersView,
                                                    TypeResourceView, TypesView)
-from pulp.server.webservices.views.tasks import TasksView
 from pulp.server.webservices.views.repositories import RepoSync
+from pulp.server.webservices.views.repo_groups import (
+    RepoGroupAssociateView, RepoGroupDistributorResource, RepoGroupDistributorsView,
+    RepoGroupPublishView, RepoGroupResourceView, RepoGroupsView, RepoGroupUnassociateView
+)
+from pulp.server.webservices.views.tasks import TasksView
 
 
 urlpatterns = patterns('',
@@ -14,6 +18,19 @@ urlpatterns = patterns('',
     url(r'^v2/plugins/importers/(?P<importer_type_id>[^/]+)/$', ImporterResourceView.as_view()),
     url(r'^v2/plugins/types/$', TypesView.as_view(), name='plugin_types'),
     url(r'^v2/plugins/types/(?P<type_id>[^/]+)/$', TypeResourceView.as_view()),
-    url(r'^v2/tasks/$', TasksView.as_view()),
+    url(r'^v2/repo_groups/$', RepoGroupsView.as_view(), name='repo_groups'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/$', RepoGroupResourceView.as_view(),
+        name='repo_group_resource'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/actions/associate/$',
+        RepoGroupAssociateView.as_view(), name='repo_group_associate'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/actions/publish/$',
+        RepoGroupPublishView.as_view(), name='repo_group_publish'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/actions/unassociate/$',
+        RepoGroupUnassociateView.as_view(), name='repo_group_unassociate'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/distributors/$',
+        RepoGroupDistributorsView.as_view(), name='repo_group_distributors'),
+    url(r'^v2/repo_groups/(?P<repo_group_id>[^/]+)/distributors/(?P<distributor_id>[^/]+)/$',
+        RepoGroupDistributorResource.as_view(), name='repo_group_distributor_resource'),
     url(r'^v2/repositories/(?P<repo_id>\w+)/actions/sync/$', RepoSync.as_view()),
+    url(r'^v2/tasks/$', TasksView.as_view()),
 )
