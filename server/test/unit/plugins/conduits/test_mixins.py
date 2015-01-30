65baed680bb3fa417c0636a66efedf33237db97d
Randy Barlow <rbarlow@redhat.com>
2015-01-30 15:03:51 -0500

    PEP-8 tests for pulp.server.managers.auth.password.

--- server/test/unit/plugins/conduits/test_mixins.py
+++ server/test/unit/plugins/conduits/test_mixins.py
@@ -1,31 +1,14 @@
-#!/usr/bin/python
-#
-# Copyright (c) 2011 Red Hat, Inc.
-#
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
-# Python
 import mock
 
+from ... import base
 from pulp.devel import mock_plugins
-import base
-
 from pulp.plugins.conduits.mixins import ImporterScratchPadMixin, ImporterConduitException
-import pulp.plugins.types.database as types_database
 from pulp.server.db.model.repository import Repo
-import pulp.server.managers.factory as manager_factory
 from pulp.server.managers.repo.cud import RepoManager
 from pulp.server.managers.repo.importer import RepoImporterManager
+import pulp.plugins.types.database as types_database
+import pulp.server.managers.factory as manager_factory
 
-# -- test cases ---------------------------------------------------------------
 
 class ImporterScratchPadMixinTests(base.PulpServerTests):
 
