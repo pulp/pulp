cccbe3bb3ebe602c4d05a249bb8b4936b58237e7
Randy Barlow <rbarlow@redhat.com>
2015-02-13 10:54:15 -0500

    PEP-8 test_manager_factory.py.

--- server/test/unit/server/managers/test_factory.py
+++ server/test/unit/server/managers/test_factory.py
@@ -1,22 +1,6 @@
-#!/usr/bin/python
-#
-# Copyright (c) 2011 Red Hat, Inc.
-#
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
-# Python
 import unittest
 
 from pulp.server.managers import factory
-
 from pulp.server.managers.auth.authentication import AuthenticationManager
 from pulp.server.managers.auth.cert.cert_generator import CertGenerationManager
 from pulp.server.managers.auth.cert.certificate import CertificateManager
@@ -33,12 +17,10 @@ from pulp.server.managers.content.query import ContentQueryManager
 from pulp.server.managers.content.upload import ContentUploadManager
 from pulp.server.managers.event.remote import TopicPublishManager
 from pulp.server.managers.repo.cud import RepoManager
-from pulp.server.managers.repo.unit_association import RepoUnitAssociationManager
 from pulp.server.managers.repo.publish import RepoPublishManager
 from pulp.server.managers.repo.query import RepoQueryManager
 from pulp.server.managers.repo.sync import RepoSyncManager
-
-# -- test cases --------------------------------------------------------------
+from pulp.server.managers.repo.unit_association import RepoUnitAssociationManager
 
 
 class FactoryTests(unittest.TestCase):
@@ -65,7 +47,8 @@ class FactoryTests(unittest.TestCase):
         self.assertTrue(isinstance(factory.user_manager(), UserManager))
         self.assertTrue(isinstance(factory.user_query_manager(), UserQueryManager))
         self.assertTrue(isinstance(factory.repo_manager(), RepoManager))
-        self.assertTrue(isinstance(factory.repo_unit_association_manager(), RepoUnitAssociationManager))
+        self.assertTrue(isinstance(factory.repo_unit_association_manager(),
+                                   RepoUnitAssociationManager))
         self.assertTrue(isinstance(factory.repo_publish_manager(), RepoPublishManager))
         self.assertTrue(isinstance(factory.repo_query_manager(), RepoQueryManager))
         self.assertTrue(isinstance(factory.repo_sync_manager(), RepoSyncManager))
@@ -101,7 +84,7 @@ class FactoryTests(unittest.TestCase):
             self.fail('Invalid manager type did not raise an exception')
         except factory.InvalidType, e:
             self.assertEqual(e.type_key, 'foo')
-            print(e)
+            self.assertEqual(str(e), 'Invalid manager type requested [foo]')
 
     def test_register_and_reset(self):
         """
