b4750e89360320aae737f0045df40f2d7a38446d
Barnaby Court <bcourt@redhat.com>
2015-02-17 12:27:51 -0500

    Bump patch version in core pulp

--- client_consumer/setup.py
+++ client_consumer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-client-consumer',
-    version='2.6.0',
+    version='2.6.1a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- nodes/common/setup.py
+++ nodes/common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_common',
-    version='2.6.0',
+    version='2.6.1a1',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
