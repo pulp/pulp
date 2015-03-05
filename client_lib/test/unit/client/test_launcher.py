89926941369efad9386776ce9c06da8d493fd977
Sayli Karmarkar <skarmark@redhat.com>
2015-03-05 13:25:34 -0800

    a few additional unit tests

--- client_lib/test/unit/client/test_launcher.py
+++ client_lib/test/unit/client/test_launcher.py
@@ -1,9 +1,11 @@
 """
 This module contains tests for the pulp.client.launcher module.
 """
+import logging
 import os
 import shutil
 import stat
+import sys
 import tempfile
 import unittest
 
@@ -11,6 +13,31 @@ import mock
 
 from pulp.client import constants, launcher
 from pulp.common import config
+
+
+class TestLoggingSetup(unittest.TestCase):
+    """
+    This class contains tests for default logging setup.
+    """
+    def test_initialize_logging_no_verbose(self):
+        cli_logger = launcher._initialize_logging(verbose=None)
+        self.assertEqual(cli_logger.level, logging.FATAL)
+        self._test_handler(cli_logger)
+
+    def test_initialize_logging_verbose(self):
+        cli_logger = launcher._initialize_logging(verbose=1)
+        self.assertEqual(cli_logger.level, logging.INFO)
+        self._test_handler(cli_logger)
+
+    def test_initialize_logging_extra_verbose(self):
+        cli_logger = launcher._initialize_logging(verbose=2)
+        self.assertEqual(cli_logger.level, logging.DEBUG)
+        self._test_handler(cli_logger)
+
+    def _test_handler(self, cli_logger):
+        handler = cli_logger.handlers[0]
+        self.assertEqual(handler.stream, sys.stderr)
+        self.assertEqual(handler.formatter._fmt, logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')._fmt)
 
 
 class TestCreateBindings(unittest.TestCase):
@@ -48,6 +75,25 @@ class TestCreateBindings(unittest.TestCase):
 
         self.assertEqual(bindings.bindings.server.verify_ssl, True)
         self.assertEqual(bindings.bindings.server.ca_path, different_path)
+
+    def test_verify_default_logging(self):
+        """
+        Make sure that the None or 1 values for verbose set api_responses_logger to None
+        """
+        bindings = launcher._create_bindings(self.config, None, 'username', 'password')
+        self.assertEqual(bindings.bindings.server.api_responses_logger, None)
+        bindings = launcher._create_bindings(self.config, None, 'username', 'password', verbose=1)
+        self.assertEqual(bindings.bindings.server.api_responses_logger, None)
+
+    def test_verify_debug_logging(self):
+        """
+        Make sure that verbose=2 sets api_responses_logger to correct logger with sys.stderr handler
+        """
+        bindings = launcher._create_bindings(self.config, None, 'username', 'password', verbose=2)
+        api_logger = bindings.bindings.server.api_responses_logger
+        handler = api_logger.handlers[0]
+        self.assertEqual(handler.stream, sys.stderr)
+        self.assertEqual(handler.formatter._fmt, logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')._fmt)
 
 
 class TestEnsureUserPulpDir(unittest.TestCase):
