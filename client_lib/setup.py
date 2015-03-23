0f05c98117bef36f66db3fcaba96c873357195fa
Barnaby Court <bcourt@redhat.com>
2015-03-23 09:27:21 -0400

    Bump to 2.6.0-1

--- client_lib/setup.py
+++ client_lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-client-lib',
-    version='2.6.0c10',
+    version='2.6.0',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- nodes/parent/setup.py
+++ nodes/parent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_parent',
-    version='2.6.0c10',
+    version='2.6.0',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
