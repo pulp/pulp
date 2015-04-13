64a1d03dfd58b9ebf9e25019f94eb779c896606f
Ina Panova <ipanova@redhat.com>
2015-04-13 15:10:55 +0200

    convert "migration_tracker" collection to mongoengine.

--- server/pulp/server/db/model/migration_tracker.py
+++ server/pulp/server/db/model/migration_tracker.py
@@ -1,18 +1,7 @@
-# Copyright (c) 2012 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
-from pulp.server.db.model.base import Model
+from mongoengine import Document, IntField, StringField
 
 
-class MigrationTracker(Model):
+class MigrationTracker(Document):
     """
     This is used to track state about our migrations package. There will be one object for each
     migration package in pulp.server.db.migrations, and we will track which migration version each
@@ -24,29 +13,11 @@ class MigrationTracker(Model):
     :type version: int
     """
 
-    collection_name = 'migration_trackers'
-    unique_indices = ('name',)
+    name = StringField(unique=True, required=True)
+    version = IntField(default=0)
+    # For backward compatibility
+    _ns = StringField(default='migration_trackers')
 
-    def __init__(self, name, version):
-        """
-        Initialize the MigrationTracker with name and version.
-
-        :param name:    The name is used to store the name of the migration package this object is
-                        tracking
-        :type  name:    str
-        :param version: The version we want to set for the MigrationTracker
-        :type  version: int
-        """
-        super(MigrationTracker, self).__init__()
-
-        self.name = name
-        self.version = version
-
-    def save(self):
-        """
-        Save any changes made to this MigrationTracker to the database. If it doesn't exist in the
-        database already, insert a new record to represent it.
-        """
-        self.get_collection().update({'name': self.name},
-                                     {'name': self.name, 'version': self.version},
-                                     upsert=True, safe=True)
+    meta = {'collection': 'migration_trackers',
+            'indexes': [],  # small collection, does not need an index
+            'allow_inheritance': False}
