c86b995895a31c09d53fe43402a9d9772bd1c68c
Dennis Kliban <dkliban@redhat.com>
2015-04-22 13:14:30 -0400

    Fixed some flake8 stuff

--- server/test/unit/server/managers/content/test_query.py
+++ server/test/unit/server/managers/content/test_query.py
@@ -1,5 +1,4 @@
 import errno
-import inspect
 import os
 import unittest
 
@@ -25,11 +24,11 @@ class TestContentQueryManager(unittest.TestCase):
         mock_makedirs.side_effect = OSError(errno.EACCES, os.strerror(errno.EACCES))
         self.assertRaises(OSError, ContentQueryManager().request_content_unit_file_path, 'rpm', '/name/blah')
         mock_makedirs.assert_called_once_with('/var/lib/pulp/content/rpm/name')
-        
+
     @mock.patch('pulp.server.managers.content.upload.os.makedirs')
     @mock.patch.object(ContentQueryManager, 'get_root_content_dir')
     def test_request_content_unit_file_path_no_error(self, mock_root_dir, mock_makedirs):
         mock_root_dir.return_value = '/var/lib/pulp/content/rpm/'
-        mock_makedirs.return_value= '/var/lib/pulp/content/rpm/name'
+        mock_makedirs.return_value = '/var/lib/pulp/content/rpm/name'
         ContentQueryManager().request_content_unit_file_path('rpm', '/name/blah')
         mock_makedirs.assert_called_once_with('/var/lib/pulp/content/rpm/name')
