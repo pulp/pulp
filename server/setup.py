668bbe781a72f9aa0cf02bfcfb2618d30b27fe52
Dennis Kliban <dkliban@redhat.com>
2015-05-04 14:27:50 -0400

    Bumping version for release

--- server/setup.py
+++ server/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-server',
-    version='2.6.1c3',
+    version='2.6.1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
