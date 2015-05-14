c7c99cd39f24b1852b6e0df33c350eb50633b565
Randy Barlow <rbarlow@redhat.com>
2015-05-14 19:33:31 -0400

    PEP-8 client_lib.

--- client_lib/test/unit/test_commands_consumer_manage.py
+++ client_lib/test/unit/test_commands_consumer_manage.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2013 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the License
-# (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied, including the
-# implied warranties of MERCHANTABILITY, NON-INFRINGEMENT, or FITNESS FOR A
-# PARTICULAR PURPOSE.
-# You should have received a copy of GPLv2 along with this software; if not,
-# see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-
 import unittest
 
 import mock
@@ -114,4 +101,3 @@ class UpdateCommandTests(base.PulpClientTests):
         self.assertEqual(body['delta']['display_name'], 'Test Consumer')
         self.assertEqual(body['delta']['description'], 'Consumer for testing')
         self.assertEqual(body['delta']['notes'], {'a': 'a', 'b': 'b'})
-
--- client_lib/test/unit/test_commands_consumer_query.py
+++ client_lib/test/unit/test_commands_consumer_query.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright © 2013 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the License
-# (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied, including the
-# implied warranties of MERCHANTABILITY, NON-INFRINGEMENT, or FITNESS FOR A
-# PARTICULAR PURPOSE.
-# You should have received a copy of GPLv2 along with this software; if not,
-# see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-
 import unittest
 
 import mock
@@ -171,5 +158,3 @@ class HistoryCommandTests(base.PulpClientTests):
         self.assertTrue(url.find('sort') < 0)
         self.assertTrue(url.find('start_date=2013-02-14') > 0)
         self.assertTrue(url.find('end_date') < 0)
-
-
