a2a04bf32f13620822d54f553bbcc1bd39054598
Dennis Kliban <dkliban@redhat.com>
2015-05-18 14:52:32 -0400

    Bumping version

--- client_admin/setup.py
+++ client_admin/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-client-admin',
-    version='2.6.2a1',
+    version='2.6.2b2',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
