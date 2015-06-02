b029f86f0319df89ca8cb792a35bb253be0064d8
Randy Barlow <rbarlow@redhat.com>
2015-06-02 15:02:42 -0400

    PEP-8 some more of pulp.devel.

--- devel/setup.py
+++ devel/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name='pulp-devel',
     version='2.8.0a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
-    author_email='pulp-list@redhat.com',
-    )
+    author_email='pulp-list@redhat.com')
