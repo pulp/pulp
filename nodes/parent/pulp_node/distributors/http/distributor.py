351fcc56c146a261ea4d8716f5726436bb97ec60
shubham90 <sbhawsin@redhat.com>
2015-08-05 16:42:32 -0400

    Issue 1097: Cancelling a task while sync
    https://pulp.plan.io/issues/1097
    
    closes #1097

--- nodes/parent/pulp_node/distributors/http/distributor.py
+++ nodes/parent/pulp_node/distributors/http/distributor.py
@@ -1,15 +1,4 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright © 2013 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public
-# License as published by the Free Software Foundation; either version
-# 2 of the License (GPLv2) or (at your option) any later version.
-# There is NO WARRANTY for this software, express or implied,
-# including the implied warranties of MERCHANTABILITY,
-# NON-INFRINGEMENT, or FITNESS FOR A PARTICULAR PURPOSE. You should
-# have received a copy of GPLv2 along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 from gettext import gettext as _
 from logging import getLogger
@@ -146,7 +135,7 @@ class NodesHttpDistributor(Distributor):
         repo_publish_dir = self._get_publish_dir(repo.id, config)
         return HttpPublisher(base_url, alias, repo.id, repo_publish_dir)
 
-    def cancel_publish_repo(self, call_report, call_request):
+    def cancel_publish_repo(self):
         pass
 
     def create_consumer_payload(self, repo, config, binding_config):
