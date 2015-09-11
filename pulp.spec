1a84a1693ec69ed321ff64b366cecd3d35e1b0ad
Dennis Kliban <dkliban@redhat.com>
2015-09-11 17:48:25 +0000

    Bumping version for 2.6.3 release

--- pulp.spec
+++ pulp.spec
@@ -33,7 +33,7 @@
 
 Name: pulp
 Version: 2.6.3
-Release: 0.3.rc%{?dist}
+Release: 1%{?dist}
 Summary: An application for managing software content
 Group: Development/Languages
 License: GPLv2
--- server/setup.py
+++ server/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-server',
-    version='2.6.3c3',
+    version='2.6.3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
