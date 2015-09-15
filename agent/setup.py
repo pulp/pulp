b1116082fde328ba8438f47ec720a33e5cd72b95
Dennis Kliban <dkliban@redhat.com>
2015-09-15 08:39:26 -0400

    Bumping version to 2.7.0-0.5.beta

--- agent/setup.py
+++ agent/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-agent',
-    version='2.7.0b4',
+    version='2.7.0b5',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- server/setup.py
+++ server/setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='pulp-server',
-    version='2.7.0b4',
+    version='2.7.0b5',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
