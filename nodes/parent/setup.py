f4f98ed75291df393c199c35e87eb2516253ebaa
Dennis Kliban <dkliban@redhat.com>
2015-09-22 11:39:01 -0400

    Bumping version to 2.6.4-0.2.beta

--- nodes/parent/setup.py
+++ nodes/parent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_parent',
-    version='2.6.3',
+    version='2.6.4b2',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
--- server/setup.py
+++ server/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-server',
-    version='2.6.3',
+    version='2.6.4b2',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
