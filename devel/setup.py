04be74027f69a2d474c667cda3884571d60710fb
Dennis Kliban <dkliban@redhat.com>
2015-10-21 16:06:14 -0400

    Bumping version to 2.6.5-0.1.beta

--- devel/setup.py
+++ devel/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-devel',
-    version='2.6.3a1',
+    version='2.6.5b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
--- server/setup.py
+++ server/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-server',
-    version='2.6.3a1',
+    version='2.6.5b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
