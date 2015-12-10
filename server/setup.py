a49bc6ff43407a7236dbd7bf5f393652bc74f877
Austin Macdonald <asmacdo@gmail.com>
2015-12-10 15:29:16 -0500

    Bumping version to 2.7.1-1

--- server/setup.py
+++ server/setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='pulp-server',
-    version='2.7.1b1',
+    version='2.7.1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
