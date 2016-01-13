b51219cee90e07f2ae68f2f26a667c48bd589ce3
Dennis Kliban <dkliban@redhat.com>
2016-01-13 10:40:54 -0500

    Bumping version to 2.8.0-0.2.beta

--- nodes/common/setup.py
+++ nodes/common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_common',
-    version='2.8.0b1',
+    version='2.8.0b2',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
