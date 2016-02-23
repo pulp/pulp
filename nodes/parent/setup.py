9764491b7668ef7de0c556a6adf48814003b4d55
Dennis Kliban <dkliban@redhat.com>
2016-02-23 11:16:12 -0500

    Bumping version to 2.8.0-0.6.beta

--- nodes/parent/setup.py
+++ nodes/parent/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_parent',
-    version='2.8.0b5',
+    version='2.8.0b6',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
--- streamer/setup.py
+++ streamer/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-streamer',
-    version='2.8.0b5',
+    version='2.8.0b6',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
