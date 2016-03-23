3272d95c9721d9a8524032069d9c4c213f006a42
Sean Myers <sean.myers@redhat.com>
2016-03-23 17:57:23 -0400

    Bumping version to 2.8.1-0.2.beta

--- nodes/extensions/admin/setup.py
+++ nodes/extensions/admin/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_admin_extensions',
-    version='2.8.1b1',
+    version='2.8.1b2',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
--- pulp.spec
+++ pulp.spec
@@ -35,7 +35,7 @@
 
 Name: pulp
 Version: 2.8.1
-Release: 0.1.beta%{?dist}
+Release: 0.2.beta%{?dist}
 Summary: An application for managing software content
 Group: Development/Languages
 License: GPLv2
