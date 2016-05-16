5996d948fdc1ea9bb19cb2a3321c764f9c0b6649
pulpbot <pulp-infra@redhat.com>
2016-05-16 10:49:14 -0400

    Bumping version to 2.8.3-0.4.rc

--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-common',
-    version='2.8.3c3',
+    version='2.8.3c4',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
