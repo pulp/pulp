2e830ec07420d9422b26745efc4fbf0de0691fe9
Sean Myers <sean.myers@redhat.com>
2016-05-17 13:42:10 -0400

    Bumping version to 2.8.3-1

--- streamer/setup.py
+++ streamer/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-streamer',
-    version='2.8.3c4',
+    version='2.8.3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
