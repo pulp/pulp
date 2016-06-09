c80a2de0952b8d14e2d57ecfa9261f41224a9dec
Sean Myers <sean.myers@redhat.com>
2016-06-08 20:04:22 -0400

    deal with libs dropping py2.4 support breaking PR tests
    As far as I know, the only place we install these libraries from pip is
    in dev environments and in the PR tester. So, I think this change
    therefore only affects developers working on environments running python
    2.4, like el5, and our ability to test our pull requests on environments
    running python 2.4, like el5. :)

--- bindings/setup.py
+++ bindings/setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python2
+import sys
 
 from setuptools import setup, find_packages
+
+PYTHON_MAJOR_MINOR = '%s.%s' % (sys.version_info[0], sys.version_info[1])
+
+# m2crypto 0.25 broke compatability with py2.4
+if PYTHON_MAJOR_MINOR > '2.4':
+    M2CRYPTO_REQUIRES = 'm2crypto'
+else:
+    M2CRYPTO_REQUIRES = 'm2crypto<0.24'
 
 setup(
     name='pulp-bindings',
@@ -9,5 +18,5 @@ setup(
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
     author_email='pulp-list@redhat.com',
-    install_requires=['m2crypto', 'oauth2>=1.5.170']
+    install_requires=['oauth2>=1.5.170', M2CRYPTO_REQUIRES]
 )
--- client_lib/setup.py
+++ client_lib/setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python2
+import sys
 
 from setuptools import setup, find_packages
+
+PYTHON_MAJOR_MINOR = '%s.%s' % (sys.version_info[0], sys.version_info[1])
+
+# m2crypto 0.25 broke compatability with py2.4
+if PYTHON_MAJOR_MINOR > '2.4':
+    M2CRYPTO_REQUIRES = 'm2crypto'
+else:
+    M2CRYPTO_REQUIRES = 'm2crypto<0.24'
 
 setup(
     name='pulp-client-lib',
@@ -9,5 +18,5 @@ setup(
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
     author_email='pulp-list@redhat.com',
-    install_requires=['isodate>0.5.0', 'm2crypto', 'okaara>=1.0.32', 'setuptools']
+    install_requires=['isodate>0.5.0', 'okaara>=1.0.32', 'setuptools', M2CRYPTO_REQUIRES]
 )
