d6d25b1ba97c578f2e861e8ea18d9577cf62f0a8
Sean Myers <sean.myers@redhat.com>
2016-08-02 20:31:02 +0000

    Version bump to 2.10b1

--- oid_validation/setup.py
+++ oid_validation/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-oid_validation',
-    version='2.9.0b1',
+    version='2.10b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
