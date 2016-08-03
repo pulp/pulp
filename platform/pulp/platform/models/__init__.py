d0dfa5069cc51e5417a7d2a2f690d646f7524d75
Jeff Ortel <jortel@redhat.com>
2016-08-03 10:33:29 -0500

    Add consumer models. closes #2085

--- platform/pulp/platform/models/__init__.py
+++ platform/pulp/platform/models/__init__.py
@@ -1,3 +1,6 @@
 # https://docs.djangoproject.com/en/1.8/topics/db/models/#organizing-models-in-a-package
 from .base import Model, MasterModel  # NOQA
 from .generic import GenericRelationModel, GenericKeyValueStore, Config, Notes, Scratchpad  # NOQA
+
+from .consumer import Consumer, ConsumerContent  # NOQA
+from .repository import Distributor  # NOQA
