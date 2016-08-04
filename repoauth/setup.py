6cffb248a7a529795b8e479c176aaa0ed5e35d39
Sean Myers <sean.myers@redhat.com>
2016-08-04 14:27:31 -0400

    Bumping version to 2.9.2-0.2.beta

--- repoauth/setup.py
+++ repoauth/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-repoauth',
-    version='2.9.2b1',
+    version='2.9.2b2',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
