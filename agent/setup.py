ecbff939fb7e7139c592740d78a685b868496523
Sean Myers <sean.myers@redhat.com>
2016-08-04 10:10:57 -0400

    Bumping version to 2.9.2-0.1.beta

--- agent/setup.py
+++ agent/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-agent',
-    version='2.9.2a1',
+    version='2.9.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- devel/setup.py
+++ devel/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-devel',
-    version='2.9.2a1',
+    version='2.9.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
