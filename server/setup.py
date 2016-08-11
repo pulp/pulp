74f653a163d660492bde58583e09eabef9e63e45
Sean Myers <sean.myers@redhat.com>
2016-08-11 14:36:19 -0400

    Bumping version to 2.9.2-0.3.beta

--- server/setup.py
+++ server/setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name='pulp-server',
-    version='2.9.2b2',
+    version='2.9.2b3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
