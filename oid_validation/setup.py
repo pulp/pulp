9824cb4ad98d39036de69a3cea930df28d0701f3
Sean Myers <sean.myers@redhat.com>
2016-08-11 16:19:40 -0400

    Bumping version to 2.8.7-0.3.beta

--- oid_validation/setup.py
+++ oid_validation/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-oid_validation',
-    version='2.8.7b1',
+    version='2.8.7b3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
