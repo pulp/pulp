577298c2cd346debcac852eedc4869728cfcd040
Sean Myers <sean.myers@redhat.com>
2016-09-19 13:22:18 +0000

    db-reset no longer "rm -rf"s (instead it just warns)
    Also some minor tweaks to how travis runs to cut down on the
    amount of bash-fu
    
    re #2194
    https://pulp.plan.io/issues/2194

--- app/pulp/app/db-reset.sh
+++ app/pulp/app/db-reset.sh
@@ -1,9 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 
-# This assumes you're running in vagrant.
-echo "drop database pulp; create database pulp owner pulp" | sudo -u postgres psql
-
-rm -rf migrations
-
+cd `dirname "$0"`
+if [ -d migrations ]
+then
+    echo "Platform 'migrations' dir already exists (`pwd`/migrations)"
+    echo "If resetting the DB fails, migrations for pulp apps (including platform)"
+    echo "may need to be removed for the DB reset to succeed."
+    echo ""
+    echo "Continuing in 3 seconds."
+    sleep 3
+fi
+python manage.py reset_db --noinput
 python manage.py makemigrations pulp --noinput
 python manage.py migrate --noinput
