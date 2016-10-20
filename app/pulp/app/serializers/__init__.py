f212ac5787d4cb6602f26fbd53b0fe78193df096
Bihan Zhang <bizhang@redhat.com>
2016-10-20 11:40:11 -0400

    Add worker serializers
    closes #2300

--- app/pulp/app/serializers/__init__.py
+++ app/pulp/app/serializers/__init__.py
@@ -8,4 +8,4 @@ from pulp.app.serializers.generic import (ConfigKeyValueRelatedField,  # NOQA
     NotesKeyValueRelatedField)  # NOQA
 from pulp.app.serializers.content import ContentSerializer, ContentRelatedField  # NOQA
 from pulp.app.serializers.repository import RepositorySerializer  # NOQA
-from pulp.app.serializers.task import TaskSerializer  # NOQA
+from pulp.app.serializers.task import TaskSerializer, WorkerSerializer # NOQA
