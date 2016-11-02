3df7cff6babc6093e1bee543dd74b2bc39b8d8b7
Sean Myers <sean.myers@redhat.com>
2016-11-02 14:10:10 -0400

    Bumping version to 2.10.2-0.1.beta

--- bindings/setup.py
+++ bindings/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-bindings',
-    version='2.10.1b2',
+    version='2.10.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- repoauth/setup.py
+++ repoauth/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-repoauth',
-    version='2.10.1b2',
+    version='2.10.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
