59e4d6152c37d0fe5c850ab48000ba15c0f53107
Bihan Zhang <bizhang@redhat.com>
2016-11-03 13:42:06 -0400

    Add task and worker viewsets
    closes #2312
    https://pulp.plan.io/issues/2312

--- app/pulp/app/response.py
+++ app/pulp/app/response.py
@@ -1,21 +1,22 @@
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class OperationPostponedResponse(Response):
     """
     An HTTP response class for returning 202 and a list of spawned tasks.
 
-    This reponse object should be used by views that dispatch asynchronous tasks. The most common
+    This response object should be used by views that dispatch asynchronous tasks. The most common
     use case is for sync and publish operations. When JSON is requested, the response will look
     like the following:
 
         [
             {
-                "_href": "/this/needs/to/be/updated/adlfk-bala-23k5l7-lslser",
+                "_href": "/api/v3/tasks/adlfk-bala-23k5l7-lslser",
                 "task_id": "adlfk-bala-23k5l7-lslser"
             },
             {
-                "_href": "/this/needs/to/be/updated/fr63x-dlsd-4566g-dv64m",
+                "_href": "/api/v3/tasks/fr63x-dlsd-4566g-dv64m",
                 "task_id": "fr63x-dlsd-4566g-dv64m"
             }
         ]
@@ -26,9 +27,9 @@ class OperationPostponedResponse(Response):
         Args:
             task_results (list): List of AsyncResult objects used to generate the response.
         """
-        raise NotImplementedError
         tasks = []
         for result in task_results:
-            task = {"_href": result.task_id, "task_id": result.task_id}
+            task = {"_href": reverse('tasks-detail', args=[result.task_id]),
+                    "task_id": result.task_id}
             tasks.append(task)
         super(OperationPostponedResponse, self).__init__(data=tasks, status=202)
