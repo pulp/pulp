d13ef2a13411180a962dd259bfd23ef2457bdb31
Sean Myers <sean.myers@redhat.com>
2016-11-14 10:18:32 -0500

    Bumping version to 2.10.2-0.2.beta

--- nodes/child/setup.py
+++ nodes/child/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp_node_child',
-    version='2.10.1b2',
+    version='2.10.2b2',
     license='GPLv2+',
     packages=find_packages(),
     author='Pulp Team',
