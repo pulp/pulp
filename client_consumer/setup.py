761e4e574ec5678debb4f6de5e2d5713b6acb215
Sean Myers <sean.myers@redhat.com>
2016-11-15 12:26:23 -0500

    Bumping version to 2.10.2-0.3.beta

--- client_consumer/setup.py
+++ client_consumer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-client-consumer',
-    version='2.10.2b2',
+    version='2.10.2b3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- devel/setup.py
+++ devel/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-devel',
-    version='2.10.2b2',
+    version='2.10.2b3',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
