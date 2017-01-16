abcce73bc966ad34909d896e2b8b2e5808895046
Sean Myers <sean.myers@redhat.com>
2017-01-16 16:08:43 -0500

    Bumping version to 2.11.2-0.1.alpha

--- bindings/setup.py
+++ bindings/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-bindings',
-    version='2.11.1b1',
+    version='2.11.2a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- common/setup.py
+++ common/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-common',
-    version='2.11.1b1',
+    version='2.11.2a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
