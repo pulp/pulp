1f4169e8a6eb83ba7673dee4adcf168c2feb1cd3
Sean Myers <sean.myers@redhat.com>
2017-01-17 18:45:37 -0500

    Bumping version to 2.12.1-0.1.alpha

--- agent/setup.py
+++ agent/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-agent',
-    version='2.12b1',
+    version='2.12.1a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
