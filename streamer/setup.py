a9a1f79f6ac2def8f7ed3b21fda4d7d30f22bb4a
Sean Myers <sean.myers@redhat.com>
2017-01-17 16:59:55 -0500

    Bumping version to 2.12.0-0.1.beta

--- streamer/setup.py
+++ streamer/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-streamer',
-    version='2.12a1',
+    version='2.12b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
