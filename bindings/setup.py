55cb98f00a3794fdf0caf7bd2c43381833fc330f
Sean Myers <sean.myers@redhat.com>
2017-01-23 16:11:02 -0500

    Bumping version to 2.11.2-0.1.beta

--- bindings/setup.py
+++ bindings/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-bindings',
-    version='2.11.2a1',
+    version='2.11.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- streamer/setup.py
+++ streamer/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-streamer',
-    version='2.11.2a1',
+    version='2.11.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
