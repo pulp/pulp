bf7433236c5cf5203808341c74715ad257d69718
Dennis Kliban <dkliban@redhat.com>
2017-05-11 15:06:22 -0400

    Problem: API does not support publishing a repository
    Solution: Add API endpoint to dispatch a publish with a specific publisher.
    
    closes #2398

--- platform/pulp/app/tasks/publisher.py
+++ platform/pulp/app/tasks/publisher.py
@@ -1,6 +1,9 @@
+from gettext import gettext as _
+
 from celery import shared_task
 
 from pulp.app import models
+from pulp.tasking.services import storage
 from pulp.tasking.tasks import UserFacingTask
 
 
@@ -15,3 +18,22 @@ def delete(repo_name, publisher_name):
     :type  publisher_name:  str
     """
     models.Publisher.objects.filter(name=publisher_name, repository__name=repo_name).delete()
+
+@shared_task(base=UserFacingTask)
+def publish(repo_name, publisher_name):
+    """
+    Call publish on the publisher defined by a plugin.
+
+    A working directory is prepared, the plugin's publish is called, and then working directory is
+    removed.
+
+    Args:
+        repo_name (str): unique name to specify the repository.
+        publisher_name (str): name to specify the Publisher.
+    """
+    publisher = models.Publisher.objects.get(name=publisher_name,
+                                             repository__name=repo_name).cast()
+
+    with storage.working_dir_context() as working_dir:
+        publisher.working_dir = working_dir
+        publisher.publish()
