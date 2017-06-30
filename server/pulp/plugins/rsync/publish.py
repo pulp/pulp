a212fdcf28b296fe6f40ad5a68a96695c4a49341
Daniel Alley <dalley@redhat.com>
2017-06-30 12:22:25 -0400

    Fix fastforward rsync publishes
    Fixes an issue where fastforward publishes were not
    occurring when they should have been.
    
    closes #2844
    https://pulp.plan.io/issues/2844

--- server/pulp/plugins/rsync/publish.py
+++ server/pulp/plugins/rsync/publish.py
@@ -390,7 +390,7 @@ class Publisher(PublishStep):
         force_full = force_full | config_force_full
         delete = self.get_config().get("delete", False)
 
-        return not force_full and not delete and not self.last_predist_last_published and \
+        return not force_full and not delete and self.last_predist_last_published and \
             ((last_deleted and last_published and last_published > last_deleted) or not last_deleted)
 
     def create_date_range_filter(self, start_date=None, end_date=None):
