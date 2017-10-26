768c6738821ec3d89abb56372da3699e8bd5217f
Patrick Creech <pcreech@redhat.com>
2017-10-26 14:48:38 -0400

    Bumping version to 2.14.2-1

--- client_consumer/setup.py
+++ client_consumer/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-client-consumer',
-    version='2.14.2b1',
+    version='2.14.2',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
