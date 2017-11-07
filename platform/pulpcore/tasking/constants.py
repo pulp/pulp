0414f3a668cda91b4f46f31f351d75de960a4034
David Davis <daviddavis@redhat.com>
2017-11-07 13:58:59 -0500

    Removing celerybeat code
    fixes #3076
    https://pulp.plan.io/issues/3076

--- platform/pulpcore/tasking/constants.py
+++ platform/pulpcore/tasking/constants.py
@@ -3,8 +3,6 @@ from types import SimpleNamespace
 TASKING_CONSTANTS = SimpleNamespace(
     # The name of the resource manager entry in the workers table
     RESOURCE_MANAGER_WORKER_NAME='resource_manager',
-    # The name of the celerybeat entry in the workers table
-    CELERYBEAT_WORKER_NAME='celerybeat',
     # The amount of time (in seconds) between process wakeups to "heartbeat" and perform
     # their tasks.
     PULP_PROCESS_HEARTBEAT_INTERVAL=5,
