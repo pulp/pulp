351b54fb0ecfb7adc8fc702dee8bad71cf44842a
David Davis <daviddavis@redhat.com>
2018-01-03 13:57:23 -0500

    Add username filters for user list endpoint
    fixes #3142
    https://pulp.plan.io/issues/3142

--- pulpcore/pulpcore/app/viewsets/user.py
+++ pulpcore/pulpcore/app/viewsets/user.py
@@ -1,9 +1,21 @@
+from django_filters.rest_framework import filterset
+
 from pulpcore.app.models import User
 from pulpcore.app.serializers import UserSerializer
 from pulpcore.app.viewsets import NamedModelViewSet
+from pulpcore.app.viewsets.custom_filters import CharInFilter
+
+
+class UserFilter(filterset.FilterSet):
+    username_in_list = CharInFilter(name='username', lookup_expr='in')
+
+    class Meta:
+        model = User
+        fields = ['username', 'username_in_list']
 
 
 class UserViewSet(NamedModelViewSet):
     endpoint_name = 'users'
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_class = UserFilter
