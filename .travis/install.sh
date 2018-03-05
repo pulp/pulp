09d889401171998db62f0e23ce2e5dd199013ac7
David Davis <daviddavis@redhat.com>
2018-03-05 17:19:27 -0500

    Run pulp 3 code against pulp_file smoke tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -ev
+set -v
 
 # dev_requirements should not be needed for testing; don't install them to make sure
 pip install -r test_requirements.txt
@@ -7,3 +7,7 @@ pushd common/ && pip install -e . && popd
 pushd pulpcore/ && pip install -e . && popd
 pushd plugin/ && pip install -e .  && popd
 pip install pytest git+https://github.com/PulpQE/pulp-smash.git#egg=pulp-smash
+
+if [ $TEST = 'pulp_file' ]; then
+  pip install git+https://github.com/pulp/pulp_file.git#egg=pulp_file
+fi
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,25 +1,54 @@
 #!/usr/bin/env sh
-set -ev
+set -v
 
-if [ $DB = 'docs' ]; then
+result=0
+
+if [ $TEST = 'docs' ]; then
   pip3 install sphinx sphinxcontrib-swaggerdoc
   cd docs
   make html
+  if [ $? -ne 0 ]; then
+    result=1
+  fi
 else
   # flake8
-  flake8 --config flake8.cfg
+  flake8 --config flake8.cfg || exit 1
+
+  if [ $TEST = 'pulp_file' ]; then
+    pulp-manager makemigrations pulp_file --noinput
+  fi
 
   pulp-manager makemigrations pulp_app --noinput
+
   pulp-manager migrate auth --noinput
-  pulp-manager migrate --noinput && pulp-manager test
+  pulp-manager migrate --noinput
+  if [ $? -ne 0 ]; then
+    result=1
+  fi
+
   pulp-manager test pulpcore/pulpcore/app/tests
+  if [ $? -ne 0 ]; then
+    result=1
+  fi
+
   pulp-manager reset-admin-password --password admin
   pulp-manager runserver >>~/django_runserver.log 2>&1 &
   celery worker -A pulpcore.tasking.celery_app:celery -n resource_manager@%h -Q resource_manager -c 1 --events --umask 18 >>~/resource_manager.log 2>&1 &
   celery worker -A pulpcore.tasking.celery_app:celery -n reserved_resource_worker_1@%h -c 1 --events --umask 18 >>~/reserved_workers-1.log 2>&1 &
   sleep 5
-  py.test -v --color=yes --pyargs pulp_smash.tests.pulp3.pulpcore
+
+  if [ $TEST = 'pulp_file' ]; then
+    py.test -v --color=yes --pyargs pulp_smash.tests.pulp3
+  else
+    py.test -v --color=yes --pyargs pulp_smash.tests.pulp3.pulpcore
+  fi
+  if [ $? -ne 0 ]; then
+    result=1
+  fi
+
   cat ~/django_runserver.log
   cat ~/resource_manager.log
   cat ~/reserved_workers-1.log
 fi
+
+exit $result
