6a27148706e72fbc23c15e53f1b56643f19ac10d
Patrick Creech <pcreech@redhat.com>
2018-03-15 14:08:21 -0400

    Update versions for pulp 2.17 alpha

--- devel/setup.py
+++ devel/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-devel',
-    version='2.16a1',
+    version='2.17a1',
     license='GPLv2+',
     packages=find_packages(exclude=['test', 'test.*']),
     author='Pulp Team',
