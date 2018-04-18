e75870010c2cffb125ee08042c1d5eb0e7b5c026
David Davis <daviddavis@redhat.com>
2018-04-18 16:36:43 -0400

    Add 3.6 to list of supported python versions

--- pulpcore/setup.py
+++ pulpcore/setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ),
     entry_points={
         'console_scripts': [
