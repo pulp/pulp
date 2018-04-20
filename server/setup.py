09aed1c8d42373f1cee905bd3c0953030b0410e5
Patrick Creech <pcreech@redhat.com>
2018-04-19 21:03:26 -0400

    Update version for 2.16.1 beta

--- server/setup.py
+++ server/setup.py
@@ -21,7 +21,7 @@ else:
 
 setup(
     name='pulp-server',
-    version='2.16',
+    version='2.16.1b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
