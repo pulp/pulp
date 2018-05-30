21ad8fe7ccf9f5d2f8bd3cf30bf18daac694ddc5
Daniel Alley <dalley@redhat.com>
2018-05-30 18:31:34 -0400

    Fix dependencies for source installation
    pulpcore is currently at 3.0.0b4, pulpcore-plugin requires 3.0.0b3,
    so the editable installation fails and it falls back to installing from
    PyPI. So the developer installation will not actually be a developer
    installation.
    
    closes #3712
    https://pulp.plan.io/issues/3712

--- plugin/setup.py
+++ plugin/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'pulpcore==3.0.0b3',
+    'pulpcore',
     'aiohttp',
     'aiofiles',
     'backoff',
@@ -14,7 +14,7 @@ setup(
     name='pulpcore-plugin',
     description='Pulp Plugin API',
     long_description=long_description,
-    version='0.1.0b2',
+    version='0.1.0b3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
