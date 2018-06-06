da7305d5befb37b2c1aa849a9f41189230bf2fc6
werwty <bihan.zh@gmail.com>
2018-06-06 16:05:31 -0400

    Bump pulpcore version to beta5, update docs to reflect this.

--- pulpcore/setup.py
+++ pulpcore/setup.py
@@ -21,7 +21,7 @@ setup(
     name='pulpcore',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
-    version='3.0.0b4',
+    version='3.0.0b5',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
