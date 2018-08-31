a451a957c8abce239b7bd2e6479f3cff5fd8b358
Dennis Kliban <dkliban@redhat.com>
2018-08-31 16:22:30 -0400

    Changes the docs-builder script to us 2.17 as latest version

--- .travis/docs-builder.py
+++ .travis/docs-builder.py
@@ -17,7 +17,7 @@ WORKING_DIR = os.path.join(os.environ['TRAVIS_BUILD_DIR'], '../working')
 VERSION_REGEX = "(\s*)(version)(\s*)(=)(\s*)(['\"])(.*)(['\"])(.*)"
 RELEASE_REGEX = "(\s*)(release)(\s*)(=)(\s*)(['\"])(.*)(['\"])(.*)"
 
-LATEST = '2.16'
+LATEST = '2.17'
 
 USERNAME = 'doc_builder'
 HOSTNAME = '8.43.85.236'
