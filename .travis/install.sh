55101fc4553faba18813c2876d4a31ca04e173e7
Dennis Kliban <dkliban@redhat.com>
2018-09-17 08:30:18 -0400

    add sphinx_rtd_theme to the doc install script
    re: #3963
    https://pulp.plan.io/issues/3963

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,7 +9,7 @@ pushd pulpcore/ && pip install -e . && popd
 pushd plugin/ && pip install -e .  && popd
 
 if [ "$TEST" = 'docs' ]; then
-  pip3 install sphinx sphinxcontrib-openapi
+  pip3 install sphinx sphinxcontrib-openapi sphinx_rtd_theme
   return "$?"
 fi
 
