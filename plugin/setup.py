d5a850d7d77fb5f64d4a9e2f04ca1641717c0121
Dennis Kliban <dkliban@redhat.com>
2018-09-19 15:34:43 -0400

    Release notes and release bump for beta 8
    [noissue]

--- plugin/setup.py
+++ plugin/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'pulpcore>=3.0.0b7',
+    'pulpcore>=3.0.0b8',
     'aiohttp',
     'aiofiles',
     'backoff',
@@ -14,7 +14,7 @@ setup(
     name='pulpcore-plugin',
     description='Pulp Plugin API',
     long_description=long_description,
-    version='0.1.0b5',
+    version='0.1.0b6',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
