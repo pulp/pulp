c0f08b6c6d1418b77e5bc44bbb4f325e0f8dc614
Dennis Kliban <dkliban@redhat.com>
2018-09-26 16:56:00 -0400

    docs for beta 9
    [noissue]

--- plugin/setup.py
+++ plugin/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    'pulpcore>=3.0.0b8',
+    'pulpcore>=3.0.0b9',
     'aiohttp',
     'aiofiles',
     'backoff',
@@ -14,7 +14,7 @@ setup(
     name='pulpcore-plugin',
     description='Pulp Plugin API',
     long_description=long_description,
-    version='0.1.0b6',
+    version='0.1.0b7',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
