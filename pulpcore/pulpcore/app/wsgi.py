580f37a0d5bf3f7c4c866fa8ebfbb308c7440bc5
Austin Macdonald <austin@redhat.com>
2018-10-01 10:49:38 -0400

    Move imports so wsgi uses dynaconf settings
    [noissue]

--- pulpcore/pulpcore/app/wsgi.py
+++ pulpcore/pulpcore/app/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pulpcore.app.settings")
 
 # https://github.com/rochacbruno/dynaconf/issues/89
 from dynaconf.contrib import django_dynaconf  # noqa
+from django.core.wsgi import get_wsgi_application # noqa
 
 application = get_wsgi_application()
