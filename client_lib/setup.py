7ece962cfb5d7b0ade996389e03a48cb53553aad
Patrick Creech <pcreech@redhat.com>
2018-10-02 09:41:54 -0400

    Pulp 2.17.1 beta 2

--- client_lib/setup.py
+++ client_lib/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-client-lib',
-    version='2.17.1b1',
+    version='2.17.1b2',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
