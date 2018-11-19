3be1d3a9b5a6020cd634f0fdc4cba565c450794f
Patrick Creech <pcreech@redhat.com>
2018-11-19 14:12:50 -0500

    Update Version for 2.18.0 Beta 1

--- server/setup.py
+++ server/setup.py
@@ -21,7 +21,7 @@ else:
 
 setup(
     name='pulp-server',
-    version='2.19a1',
+    version='2.18b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
