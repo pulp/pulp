61d3ee7e0da913dc0ffd7d563003cba95bb889f2
Dennis Kliban <dkliban@redhat.com>
2018-11-21 12:27:52 -0500

    Adds release notes and version bump for beta 15
    [noissue]

--- pulpcore/setup.py
+++ pulpcore/setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.0b14',
+    version='3.0.0b15',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     license='GPLv2+',
