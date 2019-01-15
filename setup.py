cf15bdfa61c095b55ed1565944819f1a89ab2dc2
David Davis <daviddavis@redhat.com>
2019-01-15 09:10:31 -0500

    Bumping release version and adding rel notes
    [noissue]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requirements = [
 
 setup(
     name='pulpcore',
-    version='3.0.0b18',
+    version='3.0.0b19',
     description='Pulp Django Application and Related Modules',
     long_description=long_description,
     long_description_content_type="text/markdown",
