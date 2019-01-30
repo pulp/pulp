5d3dea74a22932ca5d73476bdc8cf67660c18ff9
Grant Byers <grant@apnic.net>
2019-01-30 16:22:56 +1000

    Add anonymous proxy support to client tools. Add CLI proxy release note, per request.
    Fixes: #3625
    https://pulp.plan.io/issues/3625

--- client_lib/test/unit/client/test_launcher.py
+++ client_lib/test/unit/client/test_launcher.py
@@ -24,7 +24,8 @@ class TestCreateBindings(unittest.TestCase):
         self.ca_path = '/some/path'
         self.config['filesystem'] = {'id_cert_dir': '/dir/', 'id_cert_filename': 'file'}
         self.config['server'] = {'host': 'awesome_host', 'port': 1234, 'verify_ssl': 'true',
-                                 'ca_path': self.ca_path, 'api_prefix': '/mock/prefix'}
+                                 'ca_path': self.ca_path, 'api_prefix': '/mock/prefix',
+                                 'proxy_host': '', 'proxy_port': 3128}
 
     def test_verify_ssl_false(self):
         """
