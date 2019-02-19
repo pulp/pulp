047fa4e97f227287cfcab51ee94464ff8dd44b8d
Zach Huntington-Meath <zhunting@redhat.com>
2019-02-19 14:06:29 -0500

    Pulp 2.18.1 GA

--- agent/setup.py
+++ agent/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-agent',
-    version='2.18.1b1',
+    version='2.18.1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
