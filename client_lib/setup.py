bd5e955e70b9c146094d1f40bee3a24bd8885957
Patrick Creech <pcreech@redhat.com>
2019-03-18 21:52:53 -0400

    2.19.0 RC 1

--- client_lib/setup.py
+++ client_lib/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-client-lib',
-    version='2.19a1',
+    version='2.19c1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
