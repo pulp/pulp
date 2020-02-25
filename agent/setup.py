f10a3e8269c942982bc7afb333d3a7ac505d60f5
Evgeni Golov <evgeni@golov.de>
2020-02-25 16:21:16 +0100

    pulp 2.21.1 beta 1

--- agent/setup.py
+++ agent/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-agent',
-    version='2.21',
+    version='2.21.1b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- client_lib/setup.py
+++ client_lib/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-client-lib',
-    version='2.21',
+    version='2.21.1b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
