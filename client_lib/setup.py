5a3f7ae954b9c467044e2d4eac0ecc457758e7f9
Zach Huntington-Meath <zhunting@redhat.com>
2020-05-08 12:33:00 -0400

    pulp 2.21.2 beta 1

--- client_lib/setup.py
+++ client_lib/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-client-lib',
-    version='2.21.1',
+    version='2.21.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- repoauth/setup.py
+++ repoauth/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-repoauth',
-    version='2.21.1',
+    version='2.21.2b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
