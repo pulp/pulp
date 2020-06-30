3a53901ba71b0b577688fd3697014b0548dda3de
Patrick Creech <pcreech@redhat.com>
2020-06-30 11:24:19 -0400

    Pulp 2.21.3 beta 1

--- agent/setup.py
+++ agent/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-agent',
-    version='2.21.2',
+    version='2.21.3b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
