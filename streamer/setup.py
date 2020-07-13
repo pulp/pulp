d2d892b2bdbf9e6715751b39c3c0bdece637c2b8
Patrick Creech <pcreech@redhat.com>
2020-07-13 14:42:51 -0400

    Pulp 2.21.3 GA

--- streamer/setup.py
+++ streamer/setup.py
@@ -11,7 +11,7 @@ else:
 
 setup(
     name='pulp-streamer',
-    version='2.21.3b1',
+    version='2.21.3',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
