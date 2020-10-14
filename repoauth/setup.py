17a4bc3541c5d117ae9719372fc42ca184726ea0
Zach Huntington-Meath <zhunting@redhat.com>
2020-10-14 09:44:14 -0400

    Pulp 2.21.4 rc 1

--- repoauth/setup.py
+++ repoauth/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pulp-repoauth',
-    version='2.21.3',
+    version='2.21.4',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
