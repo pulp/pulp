2872c363a44d98638b5bbff903a993ae1d6fbfc0
Patrick Creech <pcreech@redhat.com>
2021-01-18 13:51:48 -0500

    Pulp 2.21.5 beta 1

--- bindings/setup.py
+++ bindings/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-bindings',
-    version='2.21.4',
+    version='2.21.5b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
--- client_lib/setup.py
+++ client_lib/setup.py
@@ -13,7 +13,7 @@ else:
 
 setup(
     name='pulp-client-lib',
-    version='2.21.4',
+    version='2.21.5b1',
     license='GPLv2+',
     packages=find_packages(exclude=['test']),
     author='Pulp Team',
