9f5f8f9c3fb5d0594c3bfc6474836a04f7213bca
mprahl <mprahl@users.noreply.github.com>
2021-01-28 10:15:38 -0500

    Set the auth private key size to be 2048 bits
    Clients such as Fedora 33 have a default OpenSSL configuration
    that doesn't allow the usage of 1024 bit keys.
    
    See more information at:
    https://fedoraproject.org/wiki/Changes/StrongCryptoSettings2
    
    closes #8317

--- server/test/unit/server/managers/auth/cert/test_cert_generator.py
+++ server/test/unit/server/managers/auth/cert/test_cert_generator.py
@@ -56,6 +56,8 @@ class TestCertGeneration(unittest.TestCase):
 
         # Verify
         self.assertTrue(pem.startswith('-----BEGIN RSA PRIVATE KEY-----'))
+        # Verify that the key size is 2048 bits
+        self.assertEqual(len(pem) == 1679)
 
     def test_generation(self):
         # Setup
